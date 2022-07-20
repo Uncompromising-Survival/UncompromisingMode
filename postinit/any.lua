a7f1d804670e4bb3f687350f24755b76d63aa6be
Scrimbles <40772985+Shodance@users.noreply.github.com>
2022-07-20 14:34:03 -0400

    Patch fix #1: Hayfever overlay not appearing. Replaced fx4.tex with UM_pollenover.tex, etc. BETA

--- postinit/any.lua
+++ postinit/any.lua
@@ -164,7 +164,7 @@ env.AddPrefabPostInitAny(function(inst)
 end)
 
 --hornet; i dont care enough to know where to put this
---env.AddReplicableComponent("hayfever")
+env.AddReplicableComponent("hayfever")
 
 
 --I don't know where else to put this
@@ -175,32 +175,7 @@ env.AddPrefabPostInit("aphid", function(inst)
 	end
 end)
 
---for the super spawner tags
-env.AddPrefabPostInitAny(function(inst)
-	local old_OnSave = inst.OnSave
-	inst.OnSave = function(inst, data)
-		if inst.umss_tags then
-			data.umss_tags = inst.umss_tags
-		end
 
-		if old_OnSave ~= nil then
-			old_OnSave(inst, data)
-		end
-	end
-
-	local old_OnLoad = inst.OnLoad
-	inst.OnLoad = function(inst, data)
-		if data ~= nil and data.umss_tags ~= nil then
-			for k, v in ipairs(data.umss_tags) do
-				inst:AddTag("umss_"..v)
-			end
-		end
-
-		if old_OnLoad ~= nil then
-			old_OnLoad(inst, data)
-		end
-	end
-end)
 
 
 
