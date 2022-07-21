2331528eb722e056f101c445d68a62653b5647f2
AtobaAzul <atoba.azul.radical@gmail.com>
2022-07-20 21:04:05 -0300

    fixtoolbox

--- scripts/prefabs/winona_toolbox.lua
+++ scripts/prefabs/winona_toolbox.lua
@@ -69,7 +69,6 @@ local function MakeTackleContainer(name, bank, build, assets)
         end
 
 		inst:AddComponent("inspectable")
-		inst.components.inventoryitem.atlasname = "images/inventoryimages/winona_toolbox.xml"
 
         inst:AddComponent("container")
         inst.components.container:WidgetSetup("winona_toolbox")
@@ -81,6 +80,7 @@ local function MakeTackleContainer(name, bank, build, assets)
 
 		inst:AddComponent("inventoryitem")
 		inst.components.inventoryitem:SetOnPutInInventoryFn(OnPutInInventory)
+		inst.components.inventoryitem.atlasname = "images/inventoryimages/winona_toolbox.xml"
 
 		inst:AddComponent("lootdropper")
 
