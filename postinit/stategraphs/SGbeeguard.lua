bb9e5fd991388d7a56fb0af3579721a92cb1cf7d
AtobaAzul <atoba.azul.radical@gmail.com>
2022-07-25 13:00:14 -0300

    bees are still getting sent to backrooms ocasionally, which causes some coords stuff to return nil.

--- postinit/stategraphs/SGbeeguard.lua
+++ postinit/stategraphs/SGbeeguard.lua
@@ -24,12 +24,12 @@ local function ArtificialLocomote(inst,destination,speed)
 		local hypoten = math.sqrt(inst:GetDistanceSqToPoint(destination))
 		local x,y,z = inst.Transform:GetWorldPosition()
 		local x_final,y_final,z_final
-		
+		if x ~= nil and y ~= nil and z ~- nil then
 		x_final = ((destination.x-x)/hypoten)*speed+x
 		z_final = ((destination.z-z)/hypoten)*speed+z
 		
 		inst.Transform:SetPosition(x_final,y,z_final)
-	
+		end
 	end
 end
 
