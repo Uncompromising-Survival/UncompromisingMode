d67e9fb2f69a884299d5e15b2fc746364a0ca5c2
AtobaAzul <atoba.azul.radical@gmail.com>
2022-09-08 16:34:39 -0300

    fix attacker = nil crash in wanda changes

--- init/init_character_changes/wanda.lua
+++ init/init_character_changes/wanda.lua
@@ -38,7 +38,7 @@ if TUNING.DSTU.WANDA_NERF then
         local _GetAttacked = self.GetAttacked
 
         function self:GetAttacked(attacker, damage, weapon, stimuli, ...)
-            if attacker:HasTag("shadow") and self.inst.prefab == "wanda" then
+            if attacker ~= nil and attacker:HasTag("shadow") and self.inst.prefab == "wanda" then
                 damage = damage * 1.2 --or whatever mult you want
                 print(damage)
                 return _GetAttacked(self, attacker, damage, weapon, stimuli, ...)
