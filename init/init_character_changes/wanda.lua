3896caec3278dd87a6291d5e08107451b2558084
AtobaAzul <atoba.azul.radical@gmail.com>
2022-09-14 11:23:38 -0300

    wanda reviver nerf, I have no idea how to test it, but I did make sure it just didn't crash when you get the item(s) or on start.

--- init/init_character_changes/wanda.lua
+++ init/init_character_changes/wanda.lua
@@ -15,13 +15,13 @@ if TUNING.DSTU.WANDA_NERF then
                         return inst.age_state == "old" and 127.5 / 51 or
                             inst.age_state == "normal" and 68 / 51 or 1
                     elseif weapon ~= nil and weapon:HasTag("shadow_item") and target:HasTag("shadow") then
-                        return inst.age_state == "old" and TUNING.WANDA_SHADOW_DAMAGE_OLD * 0.8
-                            or inst.age_state == "normal" and TUNING.WANDA_SHADOW_DAMAGE_NORMAL * 0.8
-                            or TUNING.WANDA_SHADOW_DAMAGE_YOUNG * 0.75
+                        return inst.age_state == "old" and TUNING.WANDA_SHADOW_DAMAGE_OLD * 0.5
+                            or inst.age_state == "normal" and TUNING.WANDA_SHADOW_DAMAGE_NORMAL * 0.75
+                            or TUNING.WANDA_SHADOW_DAMAGE_YOUNG
                     elseif target:HasTag("shadow") then
-                        return inst.age_state == "old" and TUNING.WANDA_REGULAR_DAMAGE_OLD * 0.8
-                            or inst.age_state == "normal" and TUNING.WANDA_REGULAR_DAMAGE_NORMAL * 0.8
-                            or TUNING.WANDA_REGULAR_DAMAGE_YOUNG * 0.8
+                        return inst.age_state == "old" and TUNING.WANDA_REGULAR_DAMAGE_OLD * 0.5
+                            or inst.age_state == "normal" and TUNING.WANDA_REGULAR_DAMAGE_NORMAL * 0.75
+                            or TUNING.WANDA_REGULAR_DAMAGE_YOUNG
                     end
                     return _CustomCombatDamage(inst, target, weapon, multiplier, mount)
                 end
@@ -29,5 +29,53 @@ if TUNING.DSTU.WANDA_NERF then
 
             inst.components.combat.customdamagemultfn = CustomCombatDamage
         end
+    end)
+
+    local function Revive_CanTarget(inst, doer, target)
+        -- This is a client side function
+        return target ~= nil and target:HasTag("playerghost") and not target:HasTag("reviving")
+    end
+
+    local function Revive_DoCastSpell(inst, doer, target)
+        if Revive_CanTarget(inst, doer, target) and inst.components.pocketwatch.inactive then
+            if target.last_death_shardid ~= nil and target.last_death_shardid ~= TheShard:GetShardId() then
+                -- if the player is about to get teleported to another shard, give them this item so they will revive on the other side
+                target.components.inventory:GiveItem(SpawnPrefab("pocketwatch_revive_reviver"))
+            end
+
+            target:PushEvent("respawnfromghost", { source = inst, from_haunt = doer == target })
+            if target.components.health ~= nil and target.components.health:GetPenalty() < 0.75 then
+                target.components.health:DeltaPenalty(0.25)
+            end
+            inst.components.rechargeable:Discharge(TUNING.POCKETWATCH_REVIVE_COOLDOWN)
+            return true
+        end
+
+        return false, "REVIVE_FAILED"
+    end
+
+    env.AddPrefabPostInit("pocketwatch_revive", function(inst)
+        if inst.components.pocketwatch ~= nil then
+            inst.components.pocketwatch.DoCastSpell = Revive_DoCastSpell
+        end
+    end)
+
+    local function reviver_DoPenalty(inst)
+        local owner = inst.components.inventoryitem:GetGrandOwner()
+        if owner == nil or not owner:HasTag("playerghost") then
+            inst:Remove()
+            return
+        end
+        if owner.components.health ~= nil and owner.components.health:GetPenalty() < 0.75 then
+            owner.components.health:DeltaPenalty(0.25)
+        end
+    end
+
+    env.AddPrefabPostInit("pocketwatch_revive_reviver", function(inst)
+        if not TheWorld.ismastersim then
+            return
+        end
+
+        inst:DoTaskInTime(0, reviver_DoPenalty)
     end)
 end
