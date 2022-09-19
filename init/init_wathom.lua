b86d19059fc69b2a0834c6ec0f67089f1b118b3e
CanisWTF <alphawolfkidgamer@gmail.com>
2022-09-19 00:28:52 +0000

    ^

--- init/init_wathom.lua
+++ init/init_wathom.lua
@@ -327,7 +327,7 @@ AddStategraphPostInit("wilson", function(inst)
 			onenter = function(inst)
 				inst:ClearBufferedAction()
 
-				inst.components.talker:Say("Can't... Breathe...", nil, true)
+--				inst.components.talker:Say("Can't... Breathe...", nil, true) -- I cant think of anything cool for Wathom to say so away this goes
 
 				inst.AnimState:PlayAnimation("sing_fail", false)
 
