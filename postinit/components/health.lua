0c13c79d3f6c8b702c141118121efb870bce4b6e
TheAxeSwingingSmasher <56132394+TheAxeSwingingSmasher@users.noreply.github.com>
2022-09-20 20:13:05 -0500

    LLA Novv sleeps things vvith a range of 7 vvhen the amulet breaks, it also provides the player vvith 1 second of invuln, -50 sanity vvhen you use it due to the fuel being released, can config these values vvith playtesters.

--- postinit/components/health.lua
+++ postinit/components/health.lua
@@ -7,9 +7,42 @@ local function MayKill(self, amount)
 	end
 end
 
+local function GetSLEEPED(inst, revived)
+    if inst ~= revived and
+        (TheNet:GetPVPEnabled() or not inst:HasTag("player")) and
+        not (inst.components.freezable ~= nil and inst.components.freezable:IsFrozen()) and
+        not (inst.components.pinnable ~= nil and inst.components.pinnable:IsStuck()) and
+        not (inst.components.fossilizable ~= nil and inst.components.fossilizable:IsFossilized()) then
+        local mount = inst.components.rider ~= nil and inst.components.rider:GetMount() or nil
+        if mount ~= nil then
+            mount:PushEvent("ridersleep", { sleepiness = 10, sleeptime = TUNING.PANFLUTE_SLEEPTIME })
+        end
+		if inst.components.sleeper ~= nil then
+            inst.components.sleeper:AddSleepiness(10, TUNING.PANFLUTE_SLEEPTIME)
+        elseif inst.components.grogginess ~= nil then
+            inst.components.grogginess:AddGrogginess(10, TUNING.PANFLUTE_SLEEPTIME)
+        else
+            inst:PushEvent("knockedout")
+        end
+    end
+end
+
+local function FindSleepoPeepo(inst)
+	local x,y,z = inst.Transform:GetWorldPosition()
+	local ents = TheSim:FindEntities(x,y,z,7,{"_combat","_health"})
+	if ents then
+		for i,v in ipairs(ents) do
+			if v.components.health and not v.components.health:IsDead() then
+				GetSLEEPED(v,inst)
+			end
+		end
+	end
+end
+
 local function TriggerLLA(self)
 	local item = self.inst.components.inventory:GetEquippedItem(EQUIPSLOTS.BODY)
 	local item2
+	FindSleepoPeepo(self.inst)
 	self:SetCurrentHealth(1)
 	if self.inst.components.oldager ~= nil then
 		--find a ageless watch
@@ -31,7 +64,13 @@ local function TriggerLLA(self)
 	if self.inst:HasTag("wathom") then
 		self.inst.AnimState:SetBuild("wathom")
 	end
+	SpawnPrefab("slingshotammo_hitfx_gold").Transform:SetPosition(self.inst.Transform:GetWorldPosition())
 	SpawnPrefab("shadow_despawn").Transform:SetPosition(self.inst.Transform:GetWorldPosition())
+	self:SetInvincible(true)
+	if self.inst.components.sanity then
+		self.inst.components.sanity:DoDelta(-50)
+	end
+	self.inst:DoTaskInTime(1,function(inst) if inst.components.health then inst.components.health:SetInvincible(false) end end)
 	item:DoTaskInTime(0, function(item) item:Remove() end)
 end
 
