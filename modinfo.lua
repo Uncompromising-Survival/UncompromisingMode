b7acdd18bedeec12501aee39c208e2f8eeb2f080
AtobaAzul <atoba.azul.radical@gmail.com>
2022-10-01 17:18:54 -0300

    ver

--- modinfo.lua
+++ modinfo.lua
@@ -1,7 +1,7 @@
 name = "[BETA]󰀀 Uncompromising Mode"
 description =
 [[
-󰀔 [ Version 1.15.20.0 : "Calm Before the Storm" ]
+󰀔 [ Version 1.15.20.1 : "Calm Before the Storm" ]
 
 Uncompromising Mode increases the risk and reward for those who have mastered Don't Starve Together.
 
@@ -16,7 +16,7 @@ Prominent features:
 
 author = "󰀈 The Uncomp Dev Team 󰀈"
 
-version = "Calm Before the Storm v1.15.20.0"
+version = "Calm Before the Storm v1.15.20.1"
 
 forumthread = "/topic/111892-announcement-uncompromising-mode/"
 
