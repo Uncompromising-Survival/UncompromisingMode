f575de4e5843e75f2da7db2a75f280beb29523a1
AtobaAzul <atoba.azul.radical@gmail.com>
2022-10-04 17:24:08 -0300

    removed copper steering wheel and bumper recipes

--- init/init_recipes.lua
+++ init/init_recipes.lua
@@ -544,7 +544,7 @@ AddRecipe2(
     nil,
     {"TOOLS"}
 )
-
+--[[
 AddRecipe2(
     "boat_bumper_copper_kit",
     {Ingredient("um_copper_pipe", 14)},
@@ -552,9 +552,9 @@ AddRecipe2(
     {numtogive = 2},
     {"SEAFARING"}
 )
-ChangeSortKey("boat_bumper_copper_kit", "boat_bumper_shell_kit", "SEAFARING", true)
+ChangeSortKey("boat_bumper_copper_kit", "boat_bumper_shell_kit", "SEAFARING", true)]]
 
-
+--[[
 AddRecipe2(
     "steeringwheel_copper_item",
     {Ingredient("um_copper_pipe", 3), Ingredient("gears", 1)},
@@ -562,7 +562,7 @@ AddRecipe2(
     nil,
     {"SEAFARING"}
 )
-ChangeSortKey("steeringwheel_copper_item", "steeringwheel_item", "SEAFARING", true)
+ChangeSortKey("steeringwheel_copper_item", "steeringwheel_item", "SEAFARING", true)]]
 
 --[[
 AddRecipe2(--unsure...
@@ -583,7 +583,7 @@ AddDeconstructRecipe("gore_horn_hat", {Ingredient("meat", 2), Ingredient("nightm
 AddDeconstructRecipe("crabclaw", {Ingredient("rocks", 4), Ingredient("cutstone", 1)})
 AddDeconstructRecipe("slobberlobber", {Ingredient("dragon_scales", 1), Ingredient("meat", 2)})
 AddDeconstructRecipe("um_beegun", {Ingredient("honeycomb", 6),Ingredient("royal_jelly", 2)})
-AddDeconstructRecipe("steeringwheel_copper",  {Ingredient("um_copper_pipe", 3), Ingredient("gears", 1)})
+--AddDeconstructRecipe("steeringwheel_copper",  {Ingredient("um_copper_pipe", 3), Ingredient("gears", 1)})
 
 --Sailing Rebalance related recipes.
 
