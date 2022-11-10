b25e44aaf48b8f48b3e49cb10d48d28b3f3958cc
AtobaAzul <atoba.azul.radical@gmail.com>
2022-11-10 12:26:51 -0300

    reduced darksword drain

--- postinit/prefabs/nightsword.lua
+++ postinit/prefabs/nightsword.lua
@@ -11,7 +11,7 @@ env.AddPrefabPostInit("nightsword", function(inst)
 
     local function DrainSanity(inst, attacker, target)
         if attacker.components.sanity ~= nil and attacker.age_state ~= "old" and not attacker:HasTag("shadowmagic") then
-            attacker.components.sanity:DoDelta(-0.5*(attacker:HasTag("Funny_Words_Magic_Man") and 0.5 or 1))
+            attacker.components.sanity:DoDelta((attacker:HasTag("Funny_Words_Magic_Man") and -0.125 or -0.33))
         end
         if _onattack ~= nil then--mod compat/future proofing, just in case
             _onattack(inst, attacker, target)
