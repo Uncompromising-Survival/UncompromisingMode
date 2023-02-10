1190dfbd68ae53e62675bfdf33385c2fdb905ee9
atoba.azul.radical <atoba.azul.radical@gmail.com>
2023-02-10 11:03:27 -0300

    BY GOD'S LIGHT I SMITE YOU (Deleted sand.lua)
      - Also fixed quotes referencing SAND and not UM_SAND

--- init/init_descriptions/wathgrithr.lua
+++ init/init_descriptions/wathgrithr.lua
@@ -87,8 +87,8 @@ ACTIONFAIL = GLOBAL.STRINGS.CHARACTERS.WATHGRITHR.ACTIONFAIL
         "THIS IS WHERE I BELONG",
         "I DON'T WANT TO GO BACK",
     }
-	DESCRIBE.SAND = "You are a very clean dirt."
-	DESCRIBE.SANDHILL = "I will not make a mountain out of a sandhill."
+	DESCRIBE.UM_SAND = "You are a very clean dirt."
+	DESCRIBE.UM_SANDHILL = "I will not make a mountain out of a sandhill."
 	DESCRIBE.SNOWPILE = "Skaði would not be impressed."
 	DESCRIBE.SNOWGOGGLES = "We shall fight the storm itself!"
 	
