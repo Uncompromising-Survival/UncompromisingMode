15c3385e817c64e1ac4dedde5639fcc695a94004
Scrimbles <40772985+Shodance@users.noreply.github.com>
2023-02-17 22:49:10 -0500

    ver increase

--- modinfo.lua
+++ modinfo.lua
@@ -1,7 +1,7 @@
 name = "[BETA]󰀀 Uncompromising Mode"
 description =
 [[
-󰀔 [ Beta Version v2.0.1.11 ]
+󰀔 [ Beta Version v2.0.1.12 ]
 
 Uncompromising Mode increases the risk and reward for those who have mastered Don't Starve Together.
 
@@ -15,7 +15,7 @@ Prominent features:
 
 author = "󰀈 The Uncomp Dev Team 󰀈"
 
-version = "Beta ??? v2.0.1.11"
+version = "Beta ??? v2.0.1.12"
 
 forumthread = "/topic/111892-announcement-uncompromising-mode/"
 
