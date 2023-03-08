80d6d6b6f0f3e63f35161bfbcfa8e209ae18d643
atoba.azul.radical <atoba.azul.radical@gmail.com>
2023-03-08 15:41:56 -0300

    ver

--- modinfo.lua
+++ modinfo.lua
@@ -1,7 +1,7 @@
 name = "[BETA]󰀀 Uncompromising Mode"
 description =
 [[
-󰀔 [ Beta Version v2.0.4.6 ]
+󰀔 [ Beta Version v2.0.4.7 ]
 
 Uncompromising Mode increases the risk and reward for those who have mastered Don't Starve Together.
 
@@ -15,7 +15,7 @@ Prominent features:
 
 author = "󰀈 The Uncomp Dev Team 󰀈"
 
-version = "Beta ??? v2.0.4.6"
+version = "Beta ??? v2.0.4.7"
 
 forumthread = "/topic/111892-announcement-uncompromising-mode/"
 
