278b6a60293c2238524f187bba3c528f421228b2
ShyNuke3D <131507193+ShyNuke@users.noreply.github.com>
2023-05-26 14:49:50 -0300

    No Souls!

--- postinit/components/lootdropper.lua
+++ postinit/components/lootdropper.lua
@@ -18,7 +18,7 @@ env.AddComponentPostInit("lootdropper", function(self)
 			end
 	
 			local function butcherloot(prefab)
-				if self.inst:HasTag("butchermark") and not (self.inst:HasTag("epic") or self.inst:HasTag("shadowcreature") or self.inst:HasTag("structure")) and math.random() <= .25 and (previous_prefab == nil or previous_prefab ~= prefab) then
+				if self.inst:HasTag("butchermark") and not (self.inst:HasTag("epic") or self.inst:HasTag("shadowcreature") or self.inst:HasTag("structure") or self.inst:HasTag("soulless")) and math.random() <= .25 and (previous_prefab == nil or previous_prefab ~= prefab) then
 					table.insert(loots, prefab)
 				end
 				previous_prefab = prefab
