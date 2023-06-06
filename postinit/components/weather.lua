33c25e1f02c44da383d4eeb7edbec13f5508d651
atoba.azul.radical <atoba.azul.radical@gmail.com>
2023-06-06 14:11:32 -0300

    some tweaks to rain intensity

--- postinit/components/weather.lua
+++ postinit/components/weather.lua
@@ -2,12 +2,14 @@ local env = env
 GLOBAL.setfenv(1, GLOBAL)
 local UpvalueHacker = require("tools/upvaluehacker")
 env.AddComponentPostInit("weather", function(self)
+
+    local _CalculatePrecipitationRate = UpvalueHacker.GetUpvalue(self.OnUpdate, "CalculatePrecipitationRate")
+
     local _OnUpdate = self.OnUpdate
     local _rainfx
     local _snowfx
     local _pollenfx
     local _hasfx = not TheNet:IsDedicated()
-
     for i, v in pairs(Ents) do
         if v.prefab then
             if v.prefab == "rain" then
@@ -22,6 +24,8 @@ env.AddComponentPostInit("weather", function(self)
 
     function self:OnUpdate(dt)
         _OnUpdate(self, dt)
+        local preciprate = _CalculatePrecipitationRate()
+
         if TheWorld.state.issummer and TheWorld.net:HasTag("heatwavestartnet") then
             if _hasfx then
                 _pollenfx.particles_per_tick = _pollenfx.particles_per_tick * 20 + 1 -- MOREEEEEEEEEEEEEEEE
@@ -31,8 +35,8 @@ env.AddComponentPostInit("weather", function(self)
             if _hasfx then
                 local tornado_dist = math.sqrt(ThePlayer:GetDistanceSqToInst(TheSim:FindFirstEntityWithTag("um_tornado")))
                 local intensity = Lerp(10, 1, tornado_dist / 300)
-                _rainfx.particles_per_tick = intensity
-                _rainfx.splashes_per_tick = intensity
+                _rainfx.particles_per_tick = preciprate * 5 + intensity
+                _rainfx.splashes_per_tick = preciprate * 3 + intensity
             end
         end
     end
