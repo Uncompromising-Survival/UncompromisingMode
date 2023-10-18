9d1c5e714f031f238584b0223e0918ae14da6990
AtobaAzul <atoba.azul.radical@gmail.com>
2023-10-18 12:48:38 -0300

    made um_music config clientside, nofishyincrockpot config

--- init/init_postinit.lua
+++ init/init_postinit.lua
@@ -379,7 +379,7 @@ if GetModConfigData("scalemailbuff") then
     table.insert(prefab_post, "armor_dragonfly")
 end
 
-if (not GLOBAL:TestForIA() or GetModConfigData("um_music")) then
+if (not GLOBAL:TestForIA() or GetModConfigData("um_music", true)) then
     table.insert(component_post, "dynamicmusic")
 end
 
