d5f6db8eb64ec0ca349da4bfeceff5f06fa1ce70
AtobaAzul <atoba.azul.radical@gmail.com>
2024-01-29 19:01:09 -0300

    commented out worldly skilltrees because it isn`t working. Thanks klei.

--- init/init_gamemodes/init_uncompromising_mode.lua
+++ init/init_gamemodes/init_uncompromising_mode.lua
@@ -191,6 +191,6 @@ if GetModConfigData("hardcore") then
 end
 
 modimport("init/init_loadingtips")
-if GetModConfigData("worldlyskilltrees") then
-modimport("init/init_worldlyskilltrees")
-end
+--if GetModConfigData("worldlyskilltrees") then
+--modimport("init/init_worldlyskilltrees")
+--end
