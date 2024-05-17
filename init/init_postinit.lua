ad6efc714d8c8d6dcdfedcf63dc7e31c333af9e0
TheAxeSwingingSmasher <56132394+TheAxeSwingingSmasher@users.noreply.github.com>
2024-05-17 17:03:54 -0500

    Fixes to init_postinit to prevent it from crashing on startup.

--- init/init_postinit.lua
+++ init/init_postinit.lua
@@ -288,8 +288,8 @@ if GetModConfigData("wixie_walter") then
         "healer",         -- Walter gets a 50% bonus from healing items, over time. works on companions too.
         "bufferedaction", -- This handles wixie sending an rpc with the mouse pointer click location
         "wobypicking",    -- This reroutes the pickup action and pickable component to add items to wobys container instead of a nil inventory
-        "dryer"           -- This reroutes the dryer harvest action to add items to a container instead of a nil inventory
-        "channelcastable" -- Just a dirty wrap of the component to stop it from removing the channelcastable component from her weapon when unequipped
+        "dryer",           -- This reroutes the dryer harvest action to add items to a container instead of a nil inventory
+        "channelcastable", -- Just a dirty wrap of the component to stop it from removing the channelcastable component from her weapon when unequipped
 
     }
 
