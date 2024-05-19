d62d4138c78054a3cb1877621008e100618bf6e9
AtobaAzul <atoba.azul.radical@gmail.com>
2024-05-19 11:47:47 -0300

    Fixed incorrect string thing, fixed boat engine recipe being there despite prefab not existing on beta.

--- init/init_recipes.lua
+++ init/init_recipes.lua
@@ -1258,7 +1258,7 @@ if GetModConfigData("wormwood_trapbuffs") then
     GLOBAL.GetValidRecipe("trap_bramble").numtogive = 2
 end
 
-AddRecipe2("um_boat_engine", { Ingredient("wagpunk_bits", 4), Ingredient("cutstone", 2), Ingredient("palmcone_scale", 6)}, TECH.SCIENCE_TWO, { placer = "um_boat_engine_placer",min_spacing=1.5 }, { "SEAFARING" })
+--AddRecipe2("um_boat_engine", { Ingredient("wagpunk_bits", 4), Ingredient("cutstone", 2), Ingredient("palmcone_scale", 6)}, TECH.SCIENCE_TWO, { placer = "um_boat_engine_placer",min_spacing=1.5 }, { "SEAFARING" })
 
 -- WIXIE RELATED CRAFTS
 if GetModConfigData("wixie_walter") then
