621119889e115f38ff90d769aff032f593a885b7
AtobaAzul <atoba.azul.radical@gmail.com>
2024-06-09 12:48:54 -0300

    ver

--- modinfo.lua
+++ modinfo.lua
@@ -6,7 +6,7 @@ if not folder_name:find("workshop-") then
 end
 
 --RELEASE.MAJOR.MINOR.FIX
-local _version = "1.5.0.23"
+local _version = "1.5.0.24"
 
 description = [[
     󰀔 [ "Ghosts from the Past"  (Beta ver: v]] .. _version .. [[)]
