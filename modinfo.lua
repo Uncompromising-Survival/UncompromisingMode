86a1696d5fb8b13925b80d8e740ed607ee85cabe
TheAxeSwingingSmasher <56132394+TheAxeSwingingSmasher@users.noreply.github.com>
2024-07-23 09:14:26 -0500

    Ver Increase

--- modinfo.lua
+++ modinfo.lua
@@ -6,7 +6,7 @@ if not folder_name:find("workshop-") then
 end
 
 --RELEASE.MAJOR.MINOR.FIX
-local _version = "1.6.0.2"
+local _version = "1.6.0.3"
 
 description = [[
     󰀔 [ "Ghosts from the Past"  (Beta ver: v]] .. _version .. [[)]
