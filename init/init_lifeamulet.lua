e0a08c0294aca9b89b1aab4f0f672bfe6c37f40f
Maximum101 <90657656+Maximum101@users.noreply.github.com>
2024-11-09 21:15:43 -0500

    Add fuel sound to Life Giving Amulet, and more.
    Add fuel sound to Life Giving Amulet, some corrections to the code, and commented out unused code.

--- init/init_lifeamulet.lua
+++ init/init_lifeamulet.lua
@@ -7,81 +7,48 @@ GLOBAL.setfenv(1, GLOBAL)
 
 --Classic DS Red Amulet revive (only when worn upon death), and health tick changes
 
-local function healowner(inst, owner)
-    local owner = inst.components.inventoryitem ~= nil and inst.components.inventoryitem.owner or nil
-
-    if inst.components.fueled and inst.components.fueled.currentfuel == 0 then
-        if inst.task ~= nil then
-            inst.task:Cancel()
-            inst.task = nil
-        end
-
-        return
-    end
-
-    if (owner.components.health and owner.components.health:IsHurt())
-        and (owner.components.hunger and owner.components.hunger.current > 5) and not owner:HasTag("deathamp") and owner.components.oldager == nil then
-        owner.components.health:DoDelta(TUNING.REDAMULET_CONVERSION, false, "redamulet")
-        owner.components.hunger:DoDelta(-TUNING.REDAMULET_CONVERSION)
-        inst.components.fueled:DoDelta(-18)
-
-        local healtime = 10
-
-        if owner.components.health ~= nil and owner.components.health:GetPercent() <= 0.5 then
-            healtime = 1 + (8 * owner.components.health:GetPercent())
-        end
-
-        inst.task = inst:DoTaskInTime(healtime, healowner, owner)
+local function CLIENT_PlayFuelSound(inst)
+    local parent = inst.entity:GetParent()
+    local container = parent ~= nil and (parent.replica.inventory or parent.replica.container) or nil
+    if container ~= nil and container:IsOpenedBy(ThePlayer) then
+        TheFocalPoint.SoundEmitter:PlaySound("dontstarve/common/nightmareAddFuel")
     end
 end
 
-local function onequip_red(inst, owner)
-    local skin_build = inst:GetSkinBuild()
-    if skin_build ~= nil then
-        owner:PushEvent("equipskinneditem", inst:GetSkinName())
-        owner.AnimState:OverrideItemSkinSymbol("swap_body", skin_build, "swap_body", inst.GUID, "torso_amulets")
+local function SERVER_PlayFuelSound(inst)
+    local owner = inst.components.inventoryitem.owner
+    if owner == nil then
+        inst.SoundEmitter:PlaySound("dontstarve/common/nightmareAddFuel")
+    elseif inst.components.equippable:IsEquipped() and owner.SoundEmitter ~= nil then
+        owner.SoundEmitter:PlaySound("dontstarve/common/nightmareAddFuel")
     else
-        owner.AnimState:OverrideSymbol("swap_body", "torso_amulets", "redamulet")
-    end
-
-    local healtime = 10
-
-    if owner.components.health ~= nil and owner.components.health:GetPercent() <= 0.5 then
-        healtime = 1 + (8 * owner.components.health:GetPercent())
-    end
-
-    inst.task = inst:DoTaskInTime(healtime, healowner, nil, owner)
-    --inst.task = inst:DoPeriodicTask(10, healowner, nil, owner)
-end
-
-local function onunequip_red(inst, owner)
-    if owner.sg == nil or owner.sg.currentstate.name ~= "amulet_rebirth" then
-        owner.AnimState:ClearOverrideSymbol("swap_body")
-    end
-
-    local skin_build = inst:GetSkinBuild()
-    if skin_build ~= nil then
-        owner:PushEvent("unequipskinneditem", inst:GetSkinName())
-    end
-
-    if inst.task ~= nil then
-        inst.task:Cancel()
-        inst.task = nil
+        inst.playfuelsound:push()
+        --Dedicated server does not need to trigger sfx
+        if not TheNet:IsDedicated() then
+            CLIENT_PlayFuelSound(inst)
+        end
     end
 end
 
-local function nofuel_red(inst)
-    if inst.task ~= nil then
-        inst.task:Cancel()
-        inst.task = nil
-    end
-end
-
-local function ontakefuel_red(inst)
-    if inst.components.equippable:IsEquipped() then
+local function AmuletPostInit(inst)
+    local function healowner(inst, owner)
         local owner = inst.components.inventoryitem ~= nil and inst.components.inventoryitem.owner or nil
 
-        if inst.task == nil and owner ~= nil and not owner:HasTag("deathamp") and owner.components.oldager == nil then --don't bother healing a dead man walking or the person who cannot heal.
+        if inst.components.fueled ~= nil and inst.components.fueled:IsEmpty() then
+            if inst.task ~= nil then
+                inst.task:Cancel()
+                inst.task = nil
+            end
+
+            return
+        end
+
+        if (owner.components.health ~= nil and owner.components.health:IsHurt())
+            and (owner.components.hunger ~= nil and owner.components.hunger.current > 5) and not owner:HasTag("deathamp") and owner.components.oldager == nil then
+            owner.components.health:DoDelta(TUNING.REDAMULET_CONVERSION, false, "redamulet")
+            owner.components.hunger:DoDelta(-TUNING.REDAMULET_CONVERSION)
+            inst.components.fueled:DoDelta(-18)
+
             local healtime = 10
 
             if owner.components.health ~= nil and owner.components.health:GetPercent() <= 0.5 then
@@ -91,33 +58,100 @@ local function ontakefuel_red(inst)
             inst.task = inst:DoTaskInTime(healtime, healowner, owner)
         end
     end
-end
 
-env.AddPrefabPostInit("amulet", function(inst)
-    if not TheWorld.ismastersim then
-        return
+    local function onequip_red(inst, owner)
+        local skin_build = inst:GetSkinBuild()
+        if skin_build ~= nil then
+            owner:PushEvent("equipskinneditem", inst:GetSkinName())
+            owner.AnimState:OverrideItemSkinSymbol("swap_body", skin_build, "swap_body", inst.GUID, "torso_amulets")
+        else
+            owner.AnimState:OverrideSymbol("swap_body", "torso_amulets", "redamulet")
+        end
+
+        local healtime = 10
+
+        if owner.components.health ~= nil and owner.components.health:GetPercent() <= 0.5 then
+            healtime = 1 + (8 * owner.components.health:GetPercent())
+        end
+
+        inst.task = inst:DoTaskInTime(healtime, healowner, nil, owner)
+        --inst.task = inst:DoPeriodicTask(10, healowner, nil, owner)
+    end
+
+    local function onunequip_red(inst, owner)
+        if owner.sg == nil or owner.sg.currentstate.name ~= "amulet_rebirth" then
+            owner.AnimState:ClearOverrideSymbol("swap_body")
+        end
+
+        local skin_build = inst:GetSkinBuild()
+        if skin_build ~= nil then
+            owner:PushEvent("unequipskinneditem", inst:GetSkinName())
+        end
+
+        if inst.task ~= nil then
+            inst.task:Cancel()
+            inst.task = nil
+        end
+    end
+
+    local function nofuel_red(inst)
+        if inst.task ~= nil then
+            inst.task:Cancel()
+            inst.task = nil
+        end
+    end
+
+    local function ontakefuel_red(inst)
+        SERVER_PlayFuelSound(inst)
+        if inst.components.equippable ~= nil and inst.components.equippable:IsEquipped() then
+            local owner = inst.components.inventoryitem ~= nil and inst.components.inventoryitem.owner or nil
+
+            if inst.task == nil and owner ~= nil and not owner:HasTag("deathamp") and owner.components.oldager == nil then --don't bother healing a dead man walking or the person who cannot heal.
+                local healtime = 10
+
+                if owner.components.health ~= nil and owner.components.health:GetPercent() <= 0.5 then
+                    healtime = 1 + (8 * owner.components.health:GetPercent())
+                end
+
+                inst.task = inst:DoTaskInTime(healtime, healowner, owner)
+            end
+        end
     end
 
     inst:RemoveComponent("finiteuses")
 
-    inst:AddComponent("fueled")
-    inst.components.fueled:InitializeFuelLevel(TUNING.LARGE_FUEL * 2)
-    inst.components.fueled.fueltype = FUELTYPE.NIGHTMARE
-    inst.components.fueled:SetDepletedFn(nofuel_red)
-    inst.components.fueled:SetTakeFuelFn(ontakefuel_red)
-    inst.components.fueled.accepting = true
+    local fueled = inst:AddComponent("fueled")
+    fueled:InitializeFuelLevel(TUNING.LARGE_FUEL * 2)
+    fueled.fueltype = FUELTYPE.NIGHTMARE
+    fueled:SetDepletedFn(nofuel_red)
+    fueled:SetTakeFuelFn(ontakefuel_red)
+    fueled.accepting = true
 
-    if inst.components.equippable ~= nil then
-        inst.components.equippable:SetOnEquip(onequip_red)
-        inst.components.equippable:SetOnUnequip(onunequip_red)
+    local equippable = inst.components.equippable
+    if equippable ~= nil then
+        equippable:SetOnEquip(onequip_red)
+        equippable:SetOnUnequip(onunequip_red)
     end
 
     inst:RemoveComponent("hauntable")
+end
+
+env.AddPrefabPostInit("amulet", function(inst)
+    inst.playfuelsound = net_event(inst.GUID, "amulet.playfuelsound")
+
+    if not TheWorld.ismastersim then
+        --delayed because we don't want any old events
+        inst:DoTaskInTime(0, inst.ListenForEvent, "amulet.playfuelsound", CLIENT_PlayFuelSound)
+
+        return
+    end
+
+    AmuletPostInit(inst)
 end)
 
-env.AddPlayerPostInit(function(inst)
+--[[local function AmuletResurrectPostInit(inst)
     local function amulet_resurrect(inst)
-        if inst.components.inventory and inst.components.inventory.equipslots then
+        if inst.components.inventory ~= nil and inst.components.inventory.equipslots then
             for k, v in pairs(inst.components.inventory.equipslots) do
                 if v.prefab == "amulet" then
                     inst:ListenForEvent("animover", function(inst)
@@ -137,5 +171,9 @@ env.AddPlayerPostInit(function(inst)
         end
     end
 
-    --inst:ListenForEvent("death", amulet_resurrect)
-end)
+    inst:ListenForEvent("death", amulet_resurrect)
+end
+
+env.AddPlayerPostInit(function(inst)
+	AmuletResurrectPostInit(inst)
+end)]]
