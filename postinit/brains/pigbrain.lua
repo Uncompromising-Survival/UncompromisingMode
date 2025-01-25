7611400d8905734aaa130155496ccd1463fffaa9
TheAxeSwingingSmasher <56132394+TheAxeSwingingSmasher@users.noreply.github.com>
2025-01-25 11:25:41 -0600

    For beta, master, and live: Pigs no longer eat bugs.

--- postinit/brains/pigbrain.lua
+++ postinit/brains/pigbrain.lua
@@ -3,7 +3,9 @@ local UpvalueHacker = GLOBAL.require("tools/upvaluehacker")
 
 AddClassPostConstruct("brains/pigbrain", function(self)
     local FINDFOOD_CANT_TAGS = UpvalueHacker.GetUpvalue(self.OnStart, "FindFoodAction", "FINDFOOD_CANT_TAGS")
-    if FINDFOOD_CANT_TAGS ~= nil then
-		table.insert(FINDFOOD_CANT_TAGS, { "insect", "flying" })
+    if FINDFOOD_CANT_TAGS then
+		table.insert(FINDFOOD_CANT_TAGS, "insect")
+		table.insert(FINDFOOD_CANT_TAGS, "flying")
 	end
+	UpvalueHacker.SetUpvalue(self.OnStart, FINDFOOD_CANT_TAGS, "FindFoodAction", "FINDFOOD_CANT_TAGS")
 end)
