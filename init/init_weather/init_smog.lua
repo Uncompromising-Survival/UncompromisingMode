b86ecff92ea0e72071dfba32710baf9634ccd0ca
Maximum101 <90657656+Maximum101@users.noreply.github.com>
2025-03-18 17:47:16 -0400

    Made the smog state a callable event and made it so that you can "cough" outside of the state. (#197)
    * Update smog.lua
    
    * Made the smog state a callable event.

--- init/init_weather/init_smog.lua
+++ init/init_weather/init_smog.lua
@@ -14,7 +14,7 @@ end
 
 env.AddPrefabPostInitAny(function(inst)
     if not TheWorld.ismastersim then return end
-    inst:DoTaskInTime(0, function(inst) --maybe delaying this by a frame does soemthing.
+    inst:DoTaskInTime(0, function(inst) -- Maybe delaying this by a frame does something.
         if (inst:HasTag("plant") or inst:HasTag("tree")) and inst.components.burnable ~= nil then
             local _OnIgnite = inst.components.burnable.onignite
 
@@ -63,14 +63,17 @@ local function DoCoughSound(inst, fadeSteps, volumeMult)
         function() FadeCoughSound(inst, fadeSteps, volumeMult) end)
 end
 
+local UM_SMOG_COUGH_STATE_TIME = 1.66666675
+
 env.AddStategraphState("wilson", State {
     name = "um_smog_cough",
-    tags = { "um_smog_cough" },
+    tags = {"um_smog_cough"},
 
     onenter = function(inst, sayQuote)
         inst.sg.statemem.do_exit_cough = true
         inst.sg.mem.um_smog_cough = true
-        inst.AnimState:PlayAnimation("sing_pre", false)
+        inst.sg.mem.um_cough_cant_talk = true
+        inst.AnimState:PlayAnimation("sing_pre")
         inst.AnimState:PushAnimation("sing_fail", false)
 
         -- Hide fx_icon in this state so Wigfrid's cough is consistent with other characters.
@@ -81,6 +84,7 @@ env.AddStategraphState("wilson", State {
             talker:Say(GetString(inst, "GAS_DAMAGE"))
             inst.SoundEmitter:KillSound("talk")
         end
+		inst.sg:SetTimeout(UM_SMOG_COUGH_STATE_TIME)
     end,
 
     timeline =
@@ -95,36 +99,77 @@ env.AddStategraphState("wilson", State {
             inst.sg.mem.um_smog_cough = nil
             inst.sg.mem.queuetalk_timeout = nil
         end),
-    },
-
-    events =
-    {
-        EventHandler("animover", function(inst)
-            if inst.AnimState:AnimDone() then
-                inst.sg:GoToState("idle")
-            end
+        TimeEvent(30 * FRAMES, function(inst)
+            inst.sg.mem.um_cough_cant_talk = nil
         end),
     },
+
+    ontimeout = function(inst)
+        inst.sg:GoToState("idle", true)
+    end,
 
     onexit = function(inst)
         if inst.sg.statemem.do_exit_cough then DoCoughSound(inst, 5, 0.7) end
         inst.sg.statemem.do_exit_cough = false
+        inst.sg.mem.um_cough_cant_talk = nil
         inst.AnimState:ShowSymbol("fx_icon")
     end
 })
 
+local CANT_COUGH_TAGS = {"um_smog_cough", "dead"}
+local CANT_GO_TO_COUGH_STATE_TAGS = {"busy", "wereplayer"}
+local CANT_GO_TO_COUGH_NONSTATE_TAGS = {"sleeping"}
+for _, tag in pairs(CANT_COUGH_TAGS) do
+    table.insert(CANT_GO_TO_COUGH_STATE_TAGS, 1, tag)
+    table.insert(CANT_GO_TO_COUGH_NONSTATE_TAGS, 1, tag)
+end
+
+local function CantGoToCough(inst, tags)
+    return inst:HasAnyTag(tags) or inst.sg and (type(tags) == "table" and inst.sg:HasAnyStateTag(unpack(tags)) or inst.sg:HasAnyStateTag(tags))
+end
+
+local function CoughEventFunction(inst, data)
+    if inst.sg.mem.um_recent_smog_cough == true or CantGoToCough(inst, CANT_GO_TO_COUGH_NONSTATE_TAGS) or inst.components.freezable ~= nil and inst.components.freezable:IsFrozen() then return end
+    inst.sg.mem.um_recent_smog_cough = true
+    inst.sg.mem.um_cough_cant_talk = true
+    local talker = inst.components.talker
+    if data and data.talk and talker then
+        talker:Say(GetString(inst, "GAS_DAMAGE"), nil, true)
+        inst.SoundEmitter:KillSound("talk")
+    end
+    inst:DoTaskInTime(11 * FRAMES, function(inst) DoCoughSound(inst, 5, 0.7) end)
+    inst:DoTaskInTime(24 * FRAMES, function(inst) DoCoughSound(inst, 10, 0.8) end)
+    inst:DoTaskInTime(30 * FRAMES, function(inst) if inst.sg and inst.sg.mem then inst.sg.mem.um_cough_cant_talk = nil end end)
+    inst:DoTaskInTime(UM_SMOG_COUGH_STATE_TIME, function(inst) if inst.sg and inst.sg.mem then inst.sg.mem.um_recent_smog_cough = nil end end)
+end
+
 env.AddStategraphPostInit("wilson", function(sg)
-    local _IdleOnEnter = sg.states["idle"].onenter
-    sg.states["idle"].onenter = function(inst, pushanim, ...)
+    local ontalkeventhandler = sg.events["ontalk"]
+    local idlestate = sg.states["idle"]
+
+    sg.events["um_smog_cough"] = EventHandler("um_smog_cough", function(inst, data)
+        if inst.sg:HasStateTag("idle") and not (CantGoToCough(inst, CANT_GO_TO_COUGH_STATE_TAGS)) then
+            inst.sg:GoToState("um_smog_cough", data and data.talk or nil)
+            return
+        end
+        CoughEventFunction(inst, data)
+    end)
+
+    local oldontalkeventhandler_fn = ontalkeventhandler.fn
+    ontalkeventhandler.fn = function(inst, data, ...)
+        if inst.sg.mem.um_cough_cant_talk ~= nil then return end -- In the middle of hacking up some smog, give me a second.
+        oldontalkeventhandler_fn(inst, data, ...)
+    end
+
+    local oldidlestate_onenter = idlestate.onenter
+    idlestate.onenter = function(inst, pushanim, ...)
         local timeRemaining = inst.sg.mem.queuetalk_timeout ~= nil and inst.sg.mem.queuetalk_timeout - GetTime() or nil
         if inst.sg.mem.um_smog_cough == nil or timeRemaining == nil or timeRemaining <= 1 then
-            _IdleOnEnter(inst, pushanim, ...)
+            oldidlestate_onenter(inst, pushanim, ...)
             inst.sg.mem.um_smog_cough = nil
             return
         end
-		if not inst:HasTag("wereplayer") then
-			inst.sg:GoToState("um_smog_cough")
-		end
+        inst:PushEvent("um_smog_cough")
         inst.sg.mem.queuetalk_timeout = nil
     end
 end)
