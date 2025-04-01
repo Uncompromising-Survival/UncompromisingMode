f61aa85abbebc6bd3db11c8dc23fdc0cc44ca283
ShyNuke <131507193+ShyNuke@users.noreply.github.com>
2025-04-01 19:00:06 -0300

    Naughtiness Changes Reverted

--- init/init_tuning.lua
+++ init/init_tuning.lua
@@ -642,12 +642,12 @@ for k, v in pairs(um_naughtiness) do
 end
 
 -- Kramped buff
-if GetModConfigData("kramped_buff") then
-    GLOBAL.NAUGHTY_VALUE["glommer"] = 100
-    TUNING.KRAMPUS_NAUGHTINESS_DECAY_PERIOD = math.huge
-    TUNING.KRAMPUS_THRESHOLD = 60
-    TUNING.KRAMPUS_THRESHOLD_VARIANCE = 40
-end
+--if GetModConfigData("kramped_buff") then
+    --GLOBAL.NAUGHTY_VALUE["glommer"] = 100
+    --TUNING.KRAMPUS_NAUGHTINESS_DECAY_PERIOD = math.huge
+    --TUNING.KRAMPUS_THRESHOLD = 60
+    --TUNING.KRAMPUS_THRESHOLD_VARIANCE = 40
+--end
 
 if GetModConfigData("waxwell_nerf") then
     TUNING.SHADOWWAXWELL_SANITY_PENALTY.SHADOWPROTECTOR = 0.2 --can summon 3 fighters
