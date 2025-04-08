6bebb05d20b4eac9c02b6001347ed4bdca30e2d4
Maximum101 <90657656+Maximum101@users.noreply.github.com>
2025-04-08 18:36:36 -0400

    Changes to Booster Shot.
    Makes it a Max Healer again, and give the max healing buff.

--- postinit/prefabs/lifeinjector.lua
+++ postinit/prefabs/lifeinjector.lua
@@ -1,20 +1,27 @@
 local env = env
 GLOBAL.setfenv(1, GLOBAL)
 
-
-
-local function OnUse(inst, target)
-	if target.components.debuffable ~= nil and target.components.health ~= nil and not target.components.health:IsDead() then
-		target.components.debuffable:AddDebuff("lifeinjector_redcap_buff", "lifeinjector_redcap_buff")
-	end
+local function Heal(self, target)
+    if target.components.health ~= nil then
+        if not target.components.health:IsDead() then
+            target:AddDebuff("lifeinjector_redcap_buff", "lifeinjector_redcap_buff")
+        end
+        if self.inst.components.stackable ~= nil and self.inst.components.stackable:IsStack() then
+            self.inst.components.stackable:Get():Remove()
+        else
+            self.inst:Remove()
+        end
+        return true
+    end
 end
 
-
 env.AddPrefabPostInit("lifeinjector", function(inst)
-	if not TheWorld.ismastersim then
-		return
-	end
-	inst:AddComponent("healer")
-	inst:RemoveComponent("maxhealer")
-	inst.components.healer.onhealfn = OnUse
+    if not TheWorld.ismastersim then
+        return
+    end
+
+    local maxhealer = inst.components.maxhealer
+    if maxhealer then
+        maxhealer.Heal = Heal
+    end
 end)
