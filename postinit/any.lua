f4516d22eec1e71f2964ddbc35d8a751ac168dab
Maximum101 <maxisawesome103@gmail.com>
2025-05-10 02:55:44 +0000

    Commented out Wigfrid Vetcurse Um SpiritBuff thing, it's causing issues at the moment.

--- postinit/any.lua
+++ postinit/any.lua
@@ -229,7 +229,7 @@ function EntityScript:AddPlatformFollower(child)
     end
 end
 
-local NO_UM_SPIRITBUFF_TAGS = {"companion", "abigail", "shadowminion"}
+--[[local NO_UM_SPIRITBUFF_TAGS = {"companion", "abigail", "shadowminion"}
 env.AddPrefabPostInitAny(function(inst)
     if not TheWorld.ismastersim then 
         return inst
@@ -238,6 +238,4 @@ env.AddPrefabPostInitAny(function(inst)
     if not inst:HasAnyTag(NO_UM_SPIRITBUFF_TAGS) and inst.components and inst.components.combat then
         inst:AddComponent("um_spiritbuff")
     end
-end)
-
-
+end)]]
