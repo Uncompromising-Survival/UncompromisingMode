72f95084b13519b1ed4fa8cea404d93ff4d2cb15
ShyNuke <131507193+ShyNuke@users.noreply.github.com>
2025-05-25 14:20:44 -0300

    Ver. Inc.

--- modinfo.lua
+++ modinfo.lua
@@ -6,7 +6,7 @@ if not folder_name:find("workshop-") then
 end
 
 --RELEASE.MAJOR.MINOR.FIX
-local _version = "1.7.2.8"
+local _version = "1.7.2.9"
 
 description = [[
     󰀔 [ "Ghosts from the Past"  (Beta ver: v]] .. _version .. [[)]
