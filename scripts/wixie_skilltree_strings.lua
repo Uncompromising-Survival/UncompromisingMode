2714960842d8de5e05b8e4d76394aaee9699611a
CarlosBraw <caduborges08@gmail.com>
2025-05-25 06:35:04 -0400

    wixie_skilltree_strings.lua - Small rewrite to correct punctuation and use the Ammos' actual names.

--- scripts/wixie_skilltree_strings.lua
+++ scripts/wixie_skilltree_strings.lua
@@ -2,11 +2,11 @@ STRINGS.SKILLTREE.WIXIE = {
 	WIXIE_TAUNT_1_TITLE = "Taunt Level I",
 	WIXIE_TAUNT_1_DESC = "Taunt enemies to raise damage taken by 10%, but increase their speed 15%!",
 	WIXIE_TAUNT_2_TITLE = "Taunt Bonus I",
-	WIXIE_TAUNT_2_DESC = "Reduce speed increase by 5%",
+	WIXIE_TAUNT_2_DESC = "Reduce speed increase by 5%.",
 	WIXIE_TAUNT_3_TITLE = "Taunt Bonus II",
-	WIXIE_TAUNT_3_DESC = "Reduce speed increase by 10%",
+	WIXIE_TAUNT_3_DESC = "Reduce speed increase by 10%.",
 	WIXIE_TAUNT_4_TITLE = "Taunt Bonus III",
-	WIXIE_TAUNT_4_DESC = "Reduce speed increase by 15%",
+	WIXIE_TAUNT_4_DESC = "Reduce speed increase by 15%.",
 	WIXIE_TAUNT_5_TITLE = "Taunt Level II",
 	WIXIE_TAUNT_5_DESC = "Taunt enemies longer to raise damage taken by 15%, but increase their speed 20%!",
 	WIXIE_TAUNT_6_TITLE = "Taunt Level III",
@@ -19,39 +19,33 @@ STRINGS.SKILLTREE.WIXIE = {
 	WIXIE_AMMOCRAFT_3_TITLE = "Ammo Crafter III",
 	WIXIE_AMMOCRAFT_3_DESC = "Increase special ammo crafting amount by 5.",
 	
-	WIXIE_SLINGSHOT_AMMO_STINGER_TITLE = "Stinger Rounds",
-	WIXIE_SLINGSHOT_AMMO_STINGER_DESC = "Learn to craft Stinger Rounds: an ammo made of Stingers that sticks into enemies, deals increasing damage for subsequent hit.",
+	WIXIE_SLINGSHOT_AMMO_STINGER_TITLE = "Stinger Zingers",
+	WIXIE_SLINGSHOT_AMMO_STINGER_DESC = "Learn to craft Stinger Zingers: an ammo made of Stingers that sticks into enemies and deals increasing damage for each subsequent hit.",
 
-	WIXIE_SLINGSHOT_AMMO_DREADSTONE_TITLE = "Dreadstone Rounds",
-	WIXIE_SLINGSHOT_AMMO_DREADSTONE_DESC = "Learn to craft Dreadstone Rounds: an ammo made of Dreadstone that knocks enemies back, and has a chance of not being destroyed on impact.",
+	WIXIE_SLINGSHOT_AMMO_DREADSTONE_TITLE = "Dreadstone Pebbles",
+	WIXIE_SLINGSHOT_AMMO_DREADSTONE_DESC = "Learn to craft Dread Pebbles: an ammo made of Dreadstone that knocks enemies back and has a chance of not being destroyed on impact.",
 
-	WIXIE_SLINGSHOT_AMMO_SCRAPFEATHER_TITLE = "Shockscrap Rounds",
-	WIXIE_SLINGSHOT_AMMO_SCRAPFEATHER_DESC = "Learn to craft Shockscrap Rounds: an ammo made of Scrap and Saffron Feathers that creates electric bursts on the target.",
+	WIXIE_SLINGSHOT_AMMO_SCRAPFEATHER_TITLE = "Shockscrap Shots",
+	WIXIE_SLINGSHOT_AMMO_SCRAPFEATHER_DESC = "Learn to craft Shockscrap Shots: an ammo made of Scrap and a Saffron Feather that periodically shocks the target and nearby enemies.",
 
-	WIXIE_SLINGSHOT_AMMO_HONEY_TITLE = "Honey Rounds",
-	WIXIE_SLINGSHOT_AMMO_HONEY_DESC = "Learn to craft Honey Rounds: an ammo made of Honey that creates a slowing trail.",
-
-	WIXIE_SLINGSHOT_AMMO_MOONGLASS_TITLE = "Moon Glass Rounds",
-	WIXIE_SLINGSHOT_AMMO_MOONGLASS_DESC = "Learn to craft Moon Glass Rounds: an ammo made of Moon Shards that repeatedly cuts through enemies.",
-
-	WIXIE_SLINGSHOT_AMMO_GUNPOWDER_TITLE = "Gunpowder Rounds",
-	WIXIE_SLINGSHOT_AMMO_GUNPOWDER_DESC = "Learn to craft Gunpowder Rounds: an ammo mostly made of Gunpowder that explodes on impact, damaging and knocking back enemies.",
+	WIXIE_SLINGSHOT_AMMO_GUNPOWDER_TITLE = "Kablooies",
+	WIXIE_SLINGSHOT_AMMO_GUNPOWDER_DESC = "Learn to craft Kablooies: an ammo mostly made of Gunpowder that explodes on impact, damaging and knocking back enemies.",
 	
 	WIXIE_SHOVE_1_TITLE = "Shover I",
-	WIXIE_SHOVE_1_DESC = "Increase shove distance by 10%",
+	WIXIE_SHOVE_1_DESC = "Increase shove distance by 10%.",
 	WIXIE_SHOVE_2_TITLE = "Shover II",
-	WIXIE_SHOVE_2_DESC = "Increase shove distance by 20%",
+	WIXIE_SHOVE_2_DESC = "Increase shove distance by 20%.",
 	WIXIE_SHOVE_3_TITLE = "Shover III",
-	WIXIE_SHOVE_3_DESC = "Increase shove distance by 30%",
-	
-	WIXIE_ALLEGIANCE_SHADOW_TITLE = "Nightmare Maker",
-	WIXIE_ALLEGIANCE_SHADOW_DESC = "Unlock the ability to craft 'Jessie', Gelblob Rounds, and Pure Horror Rounds.\n ART FOR JESSIE IS UNFINISHED",
-	
-	WIXIE_ALLEGIANCE_LUNAR_TITLE = "Dream Invader",
-	WIXIE_ALLEGIANCE_LUNAR_DESC = "Unlock the ability to craft 'Claire', Lunarhusk Rounds, and Pure Brilliance Rounds.\n ART FOR CLAIRE IS UNFINISHED",
-	
-	WIXIE_ALLEGIANCE_LOCK_1_DESC = "Learn 12 skills to unlock.",
+	WIXIE_SHOVE_3_DESC = "Increase shove distance by 30%.",
 	
 	WIXIE_AMMO_BAG_TITLE = "Ammo Hoarder",
 	WIXIE_AMMO_BAG_DESC = "Learn how to craft an Ammo Pouch for carrying your excess ammo.",
+	
+	WIXIE_ALLEGIANCE_LOCK_1_DESC = "Learn 12 skills to unlock.",
+	
+	WIXIE_ALLEGIANCE_SHADOW_TITLE = "Nightmare Maker",
+	WIXIE_ALLEGIANCE_SHADOW_DESC = "Unlock the ability to craft 'Jessie', Ickies, and Pure Horror Rounds.\nART FOR JESSIE IS UNFINISHED!!",
+	
+	WIXIE_ALLEGIANCE_LUNAR_TITLE = "Dream Invader",
+	WIXIE_ALLEGIANCE_LUNAR_DESC = "Unlock the ability to craft 'Claire', Brightshade Husk Rounds, and Pure Brilliance Rounds.\nART FOR CLAIRE IS UNFINISHED!!",
 }
