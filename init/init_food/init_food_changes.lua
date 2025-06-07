0d13fc87fb0e79c6779399f9966de24d5afacdf5
ShyNuke <131507193+ShyNuke@users.noreply.github.com>
2025-06-07 12:46:17 -0300

    1/3 Honey Stats

--- init/init_food/init_food_changes.lua
+++ init/init_food/init_food_changes.lua
@@ -314,8 +314,8 @@ end
 
 AddPrefabPostInit("honey", function(inst)
 	if inst ~= nil and inst.components.edible ~= nil and TUNING.DSTU.BEEBOX_NERF then
-		inst.components.edible.healthvalue = TUNING.HEALING_SMALL / 2
-		inst.components.edible.hungervalue = TUNING.CALORIES_TINY / 2
+		inst.components.edible.healthvalue = TUNING.HEALING_SMALL / 3
+		inst.components.edible.hungervalue = TUNING.CALORIES_TINY / 3
 	end
 end)
 AddPrefabPostInit("butterflywings", function(inst)
