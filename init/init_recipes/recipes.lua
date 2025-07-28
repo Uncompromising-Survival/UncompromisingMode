3ac3606fba11ca4a9833d486434dd5e131099873
CarlosBraw <caduborges08@gmail.com>
2025-07-28 09:34:22 -0400

    Changed some recipes and deconstruction recipes

--- init/init_recipes/recipes.lua
+++ init/init_recipes/recipes.lua
@@ -115,7 +115,7 @@ ChangeSortKey("um_bear_trap_equippable_tooth", "trap_teeth", "WEAPONS", true)
 
 AddRecipe2(
     "um_bear_trap_equippable_gold",
-    { Ingredient("goldnugget", 1), Ingredient("houndstooth", 2), Ingredient("snappy_jaw", 1)},
+    { Ingredient("goldnugget", 3), Ingredient("houndstooth", 2), Ingredient("snappy_jaw", 1)},
     TECH.SCIENCE_TWO,
     { nil },
     { "WEAPONS" }
@@ -345,34 +345,34 @@ end
 
 --Deconstruct Recipes
 AddDeconstructRecipe("widowshead", { Ingredient("silk", 4), Ingredient("monstermeat", 2), Ingredient("spidergland", 2) })
-AddDeconstructRecipe("widowsgrasp", { Ingredient("silk", 4), Ingredient("monstermeat", 4) })
+AddDeconstructRecipe("widowsgrasp", { Ingredient("monstermeat", 2) })
 AddDeconstructRecipe("shadow_crown", { Ingredient("nightmarefuel", 5), Ingredient("beardhair", 3) })
 AddDeconstructRecipe("rain_horn", { Ingredient("slurtle_shellpieces", 4), Ingredient("rocks", 2), Ingredient("oceanfish_small_9_inv", 3) })
-AddDeconstructRecipe("dormant_rain_horn", { Ingredient("cookiecuttershell", 4), Ingredient("rocks", 2) })
+AddDeconstructRecipe("dormant_rain_horn", { Ingredient("cookiecuttershell", 4), Ingredient("rocks", 2), Ingredient("barnacle", 1) })
 AddDeconstructRecipe("staff_moonfall", { Ingredient("opalpreciousgem", 3), Ingredient("slurtle_shellpieces", 5), Ingredient("livinglog", 3) })
 AddDeconstructRecipe("rimeweed_whip", { Ingredient("um_rimeweed_itemvine", 6) })
-AddDeconstructRecipe("snaildrakehat", { Ingredient("snapalm", 2), Ingredient("slurtle_shellpieces", 1) })
-AddDeconstructRecipe("snaildrakebucket", { Ingredient("snapalm", 2), Ingredient("slurtle_shellpieces", 1) })
-AddDeconstructRecipe("slurtlehat", { Ingredient("slurtleslime", 2), Ingredient("slurtle_shellpieces", 1) })
-AddDeconstructRecipe("armorsnurtleshell", { Ingredient("slurtleslime", 2), Ingredient("slurtle_shellpieces", 1) })
-AddDeconstructRecipe("snappy_jaw", { Ingredient("flint", 3), Ingredient("rope", 1), Ingredient("houndstooth", 1) })
-AddDeconstructRecipe("pied_piper_flute", { Ingredient("twigs", 3), Ingredient("rat_tail", 1) })
+AddDeconstructRecipe("snaildrakehat", { Ingredient("slurtle_shellpieces", 3) })
+AddDeconstructRecipe("snaildrakebucket", { Ingredient("slurtle_shellpieces", 3) })
+AddDeconstructRecipe("slurtlehat", { Ingredient("slurtle_shellpieces", 3) })
+AddDeconstructRecipe("armorsnurtleshell", { Ingredient("slurtle_shellpieces", 3) })
+AddDeconstructRecipe("snappy_jaw", { Ingredient("flint", 3), Ingredient("rope", 1), Ingredient("houndstooth", 3) })
+AddDeconstructRecipe("pied_piper_flute", { Ingredient("twigs", 3), Ingredient("goldnugget", 1) })
 AddDeconstructRecipe("skullflask", { Ingredient("boneshard", 2), Ingredient("nightmarefuel", 6), Ingredient("livinglog", 1) })
-AddDeconstructRecipe("skullflask_empty", { Ingredient("boneshard", 2), Ingredient("nightmarefuel", 2), Ingredient("livinglog", 1) })
-AddDeconstructRecipe("corvushat", { Ingredient("silk", 6), Ingredient("feather_robin", 2), Ingredient("seeds", 1) })
+AddDeconstructRecipe("skullflask_empty", { Ingredient("boneshard", 2), Ingredient("nightmarefuel", 1), Ingredient("livinglog", 1) })
+AddDeconstructRecipe("corvushat", { Ingredient("silk", 6), Ingredient("feather_robin", 2), Ingredient("seeds", 1) }) --:)
 
 --Vet Curse Deconstruct Recipes
-AddDeconstructRecipe("cursed_antler", { Ingredient("boneshard", 8), Ingredient("nightmarefuel", 2) })
+AddDeconstructRecipe("cursed_antler", { Ingredient("boneshard", 8) })
 AddDeconstructRecipe("beargerclaw", { Ingredient("boneshard", 4), Ingredient("furtuft", 10) })
 AddDeconstructRecipe("klaus_amulet", { Ingredient("goldnugget", 4), Ingredient("nightmarefuel", 6) })
 AddDeconstructRecipe("feather_frock", { Ingredient("goose_feather", 6) })
-AddDeconstructRecipe("gore_horn_hat", { Ingredient("meat", 2), Ingredient("nightmarefuel", 6) })
-AddDeconstructRecipe("crabclaw", { Ingredient("meat", 2), Ingredient("rocks", 6), Ingredient("cutstone", 1) })
+AddDeconstructRecipe("gore_horn_hat", { Ingredient("nightmarefuel", 10) })
+AddDeconstructRecipe("crabclaw", { Ingredient("meat", 1), Ingredient("rocks", 6) })
 AddDeconstructRecipe("slobberlobber", { Ingredient("meat", 1), Ingredient("dragon_scales", 1) })
 AddDeconstructRecipe("um_beegun", { Ingredient("honeycomb", 6), Ingredient("royal_jelly", 2) })
 AddDeconstructRecipe("silksack", { Ingredient("silk", 6), Ingredient("monstermeat", 2), Ingredient("spidergland", 2) })
-AddDeconstructRecipe("um_moonfly_lantern", { Ingredient("moonglass_charged", 6), Ingredient("moonrocknugget", 4) })
-AddDeconstructRecipe("um_wingsuit", { Ingredient("malbatross_feather", 6), Ingredient("malbatross_feathered_weave", 1) })
+AddDeconstructRecipe("um_moonfly_lantern", { Ingredient("moonglass", 3), Ingredient("moonglass_charged", 3), Ingredient("moonrocknugget", 4) }) --temp moon rocks.
+AddDeconstructRecipe("um_wingsuit", { Ingredient("malbatross_feather", 6) })
 AddDeconstructRecipe("um_exhumer", { Ingredient("boneshard", 9), Ingredient("fossil_piece", 1), Ingredient("nightmarefuel", 2) })
 
 ----deconstruct recipes for craftable items
