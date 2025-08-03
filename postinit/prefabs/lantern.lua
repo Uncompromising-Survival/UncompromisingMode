11c44983fcd986545d46912dc16f498eef964cde
Maximum101 <maxisawesome103@gmail.com>
2025-08-03 07:11:38 +0000

    You can now haunt to launch Lanterns again, but only once their light is turned off.

--- postinit/prefabs/lantern.lua
+++ postinit/prefabs/lantern.lua
@@ -3,49 +3,25 @@ GLOBAL.setfenv(1, GLOBAL)
 
 --local GeneratorGroundCharging = require("generatorcharging")
 
-
 env.AddPrefabPostInit("lantern", function(inst)
     if not TheWorld.ismastersim then
         return
     end
-    
-    local function OnHaunt(inst)
-        local turnoff = inst.components.machine.turnofffn
-        local lanternfuel = inst.components.fueled
-        if inst._light then
-            turnoff(inst)
-        end
-        if math.random() < TUNING.HAUNT_CHANCE_OFTEN and not (lanternfuel:IsEmpty() or lanternfuel:IsFull()) then
-            lanternfuel:DoDelta(42.12, inst) --howlonguntilsomeonefindsoutIdidthis-C
-            local x, y, z = inst.Transform:GetWorldPosition() --maqs,youareprohibitedtomentionthis
-            local lightflakesfx = SpawnPrefab("deer_fire_flakes") --bcausefnny
-		    lightflakesfx:DoTaskInTime(0, lightflakesfx.KillFX)
-            lightflakesfx.Transform:SetPosition(x, y + 0.2, z)
-            lightflakesfx.Transform:SetScale(0.5, 0.5, 0.5)
-            lightflakesfx.AnimState:SetMultColour(0.5, 1, 0, 1)
-        end
-        return true
-    end
 
-    if inst.components.equippable ~= nil then
-        local OnEquip_old = inst.components.equippable.onequipfn
-
-        inst.components.equippable.onequipfn = function(inst, owner)
+    if inst.components.equippable then
+        local _OnEquip = inst.components.equippable.onequipfn
+        inst.components.equippable.onequipfn = function(inst, owner, ...)
             if inst.upgraded then
                 owner:AddTag("batteryuser")
             end
-
-            if OnEquip_old ~= nil then
-                OnEquip_old(inst, owner)
-            end
+            if _OnEquip then _OnEquip(inst, owner, ...) end
         end
 
-        local OnUnequip_old = inst.components.equippable.onunequipfn
-
-        inst.components.equippable.onunequipfn = function(inst, owner)
+        local _OnUnequip = inst.components.equippable.onunequipfn
+        inst.components.equippable.onunequipfn = function(inst, owner, ...)
             if not owner.UM_isBatteryUser then
                 local item = owner.components.inventory:GetEquippedItem(EQUIPSLOTS.HEAD)
-                if item ~= nil then
+                if item then
                     if not item:HasTag("electricaltool") and owner:HasTag("batteryuser") then
                         owner:RemoveTag("batteryuser")
                     end
@@ -55,15 +31,12 @@ env.AddPrefabPostInit("lantern", function(inst)
                     end
                 end
             end
-
-            if OnUnequip_old ~= nil then
-                OnUnequip_old(inst, owner)
-            end
+            if _OnUnequip then _OnUnequip(inst, owner, ...) end
         end
     end
 
     local function OnUpgrade(inst)
-        if inst ~= nil then
+        if inst then
             inst:AddTag("overchargeable")
             inst.upgraded = true
             inst:SetPrefabNameOverride("LANTERN_ELECTRICAL")
@@ -75,54 +48,55 @@ env.AddPrefabPostInit("lantern", function(inst)
             inst.components.named:SetName(STRINGS.NAMES.LANTERN_ELECTRICAL)
             --GeneratorGroundCharging(inst)
             local owner = inst.components.inventoryitem:GetGrandOwner()
-
-            if owner ~= nil and owner.components.inventory ~= nil and owner.components.inventory:GetEquippedItem(EQUIPSLOTS.HANDS) == inst then
+            if owner and owner.components.inventory ~= nil and owner.components.inventory:GetEquippedItem(EQUIPSLOTS.HANDS) == inst then
                 owner:AddTag("batteryuser")
             end
         end --but wait! won't that fuck everything up?
     end --maybe I could alter in onequip instead too.
 
     local _OnSave = inst.OnSave
-	
-    local function OnSave(inst, data)
+    local function OnSave(inst, data, ...)
         if inst.upgraded then
             data.upgraded = inst.upgraded
         end
-		
         if inst.components.fueled ~= nil then
             data.saved_fuel_value = inst.components.fueled:GetPercent()
         end
-		
-        if _OnSave ~= nil then
-            return _OnSave(inst, data)
-        end
+        if _OnSave then return _OnSave(inst, data, ...) end
     end
 
     local _OnLoad = inst.OnLoad
-	
-    local function OnLoad(inst, data)
-        if data ~= nil and data.upgraded then
+    local function OnLoad(inst, data, ...)
+        if data and data.upgraded then
             inst.upgraded = true
             OnUpgrade(inst)
-            if data.saved_fuel_value ~= nil and inst.components.fueled ~= nil then
+            if data.saved_fuel_value and inst.components.fueled then
                 inst:DoTaskInTime(0, function() inst.components.fueled:SetPercent(data.saved_fuel_value) end)
             end
         end
-		
-        if _OnLoad ~= nil then
-            return _OnLoad(inst, data)
-        end
+        if _OnLoad then return _OnLoad(inst, data, ...) end
     end
 
-    inst.OnSave = OnSave
-    inst.OnLoad = OnLoad
-
-    inst.components.hauntable:SetHauntValue(TUNING.HAUNT_SMALL)
-    inst.components.hauntable:SetOnHauntFn(OnHaunt)
+    if inst.components.hauntable then
+        local _OnHaunt = inst.components.hauntable.onhaunt
+        local function OnHaunt(inst, haunter, ...)
+            local turnoff = inst.components.machine.turnofffn
+            if inst._light and inst._light:IsValid() then
+                turnoff(inst)
+                return true
+            else
+                return _OnHaunt(inst, haunter, ...)
+            end
+        end
+        inst.components.hauntable:SetOnHauntFn(OnHaunt)
+    end
 
     inst:AddComponent("upgradeable")
     inst.components.upgradeable.upgradetype = UPGRADETYPES.ELECTRICAL
     inst.components.upgradeable.onupgradefn = OnUpgrade
 
     inst:AddComponent("named")
+
+    inst.OnSave = OnSave
+    inst.OnLoad = OnLoad
 end)
