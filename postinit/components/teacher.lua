3f9c20217b84f548a28e3b0261e99f78487839ca
Maximum101 <maxisawesome103@gmail.com>
2025-08-04 05:34:02 +0000

    Adjusted Teacher component overwrite so it's a bit better.

--- postinit/components/teacher.lua
+++ postinit/components/teacher.lua
@@ -2,26 +2,15 @@ local env = env
 GLOBAL.setfenv(1, GLOBAL)
 
 env.AddComponentPostInit("teacher", function (self)
-
-    --don't really have an option but to overwite :(
-    function self:Teach(target)
-    if self.recipe == nil then
-        self.inst:Remove()
-        return false
-    elseif target.components.builder == nil then
-        return false
-	elseif target.components.builder:KnowsRecipe(self.recipe, true) then
-        return false, "KNOWN"
-    elseif not target.components.builder:CanLearn(self.recipe) then
-        return false, "CANTLEARN"
-    else
-        target.components.builder:UnlockRecipe(self.recipe)
-        if self.onteach then
-            self.onteach(self.inst, target)
+    local _Teach = self.Teach
+    function self:Teach(target, ...)
+        local _Remove
+        if self.recipe then
+            _Remove = self.inst.Remove
+            self.inst.Remove = function() end
         end
-        --self.inst:Remove()
-        return true
-	end
-end
-
+        local ret = _Teach(self, target, ...)
+        if _Remove then self.inst.Remove = _Remove end
+        return ret
+    end
 end)
