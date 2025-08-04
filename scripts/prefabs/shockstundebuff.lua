dd5a4c05e33a2a233ab18536e648a3d53c88962b
Maximum101 <maxisawesome103@gmail.com>
2025-08-04 05:17:40 +0000

    Adjusted Shockstun Debuff to be consistent with creatures that don't have an "electrocute" state.

--- scripts/prefabs/shockstundebuff.lua
+++ scripts/prefabs/shockstundebuff.lua
@@ -1,10 +1,8 @@
 local UpvalueHacker = require("tools/upvaluehacker")
-local OldHitRecoverDelay = CommonHandlers.HitRecoveryDelay
+local _HitRecoverDelay = CommonHandlers.HitRecoveryDelay
 CommonHandlers.HitRecoveryDelay = function(inst, delay, max_hitreacts, skip_cooldown_fn, ...)
-    if inst.um_forcestundebuff then
-        return false
-    end
-    return OldHitRecoverDelay(inst, delay, max_hitreacts, skip_cooldown_fn, ...)
+    if inst.um_forcestundebuff then return false end
+    return _HitRecoverDelay(inst, delay, max_hitreacts, skip_cooldown_fn, ...)
 end
 
 local _hit_recovery_delay = UpvalueHacker.GetUpvalue(CommonHandlers.OnAttacked, "onattacked", "hit_recovery_delay")
@@ -20,35 +18,23 @@ local removetaglist = {"busy", "hit", "attack", "nointerrupt", "nohit", "jumping
 local function OhCrap(inst, target, attacker)
     if not (target.components.health and target.components.health:IsDead()) and not target:HasTag("playerghost") then
         SpawnPrefab("electricchargedfx"):SetTarget(target)
-        target.components.health:DoDelta(-2, nil, "Electricity")
-        if target.brain then
-            target.brain:Stop()
-        end
-        if target.components.locomotor then
-            target.components.locomotor:Stop()
-        end
+        --target.components.health:DoDelta(-2, nil, "Electricity")
+        if target.brain then target.brain:Stop() end
+        if target.components.locomotor then target.components.locomotor:Stop() end
         if target.sg and target.sg.currentstate and target.sg.currentstate.name ~= "shield_start" and target.sg.currentstate.name ~= "shield"
-            and not target.sg:HasAnyStateTag("electrocute", "stunned", "channeling", "notiredhit") then
+            and not target.sg:HasAnyStateTag("electrocute", "channeling") then
             for _, tag in pairs(removetaglist) do
-                if target.sg:HasStateTag(tag) then
-                    target.sg:RemoveStateTag(tag)
-                end
+                if target.sg:HasStateTag(tag) then target.sg:RemoveStateTag(tag) end
             end
-            if not target.sg:HasStateTag("caninterrupt") then
-                target.sg:AddStateTag("caninterrupt")
-            end
-            if not target.um_forcestundebuff then
-                target.um_forcestundebuff = true
-            end
+            if not target.sg:HasStateTag("caninterrupt") then target.sg:AddStateTag("caninterrupt") end
+            if not target.um_forcestundebuff then target.um_forcestundebuff = true end
         end
-        if not target:HasTag("forcestunned") then
-            target:AddTag("forcestunned")
-        end
+        if not target:HasTag("forcestunned") then target:AddTag("forcestunned") end
         target:PushEvent("attacked", {attacker = attacker, damage = 0, stimuli = "soul"})
         if target.components.combat then
-            if target.components.combat.laststartattacktime then
-                target.components.combat.laststartattacktime = target.components.combat.laststartattacktime + 0.2 -- This apparently resets the targets attack timer making it a true "stun".
-            end
+            --[[if target.components.combat.laststartattacktime then
+                target.components.combat.laststartattacktime = target.components.combat.laststartattacktime + .2 -- This apparently resets the targets attack timer making it a true "stun".
+            end]]
             if target.components.combat.hurtsound then
                 target.SoundEmitter:PlaySound(target.components.combat.hurtsound)
             end
@@ -58,53 +44,39 @@ local function OhCrap(inst, target, attacker)
     end
 end
 
-
 local _CalcEntityElectrocuteDuration = CalcEntityElectrocuteDuration
-function CalcEntityElectrocuteDuration(inst, override)
-    local val = _CalcEntityElectrocuteDuration(inst, override)
-    if inst:HasTag("extended_shock_duration") then
-        val = val * 2.5
-    end
+function CalcEntityElectrocuteDuration(inst, override, ...)
+    local val = _CalcEntityElectrocuteDuration(inst, override, ...)
+    if inst:HasTag("extended_shock_duration") then val = val * 2.5 end
     return val
 end
 
 local function OnAttached(inst, target, followsymbol, followoffset, data)
-    if not target:HasTag("electricstunimmune") then --has tag == false
+    if not target:HasTag("electricstunimmune") then
         target:AddDebuff("shockstundebuffimmunity", "shockstundebuffimmunity")
         inst.entity:SetParent(target.entity)
         inst.Transform:SetPosition(0, 0, 0) --in case of loading
-        --Only do normal shockstun bebuff if doesn't have a shock state, otherwise its handled by global CalcEntityElectrocuteDuration
+        --Only do normal shockstundebuff if doesn't have a shock state, otherwise its handled by global CalcEntityElectrocuteDuration
         if not (target.sg:HasState("electrocute") and not IsEntityElectricImmune(target)) then
-            inst.task = inst:DoPeriodicTask(0.2, OhCrap, nil, target, data and data.attacker)
+            inst.task = inst:DoPeriodicTask(.2, OhCrap, nil, target, data and data.attacker)
         else
-            target:AddTag("extended_shock_duration")
+            if not target:HasTag("extended_shock_duration") then target:AddTag("extended_shock_duration") end
             --force once again into elec. state because first hit runs it *before* this tag is added.
-            if target.sg then
-                target.sg:GoToState("electrocute")
-            end
+            if target.sg then target.sg:GoToState("electrocute") end
         end
-        inst:ListenForEvent("death", function()
-            inst:RemoveTag("extended_shock_duration")
-            inst.components.debuff:Stop()
-        end, target)
+        inst:ListenForEvent("death", function() inst.components.debuff:Stop() end, target)
         SpawnPrefab("electricchargedfx"):SetTarget(target)
     else
         inst.components.debuff:Stop()
     end
 end
 
-local function OnRemoved(inst, target)
-    if target:HasTag("extended_shock_duration") then
-        target:RemoveTag("extended_shock_duration")
-    end
-    if target.brain and not (target.components.health and target.components.health:IsDead()) then
-        target.brain:Start()
-    end
-    if target:HasTag("forcestunned") then
-        target:RemoveTag("forcestunned")
-    end
-    if target.um_forcestundebuff then
-        target.um_forcestundebuff = nil
+local function OnDetached(inst, target)
+    if target and target:IsValid() then
+        if target:HasTag("extended_shock_duration") then target:RemoveTag("extended_shock_duration") end
+        if target.brain and not (target.components.health and target.components.health:IsDead()) then target.brain:Start() end
+        if target:HasTag("forcestunned") then target:RemoveTag("forcestunned") end
+        if target.um_forcestundebuff then target.um_forcestundebuff = nil end
     end
     inst:Remove()
 end
@@ -112,20 +84,18 @@ end
 local function OnTimerDone(inst, data)
     if data.name == "stunover" then
         inst.components.debuff:Stop()
-        if inst.task ~= nil then
-            inst.task:Cancel()
-        end
+        if inst.task then inst.task:Cancel() end
     end
 end
 
-local function OnExtended(inst, target)
-    --[[if not target:HasTag("electricstunimmune") then
-    inst.components.timer:StopTimer("stunover")
-    inst.components.timer:StartTimer("stunover", 1.2)
-    inst.task:Cancel()
-    inst.task = inst:DoPeriodicTask(0.2, OhCrap, nil, target)
-    end]]
-end
+--[[local function OnExtended(inst, target)
+    if not target:HasTag("electricstunimmune") then
+        inst.components.timer:StopTimer("stunover")
+        inst.components.timer:StartTimer("stunover", 1.2)
+        inst.task:Cancel()
+        inst.task = inst:DoPeriodicTask(0.2, OhCrap, nil, target)
+    end
+end]]
 
 local function fn()
     local inst = CreateEntity()
@@ -133,7 +103,6 @@ local function fn()
     if not TheWorld.ismastersim then
         --Not meant for client!
         inst:DoTaskInTime(0, inst.Remove)
-
         return inst
     end
 
@@ -148,8 +117,8 @@ local function fn()
 
     inst:AddComponent("debuff")
     inst.components.debuff:SetAttachedFn(OnAttached)
-    inst.components.debuff:SetDetachedFn(OnRemoved)
-    inst.components.debuff:SetExtendedFn(OnExtended)
+    inst.components.debuff:SetDetachedFn(OnDetached)
+    --inst.components.debuff:SetExtendedFn(OnExtended)
     inst.components.debuff.keepondespawn = true
 
     inst:AddComponent("timer")
