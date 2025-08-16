811db0990c3886689ebb6ea74696db9308f4fed4
Maximum101 <maxisawesome103@gmail.com>
2025-08-16 03:43:48 +0000

    Unreverted change and fixed missing ... argument.

--- postinit/stategraphs/SGbeequeen.lua
+++ postinit/stategraphs/SGbeequeen.lua
@@ -158,8 +158,8 @@ env.AddStategraphPostInit("SGbeequeen", function(inst) --For some reason it's ca
     end]]
 
     local _OldOnHit = inst.states["hit"].onexit
-    inst.states["hit"].onexit = function(inst)
-        local ret = _OldOnHit and _OldOnHit(inst)
+    inst.states["hit"].onexit = function(inst, ...)
+        local ret = _OldOnHit and _OldOnHit(inst, ...)
         if not inst.abilitybusy then inst.ActivateHitAbility(inst) end
         if ret then return ret end
     end
