62d63c31167b53113d2ce810599b52d2be9e1660
Anomen77 <anomen77@gmail.com>
2025-08-29 15:46:20 +0200

    Seeds placed within a seed pack-it will not decay

--- postinit/prefabs/seedpouch.lua
+++ postinit/prefabs/seedpouch.lua
@@ -1,7 +1,8 @@
 local env = env
 GLOBAL.setfenv(1, GLOBAL)
 
-
+-- Leaving this here for clarity
+TUNING.SEEDPOUCH_PRESERVER_RATE = 0
 
 env.AddPrefabPostInit("seedpouch", function(inst)
     if not TheWorld.ismastersim then
