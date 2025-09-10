00b7d39ee74ccc1f9a16d3af7aa86d2c8a03f4f6
CarlosBraw <caduborges08@gmail.com>
2025-09-10 18:26:57 -0400

    Umbralla no longer repairs itself when there's acid rain
    (cherry picked from commit 473771845e623ed488ab790ef34a988bfa7b12e3)
    
    Co-authored-by: Anomen77 <anomen77@gmail.com>

--- postinit/prefabs/voidcloth_umbrella.lua
+++ postinit/prefabs/voidcloth_umbrella.lua
@@ -1,11 +1,15 @@
 local env = env
 GLOBAL.setfenv(1, GLOBAL)
 
-
 env.AddPrefabPostInit("voidcloth_umbrella", function(inst)
     if not TheWorld.ismastersim then
         return
     end
 
+    -- For tornado
     inst.components.waterproofer.effectiveness = 1.5
+
+    -- Don't do anything when it's acid raining.
+    -- This is much less intrusive than removing all the acid rain checks from every function 
+    inst.OnIsAcidRaining = function (inst, isacidraining) end
 end)
