d4f569c467aa48f7dbe16125fe7f10a6c34b3f77
CarlosBraw <91644972+CarlosBraw@users.noreply.github.com>
2025-09-10 20:02:28 -0400

    Update schoolspawner.lua
    The Ocean Shadow jumpscare guy now only appears during April 1st, at a 1% chance instead of .5% chance.

--- postinit/components/schoolspawner.lua
+++ postinit/components/schoolspawner.lua
@@ -15,7 +15,7 @@ env.AddComponentPostInit("schoolspawner", function(self)
         local map = TheWorld.Map
 
         local ret = _SpawnSchool(self, spawnpoint, target, override_spawn_offset)
-        if math.random() > 0.995 then
+        if math.random() > 0.99 and TUNING.DSTU.DATES.APRIL_FOOLS then
             local sea_players = {}
 
             for k, v in pairs(AllPlayers) do
