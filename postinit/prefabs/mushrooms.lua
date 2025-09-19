578c90b05236fd054b0a8a16b20d8ee2f6b07231
Maximum101 <maxisawesome103@gmail.com>
2025-09-19 00:39:48 +0000

    return inst

--- postinit/prefabs/mushrooms.lua
+++ postinit/prefabs/mushrooms.lua
@@ -4,7 +4,7 @@ GLOBAL.setfenv(1, GLOBAL)
 if TUNING.DSTU.MUSHROOM_CHANGES then
 
 	env.AddPrefabPostInit("green_mushroom", function(inst)
-		if not TheWorld.ismastersim then return end
+		if not TheWorld.ismastersim then return inst end
 		inst._opentaskfn = inst.opentaskfn
 		inst.opentaskfn = function()
 			inst:AddTag("groundmushroom")
@@ -42,7 +42,7 @@ if TUNING.DSTU.MUSHROOM_CHANGES then
 	end)
 
 	env.AddPrefabPostInit("red_mushroom", function(inst)
-		if not TheWorld.ismastersim then return end
+		if not TheWorld.ismastersim then return inst end
 		inst._opentaskfn = inst.opentaskfn
 		inst.opentaskfn = function()
 			inst:AddTag("groundmushroom")
@@ -80,7 +80,7 @@ if TUNING.DSTU.MUSHROOM_CHANGES then
 	end)
 
 	env.AddPrefabPostInit("blue_mushroom", function(inst)
-		if not TheWorld.ismastersim then return end
+		if not TheWorld.ismastersim then return inst end
 		inst._opentaskfn = inst.opentaskfn
 		inst.opentaskfn = function()
 			inst:AddTag("groundmushroom")
@@ -119,141 +119,83 @@ if TUNING.DSTU.MUSHROOM_CHANGES then
 end
 
 env.AddPrefabPostInit("green_cap", function(inst)
-
 	inst:AddTag("mushroom_fuel")
 	inst:AddTag("green_mushroom_fuel")
-	
-	if not TheWorld.ismastersim then
-		return
-	end
-
+	if not TheWorld.ismastersim then return inst end
 end)
 
 env.AddPrefabPostInit("red_cap", function(inst)
-
 	inst:AddTag("mushroom_fuel")
 	inst:AddTag("red_mushroom_fuel")
-
-	if not TheWorld.ismastersim then
-		return
-	end
-
+	if not TheWorld.ismastersim then return inst end
 end)
 
 env.AddPrefabPostInit("blue_cap", function(inst)
 
 	inst:AddTag("mushroom_fuel")
 	inst:AddTag("blue_mushroom_fuel")
-
-	if not TheWorld.ismastersim then
-		return
-	end
-
+	if not TheWorld.ismastersim then return inst end
 end)
 
 ---------------------------------------------------------
 --FEAST AND FAMINE COMPATABILITY--
 ---------------------------------------------------------
 env.AddPrefabPostInit("green_chunk", function(inst)
-
 	inst:AddTag("mushroom_fuel")
 	inst:AddTag("green_mushroom_fuel")
-
-	if not TheWorld.ismastersim then
-		return
-	end
-
+	if not TheWorld.ismastersim then return inst end
 end)
 
 env.AddPrefabPostInit("red_chunk", function(inst)
-
 	inst:AddTag("mushroom_fuel")
 	inst:AddTag("red_mushroom_fuel")
-
-	if not TheWorld.ismastersim then
-		return
-	end
-
+	if not TheWorld.ismastersim then return inst end
 end)
 
 env.AddPrefabPostInit("blue_chunk", function(inst)
-
 	inst:AddTag("mushroom_fuel")
 	inst:AddTag("blue_mushroom_fuel")
-
-	if not TheWorld.ismastersim then
-		return
-	end
-
+	if not TheWorld.ismastersim then return inst end
 end)
 
 env.AddPrefabPostInit("green_chunk_bloom", function(inst)
-
 	inst:AddTag("mushroom_fuel")
 	inst:AddTag("green_mushroom_fuel")
-
-	if not TheWorld.ismastersim then
-		return
-	end
-
+	if not TheWorld.ismastersim then return inst end
 end)
 
 env.AddPrefabPostInit("red_chunk_bloom", function(inst)
-
 	inst:AddTag("mushroom_fuel")
 	inst:AddTag("red_mushroom_fuel")
-
-	if not TheWorld.ismastersim then
-		return
-	end
-
+	if not TheWorld.ismastersim then return inst end
 end)
 
 env.AddPrefabPostInit("blue_chunk_bloom", function(inst)
-
 	inst:AddTag("mushroom_fuel")
 	inst:AddTag("blue_mushroom_fuel")
-
-	if not TheWorld.ismastersim then
-		return
-	end
-
+	if not TheWorld.ismastersim then return inst end
 end)
 ---------------------------------------------------------
 -- --
 ---------------------------------------------------------
 
 env.AddPrefabPostInit("spore_small", function(inst)
-
 	inst:AddTag("mushroom_fuel")
 	inst:AddTag("green_mushroom_fuel")
-
-	if not TheWorld.ismastersim then
-		return
-	end
-
+	if not TheWorld.ismastersim then return inst end
 end)
 
 env.AddPrefabPostInit("spore_medium", function(inst)
 
 	inst:AddTag("mushroom_fuel")
 	inst:AddTag("red_mushroom_fuel")
-
-	if not TheWorld.ismastersim then
-		return
-	end
-
+	if not TheWorld.ismastersim then return inst end
 end)
 
 env.AddPrefabPostInit("spore_tall", function(inst)
-
 	inst:AddTag("mushroom_fuel")
 	inst:AddTag("blue_mushroom_fuel")
-
-	if not TheWorld.ismastersim then
-		return
-	end
-
+	if not TheWorld.ismastersim then return inst end
 end)
 --[[
 local function testfortransformonload(inst)
