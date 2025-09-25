9a3d39f6d686391d56166a2576fb6147760b888e
loopuleasa <lupuleasa-core@gitlab.com>
2025-09-25 19:42:56 +0300

    Houndious Observious added missing chop sound

--- scripts/prefabs/houndious_observious.lua
+++ scripts/prefabs/houndious_observious.lua
@@ -34,8 +34,11 @@ local function ondestroyed(inst, worker)
     inst:Remove()
 end
 
-local function onhit(inst)
+local function onchop(inst, chopper)
 	inst.AnimState:PlayAnimation("hit",false)
+    if not (chopper ~= nil and chopper:HasTag("playerghost")) then
+        inst.SoundEmitter:PlaySound("dontstarve/wilson/use_axe_tree")
+    end
 end
 
 local function onload(inst, data)
@@ -123,7 +126,7 @@ local function fn()
     inst.components.workable:SetWorkAction(ACTIONS.CHOP)
     inst.components.workable:SetWorkLeft(25)
     inst.components.workable:SetOnFinishCallback(ondestroyed)
-    inst.components.workable:SetOnWorkCallback(onhit)       
+    inst.components.workable:SetOnWorkCallback(onchop)       
 	
 
 	
