47c857fd106200ff078b7927cc8e7e415519816d
CarlosBraw <91644972+CarlosBraw@users.noreply.github.com>
2025-10-01 03:18:45 -0400

    Beearger Bin Jerky thing, Dorman Conch dropping from CK again
    and also Horrible Food stats for Glommer Wings and Hambat!

--- postinit/prefabs/vetcurse_drops.lua
+++ postinit/prefabs/vetcurse_drops.lua
@@ -20,3 +20,5 @@ env.AddPrefabPostInit("deerclops", function(inst) VetCurseItem(inst, "cursed_ant
 env.AddPrefabPostInit("crabking", function(inst) VetCurseItem(inst, "crabclaw") end)
 env.AddPrefabPostInit("minotaur", function(inst) VetCurseItem(inst, "gore_horn_hat") end)
 env.AddPrefabPostInit("dragonfly", function(inst) VetCurseItem(inst, "slobberlobber") end)
+-- Not a vetcurse drop, but adding dormant conch back to crabking here since it was missing since ck rework disabled entire crabking.lua
+env.AddPrefabPostInit("crabking", function(inst) if not TheWorld.ismastersim then return end inst.components.lootdropper:AddChanceLoot("dormant_rain_horn", 1) end)
