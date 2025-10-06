e083feef1d061b531b08220fe440cffaced6bb41
Anomen77 <anomen77@gmail.com>
2025-10-07 01:16:37 +0200

    Cheap cube

--- init/init_recipes/recipe_changes.lua
+++ init/init_recipes/recipe_changes.lua
@@ -122,6 +122,11 @@ AllRecipes["moonrockidol"].ingredients = {
     Ingredient("purplegem", 1)
 }
 
+AllRecipes["icehat"].ingredients = {
+    Ingredient("rope", 2),
+    Ingredient("ice", 15)
+}
+
 AllRecipes["minifan"].ingredients = {
     Ingredient("twigs", 3),
     Ingredient("petals", 4)
