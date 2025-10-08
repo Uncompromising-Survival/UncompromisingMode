944680ca9aba58bf36e6aa7c467b899d8d15df7a
Maximum101 <maxisawesome103@gmail.com>
2025-10-08 01:49:37 +0000

    More changes to the Scythe action thing Carlos pushed.

--- postinit/components/playercontroller.lua
+++ postinit/components/playercontroller.lua
@@ -6,13 +6,12 @@ GLOBAL.setfenv(1, GLOBAL)
 env.AddComponentPostInit("playercontroller", function(self) --By Summerrr, I didn't do anything lool -C
     local _GetActionButtonAction = self.GetActionButtonAction
     function self:GetActionButtonAction(...)
-        local bufferedAction = _GetActionButtonAction(self, ...)
-        local action = bufferedAction ~= nil and bufferedAction.action or nil
-        local inventory = self.inst.components.inventory
-        local tool = inventory ~= nil and inventory:GetEquippedItem(EQUIPSLOTS.HANDS) or nil --You spotted some silly stuff down there too, Max!! TY!!! -C
-        if bufferedAction ~= nil and action ~= nil and action.id == ACTIONS.PICK.id and tool ~= nil and tool:HasTag(ACTIONS.SCYTHE.id.."_tool") then 
-            return BufferedAction(self.inst, bufferedAction.target, ACTIONS.SCYTHE, action ~= ACTIONS.SMOTHER and tool or nil)
+        local actbuttonaction = _GetActionButtonAction(self, ...)
+        local action = actbuttonaction and actbuttonaction.action
+        local tool = self.inst.components.inventory and self.inst.components.inventory:GetEquippedItem(EQUIPSLOTS.HANDS) --You spotted some silly stuff down there too, Max!! TY!!! -C
+        if actbuttonaction and action and action.id == ACTIONS.PICK.id and tool and tool:HasTag(ACTIONS.SCYTHE.id.."_tool") then 
+            return BufferedAction(self.inst, actbuttonaction.target, ACTIONS.SCYTHE, action ~= ACTIONS.SMOTHER and tool or nil)
         end
-        return bufferedAction
+        return actbuttonaction
     end
 end)
