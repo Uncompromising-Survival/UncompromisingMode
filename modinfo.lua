4cdb6a32d9b7c2e5dd9612f7d81a6074d572a3fe
AtobaAzul <atoba.azul.radical@gmail.com>
2025-10-11 13:50:45 -0300

    ver

--- modinfo.lua
+++ modinfo.lua
@@ -6,7 +6,7 @@ if not folder_name:find("workshop-") then
 end
 
 --RELEASE.MAJOR.MINOR.FIX
-local _version = "1.7.4.0"
+local _version = "1.7.5.0"
 
 description = [[
     󰀔 [ "Ghosts of the Past (for now)"  (Beta ver: v]] .. _version .. [[)]
