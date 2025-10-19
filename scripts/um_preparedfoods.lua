220285553d1f52a74539fa403f1129f256465097
CarlosBraw <91644972+CarlosBraw@users.noreply.github.com>
2025-10-19 19:16:23 -0400

    Boneless Kebab properly does not exist with other Meaty Stew config options

--- scripts/um_preparedfoods.lua
+++ scripts/um_preparedfoods.lua
@@ -68,7 +68,6 @@ local function Ghost(eater)
     end
     eater.unghosttask = eater:DoTaskInTime(60, UnGhost)
 end
-local kebabpriority = TUNING.DSTU.BONESTEW ~= "bone_appetit" and -99 or 29
 local um_preparedfoods =
 {
     beefalowings =
@@ -731,23 +730,12 @@ local um_preparedfoods =
         floater = { "med", 0.05, 0.65 },
         card_def = { ingredients = { { "rice1", 2 }, { "honey", 1 } } },
     },
-    um_kebab =
-    {
-        test = function(cooker, names, tags) return (tags.meat and tags.meat > 2.5) and not (tags.monster and tags.monster > 2.5) and not names.boneshard end,
-        hunger = 87.5,
-        health = 8,
-        sanity = 20,
-        priority = kebabpriority,
-        weight = 1,
-        cooktime = 1,
-        foodtype = FOODTYPE.MEAT,
-        perishtime = 7.5 * TUNING.PERISH_TWO_DAY,
-        floater = { "med", 0.05, 0.65 },
-        card_def = { ingredients = { { "meat", 3 } } },
-    },
+
 }
---[[
-local um_kebab =
+
+
+if TUNING.DSTU.BONESTEW == "bone_appetit" then
+    um_preparedfoods["um_kebab"] =
     {
         test = function(cooker, names, tags) return (tags.meat and tags.meat > 2.5) and not (tags.monster and tags.monster > 2.5) and not names.boneshard end,
         hunger = 87.5,
@@ -761,10 +749,8 @@ local um_kebab =
         floater = { "med", 0.05, 0.65 },
         card_def = { ingredients = { { "meat", 3 } } },
     }
-if TUNING.DSTU.BONESTEW == "bone_appetit" then
-    table.insert(um_preparedfoods, um_kebab)
 end
-]]
+
 
 for k, v in pairs(um_preparedfoods) do
     v.name = k
