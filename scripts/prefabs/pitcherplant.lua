034b92faedd078b37733c8155d8aa1e76b037c53
ShyNuke <131507193+ShyNuke@users.noreply.github.com>
2025-10-19 11:48:29 -0300

    Pitcher Plants Fix

--- scripts/prefabs/pitcherplant.lua
+++ scripts/prefabs/pitcherplant.lua
@@ -2,148 +2,104 @@ require "prefabutil"
 
 local assets =
 {
-    Asset("ANIM", "anim/pitcher.zip"),
-
-    Asset("ANIM", "anim/bee_box_hermitcrab.zip"),
-    Asset("MINIMAP_IMAGE", "beebox_hermitcrab"),
+	Asset("ANIM", "anim/pitcher.zip"),
+	Asset("ANIM", "anim/bee_box_hermitcrab.zip"),
+	Asset("MINIMAP_IMAGE", "beebox_hermitcrab"),
 }
 
 local prefabs =
 {
-    "fruitbat",
-    "honey",
+	"fruitbat",
+	"honey",
 }
 
-SetSharedLootTable('pitcherplant',
-    {
-        { 'honey', 1 },
-    })
-
-local function OnEnterLight(inst)
-    if not (TheWorld.state.iswinter or inst:HasTag("burnt"))
-        and TheWorld.state.iscaveday then
-        Start(inst)
-    end
-end
-
-local function OnEnterDark(inst)
-    Stop(inst)
-end
-
-
+SetSharedLootTable('pitcherplant', {
+	{ 'honey', 1 },
+})
 
 local function onchildgoinghome(inst, data)
-    if data.child ~= nil and
-        data.child.bugcount ~= nil then
-        inst.count = data.child.bugcount
-    end
+	if data.child ~= nil and data.child.bugcount ~= nil then
+		inst.count = data.child.bugcount
+	end
 end
 
 local function onsave(inst, data)
-    data.count = inst.count
+	data.count = inst.count
 end
 
 local function onload(inst, data)
-    if data ~= nil and data.count ~= nil then
-        inst.count = data.count
-    end
-end
-local function StartSpawning(inst)
-    if inst.components.childspawner ~= nil
-        and not TheWorld.state.iswinter
-        and not (inst.components.freezable ~= nil and inst.components.freezable:IsFrozen()) then
-        inst.components.childspawner:StartSpawning()
-    end
+	if data ~= nil and data.count ~= nil then
+		inst.count = data.count
+	end
 end
 
-local function StopSpawning(inst)
-    if inst.components.childspawner ~= nil then
-        inst.components.childspawner:StopSpawning()
-    end
+local function UpdateSpawning(inst)
+	if inst.components.childspawner == nil then return end
+
+	if TheWorld.state.isday and not TheWorld.state.iswinter then
+		inst.components.childspawner:StartSpawning()
+	else
+		inst.components.childspawner:StopSpawning()
+	end
 end
-local function OnIsDay(inst, isday)
-    if isday then
-        StartSpawning(inst)
-    else
-        StopSpawning(inst)
-    end
-end
+
 local function OnInit(inst)
-    inst:WatchWorldState("isday", OnIsDay)
-    OnIsDay(inst, TheWorld.state.isday)
-    local x, y, z = inst.Transform:GetWorldPosition()
-    --inst.Transform:SetPosition(x,9,z)
+	inst:WatchWorldState("isday", UpdateSpawning)
+	inst:WatchWorldState("iswinter", UpdateSpawning)
+	UpdateSpawning(inst)
 end
 
 local function onspawnbat(inst, bat)
-    bat.sg:GoToState("flyback")
+	if bat and bat.sg then
+		bat.sg:GoToState("flyback")
+	end
 end
 
-local function MakeBeebox(name, common_postinit)
-    local function fn()
-        local inst = CreateEntity()
+local function fn()
+	local inst = CreateEntity()
 
-        inst.entity:AddTransform()
-        inst.entity:AddAnimState()
-        inst.entity:AddSoundEmitter()
-        inst.entity:AddMiniMapEntity()
-        inst.entity:AddNetwork()
-        inst.entity:AddLightWatcher()
+	inst.entity:AddTransform()
+	inst.entity:AddAnimState()
+	inst.entity:AddSoundEmitter()
+	inst.entity:AddMiniMapEntity()
+	inst.entity:AddNetwork()
+	inst.entity:AddLightWatcher()
 
+	inst.MiniMapEntity:SetIcon("pitcher.tex")
 
-        inst.MiniMapEntity:SetIcon("pitcher.tex")
+	inst.AnimState:SetBank("pitcher")
+	inst.AnimState:SetBuild("pitcher")
+	inst.AnimState:PlayAnimation("swinglong", true)
 
-        inst.AnimState:SetBank("pitcher")
-        inst.AnimState:SetBuild("pitcher")
-        inst.AnimState:PushAnimation("swinglong")
-        --inst:AddTag("CLASSIFIED")
-        inst:AddTag("pitcherplant")
+	inst:AddTag("pitcherplant")
 
+	inst.entity:SetPristine()
 
-        if common_postinit ~= nil then
-            common_postinit(inst)
-        end
+	if not TheWorld.ismastersim then
+		return inst
+	end
 
-        inst.entity:SetPristine()
+	inst:AddComponent("childspawner")
+	inst.components.childspawner.childname = "fruitbat"
+	inst.components.childspawner:SetMaxChildren(1)
+	inst.components.childspawner:SetRegenPeriod(60 * 8)
+	inst.components.childspawner:SetSpawnPeriod(30)
+	inst.components.childspawner:SetSpawnedFn(onspawnbat)
+	inst.components.childspawner.allowwater = true
 
-        if not TheWorld.ismastersim then
-            return inst
-        end
+	inst:ListenForEvent("childgoinghome", onchildgoinghome)
 
-        ---------------------
+	inst:AddComponent("inspectable")
+	inst:AddComponent("lootdropper")
+	inst.components.lootdropper:SetChanceLootTable("pitcherplant")
 
-        inst:ListenForEvent("childgoinghome", onchildgoinghome)
-        -------------------
+	inst.count = 0
+	inst.OnSave = onsave
+	inst.OnLoad = onload
 
-        inst:AddComponent("childspawner")
-        inst.components.childspawner.childname = "fruitbat"
-        inst.components.childspawner.allowwater = true
-		
-		inst:AddComponent("spawner")
-		inst.components.spawner:Configure("fruitbat", 60*8)
-		inst.components.spawner:SetWaterSpawning(true, true)
-		inst.components.spawner:CancelSpawning()
+	inst:DoTaskInTime(0, OnInit)
 
-        if TheWorld.state.isday and not TheWorld.state.iswinter then
-            inst.components.childspawner:StartSpawning()
-        end
-
-        inst:DoTaskInTime(0, OnInit)
-
-        inst:AddComponent("inspectable")
-        inst.components.childspawner:SetSpawnedFn(onspawnbat)
-        inst:AddComponent("lootdropper")
-        inst.components.lootdropper:SetChanceLootTable("pitcherplant")
-        MakeLargePropagator(inst)
-        inst.count = 0
-        inst.OnSave = onsave
-        inst.OnLoad = onload
-        --inst:DoTaskInTime(0,function(inst) inst:Remove() end)
-        return inst
-    end
-
-    return Prefab(name, fn, assets, prefabs)
+	return inst
 end
 
-
-return MakeBeebox("pitcherplant")
+return Prefab("pitcherplant", fn, assets, prefabs)
