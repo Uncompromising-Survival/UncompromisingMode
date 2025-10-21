5c5ce16a6156efdb5d400540d20fe0dd1ad2ab09
CarlosBraw <91644972+CarlosBraw@users.noreply.github.com>
2025-10-21 19:59:40 -0400

    Forgot to put them components in here for wixie_postinit whoops

--- init/init_postinit.lua
+++ init/init_postinit.lua
@@ -264,8 +264,10 @@ if GetModConfigData("wixie_walter") then
         "healer",         -- Walter gets a 50% bonus from healing items, over time. works on companions too.
         "bufferedaction", -- This handles wixie sending an rpc with the mouse pointer click location
         "wobypicking",    -- This reroutes the pickup action and pickable component to add items to wobys container instead of a nil inventory
-        "dryer"           -- This reroutes the dryer harvest action to add items to a container instead of a nil inventory
-
+        "dryer",           -- This reroutes the dryer harvest action to add items to a container instead of a nil inventory
+        "channelcastable", -- Just a dirty wrap of the component to stop it from removing the channelcastable component from her weapon when unequipped
+        "combat", -- Avoid using the weapon when shoving.
+        "weapon" -- Avoid using the weapon when shoving.
     }
 
     for k, v in ipairs(wixie_prefabs) do
