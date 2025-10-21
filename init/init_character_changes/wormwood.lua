0350a2aeaddbdfc445f9b56e00ed39c1ee26a748
CarlosBraw <91644972+CarlosBraw@users.noreply.github.com>
2025-10-20 23:39:35 -0400

    Wormwood VetCurse even more stuff

--- init/init_character_changes/wormwood.lua
+++ init/init_character_changes/wormwood.lua
@@ -231,16 +231,13 @@ if env.GetModConfigData("wormwood_photosynthesis") then
             end
         end
 
-        local function OnHealthDeltaCurse (inst)
+        local function OnHealthDeltaCurse(data)
             local debuffable = inst.components.debuffable
-            if inst:HasTag("vetcurse_wormwood") and debuffable then
-                local healingitems = {
-                    "healingsalve",
-                    "bandage",
-                }
-                for i, v in pairs(healingitems) do
-                    if debuffable:HasDebuff("healthregenbuff_vetcurse_"..v) then
-                        debuffable:RemoveDebuff("healthregenbuff_vetcurse_"..v)
+            if inst:HasTag("vetcurse_wormwood") and debuffable and data.amount < 0 then
+                local debuffs = debuffable.debuffs
+                for i, v in pairs(debuffs) do
+                    if string.sub(i, 1, 24) == "healthregenbuff_vetcurse" or i == "confighealbuff" or i == "compostheal_buff" or i == "tillweedsalve_buff" then
+                        debuffable:RemoveDebuff(i)
                     end
                 end
             end
@@ -256,8 +253,9 @@ if env.GetModConfigData("wormwood_photosynthesis") then
 
         inst.UpdateBloomStage = inst.components.bloomness.onlevelchangedfn --not sure if this is needed but Wormwood also uses UpdateBloomStage for this too so might as well update this
 
-        inst:ListenForEvent("healthdelta", function(inst)
+        inst:ListenForEvent("healthdelta", function(inst, data)
             skilltreemovespeed(inst)
+            OnHealthDeltaCurse(data)
         end)
 		
 		
