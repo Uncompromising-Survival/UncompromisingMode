dcfc3419db6aa6f87a84b541656dadd11a72c8c8
CarlosBraw <91644972+CarlosBraw@users.noreply.github.com>
2025-10-26 05:07:31 -0400

    Honey Poultice & Sweetflower Wrap HoT fix, Flame Repellent & Slimy Salve HoT heal buff, and various Filter organization changes

--- postinit/prefabs/bandage.lua
+++ postinit/prefabs/bandage.lua
@@ -2,14 +2,20 @@ local env = env
 GLOBAL.setfenv(1, GLOBAL)
 
 local function OnUse(inst, target)
-	if target.components.debuffable ~= nil and target.components.health ~= nil and not target.components.health:IsDead() then
-		target.configheal = 10
-		target.components.debuffable:AddDebuff("confighealbuff", "confighealbuff")
+	if target.components.debuffable and target.components.health and not target.components.health:IsDead() then
+		target:AddDebuff("confighealbuff_"..inst.prefab, "confighealbuff", {time = 10})
 	end
 end
 
 
 env.AddPrefabPostInit("bandage", function(inst)
+	if not TheWorld.ismastersim then
+		return
+	end
+	inst.components.healer.onhealfn = OnUse
+end)
+
+env.AddPrefabPostInit("healingsalve_acid", function(inst)
 	if not TheWorld.ismastersim then
 		return
 	end
