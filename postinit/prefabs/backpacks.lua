be5de1bb791bf227982ba714ad64d9152edc7fa0
ShyNuke <131507193+ShyNuke@users.noreply.github.com>
2025-11-05 00:35:32 -0300

    Fix for backpacks accepting stuff inside chests and inventory

--- postinit/prefabs/backpacks.lua
+++ postinit/prefabs/backpacks.lua
@@ -22,10 +22,10 @@ end
 env.AddPrefabPostInitAny(function(inst)
 	if inst:HasTag("backpack") and inst.components.container and inst.components.inventoryitem then
 		PocketsForDays(inst)
+		inst:AddTag("pocketbackpack")		
+		if inst.components.burnable ~= nil and inst.components.fuel == nil then
+			inst:AddComponent("fuel")
+			inst.components.fuel.fuelvalue = TUNING.LARGE_FUEL
+		end	
 	end
-	
-	if inst.components.burnable ~= nil and inst.components.fuel == nil then
-		inst:AddComponent("fuel")
-		inst.components.fuel.fuelvalue = TUNING.LARGE_FUEL
-	end	
 end)
