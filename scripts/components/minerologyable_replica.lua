3b15b214dd800edf539450d88216dafe01f400d0
AtobaAzul <atoba.azul.radical@gmail.com>
2025-11-06 17:03:14 -0300

    gemology gem durability, part 1

--- scripts/components/minerologyable_replica.lua
+++ scripts/components/minerologyable_replica.lua
@@ -1,7 +1,15 @@
 local Minerologyable = Class(function(self, inst)
     self.inst = inst
     self._enchantnum = net_smallbyte(inst.GUID, "minerologyable._enchantnum")
-	self._chilling = net_smallbyte(inst.GUID, "minerologyable._chilling")
+    self._chilling = net_smallbyte(inst.GUID, "minerologyable._chilling")
+    self._durability = net_float(inst.GUID, "minerologyable._durability", "minerologyable._durabilitydirty")
+
+    inst:ListenForEvent("minerologyable._durabilitydirty", function(inst)
+        inst:PushEvent("gemdurabilitychanged")
+        if inst._parent ~= nil then
+            inst._parent:PushEvent("gemdurabilitychanged")
+        end
+    end)
 end)
 
 function Minerologyable:SetEnchantClient(num)
@@ -9,17 +17,25 @@ function Minerologyable:SetEnchantClient(num)
 end
 
 function Minerologyable:SetChillingClient(chilling)
-	local inst = self.inst 
-	if chilling then
-		if not inst:HasTag("show_spoilage") then
-			inst:AddTag("show_spoilage")
-			inst:AddTag("icebox_valid")
-		end
-	elseif inst:HasTag("show_spoilage") then
-		inst:RemoveTag("show_spoilage")
-		inst:RemoveTag("icebox_valid")
-	end
-	
+    local inst = self.inst
+    if chilling then
+        if not inst:HasTag("show_spoilage") then
+            inst:AddTag("show_spoilage")
+            inst:AddTag("icebox_valid")
+        end
+    elseif inst:HasTag("show_spoilage") then
+        inst:RemoveTag("show_spoilage")
+        inst:RemoveTag("icebox_valid")
+    end
+end
+
+function Minerologyable:SetDurabilityClient(durability)
+    print("replica durability changed")
+    self._durability:set(durability)
+end
+
+function Minerologyable:GetDurabilityClient()
+    return self._durability:value()
 end
 
 return Minerologyable
