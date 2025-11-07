379122115e8905f7a3ad5b3c3c35189f5e17e9d2
Anomen77 <anomen77@gmail.com>
2025-11-08 00:59:24 +0100

    Removed shadow song

--- init/init_recipes/recipe_changes.lua
+++ init/init_recipes/recipe_changes.lua
@@ -281,9 +281,9 @@ if TUNING.DSTU.WOLFGANG_HUNGERMIGHTY then
 end
 
 if TUNING.DSTU.WATHGRITHR_REWORK == 1 then
-    --AllRecipes["battlesong_shadowaligned"] = nil
+    AllRecipes["battlesong_shadowaligned"] = nil
     --AllRecipes["battlesong_lunaraligned"] = nil
-    AllRecipes["battlesong_shadowaligned"].builder_skill ="wathgrithr_allegiance_lunar"  
+    --AllRecipes["battlesong_shadowaligned"].builder_skill ="wathgrithr_allegiance_lunar"  
     AllRecipes["battlesong_lunaraligned"].builder_skill ="wathgrithr_allegiance_lunar"  
 end
 
