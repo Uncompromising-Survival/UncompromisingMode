56ece2f073df7dfd946087fe56983d63f3698878
Anomen77 <anomen77@gmail.com>
2025-11-07 20:23:45 +0100

    Added comments

--- init/init_skilltreeimports.lua
+++ init/init_skilltreeimports.lua
@@ -1,4 +1,4 @@
--- ADD ALL THE SKILL THREE IMAGES HERE
+-- ADD ALL THE SKILL THREE BACKGROUND IMAGES HERE
 
 local OldGetSkilltreeBG = GLOBAL.GetSkilltreeBG
 function GLOBAL.GetSkilltreeBG(imagename, ...)
@@ -15,10 +15,15 @@ function GLOBAL.GetSkilltreeBG(imagename, ...)
     end
 end
 
+-- IMPORT ALTERNATE ICONS HERE
+-- RegisterSkilltreeIconsAtlas("name_of_the_atlas", "name_of_the_skill")
+
+-- Wortox
 RegisterSkilltreeIconsAtlas("images/wortox_lunar_stealer.xml", "wortox_lunar_stealer.tex")
 RegisterSkilltreeIconsAtlas("images/wortox_lunar_summoner.xml", "wortox_lunar_summoner.tex")
 RegisterSkilltreeIconsAtlas("images/wortox_shadow_weaver.xml", "wortox_shadow_weaver.tex")
 
+-- Wigfrid
 RegisterSkilltreeIconsAtlas("images/wathgrithr_rework_skilltree.xml", "wathgrithr_arsenal_shield_2.tex")
 RegisterSkilltreeIconsAtlas("images/wathgrithr_rework_skilltree.xml", "wathgrithr_allegiance_shadow.tex")
 RegisterSkilltreeIconsAtlas("images/wathgrithr_rework_skilltree.xml", "wathgrithr_arsenal_spear_1.tex")
