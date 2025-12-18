bda6590b753587d4641236abb4240230b3c8c418
AtobaAzul <atoba.azul.radical@gmail.com>
2025-12-18 20:39:24 -0300

    hotfix ver

--- modinfo.lua
+++ modinfo.lua
@@ -7,7 +7,7 @@ end
 
 --RELEASE.MAJOR.MINOR.FIX
 --axe, remove/add a comma VVV when you do this patch to keep the version number for playtester versions. Also, increase the beta version number, NOT the main one.
-local _version = "1.6.0.0 Beta v2.2.0"
+local _version = "1.6.0.0 Beta v2.2.1"
 
 description = [[
     󰀔 [ "On The Rocks!"  (Beta ver: v]] .. _version .. [[)]
