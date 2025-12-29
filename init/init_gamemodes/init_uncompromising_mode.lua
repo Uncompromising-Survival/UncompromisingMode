d627f7d7626860a151108ab1cee5ba99011f3f14
Anomen77 <anomen77@gmail.com>
2025-12-30 00:44:08 +0100

    THE BIG STRING MIGRATION
    Moved everything string related to init_strings

--- init/init_gamemodes/init_uncompromising_mode.lua
+++ init/init_gamemodes/init_uncompromising_mode.lua
@@ -17,41 +17,42 @@ modimport("init/init_weather/init_dragonfly_bait")
 --modimport("init/init_food/init_stuffed")
 --Currently shelved due to hunger upvalue return error
 
+-- Needs to go up here, other files will try to reference it
+modimport("init/init_tuning")
+
+
 --	[ 	Import Names and Descriptions	]	--
-modimport("init/init_names")
-modimport("init/init_bonusdescriptors")
-modimport("init/init_descriptions/generic")
-modimport("init/init_descriptions/willow")
-modimport("init/init_descriptions/wolfgang")
-modimport("init/init_descriptions/wendy")
-modimport("init/init_descriptions/wx78")
-modimport("init/init_descriptions/wickerbottom")
-modimport("init/init_descriptions/woodie")
-modimport("init/init_descriptions/wes")
-modimport("init/init_descriptions/waxwell")
-modimport("init/init_descriptions/wathgrithr")
-modimport("init/init_descriptions/webber")
-modimport("init/init_descriptions/winona")
-modimport("init/init_descriptions/wortox")
-modimport("init/init_descriptions/wormwood")
-modimport("init/init_descriptions/warly")
-modimport("init/init_descriptions/wurt")
-modimport("init/init_descriptions/walter")
-modimport("init/init_descriptions/wanda")
-modimport("init/init_descriptions/winky")
-modimport("init/init_descriptions/wathom")
---modimport("init/init_descriptions/skilltreestrings.lua")
---modimport("init/init_descriptions/announcestrings.lua")
---modimport("init/init_descriptions/descriptorstrings.lua")
+modimport("init/init_strings/init_strings")
+modimport("init/init_strings/init_names")
+modimport("init/init_strings/init_tooltips")--load before postinit please!
+modimport("init/init_bonusdescriptors") -- doesn't contain strings
+
+-- Character descriptions
+modimport("init/init_strings/init_descriptions/generic")
+modimport("init/init_strings/init_descriptions/willow")
+modimport("init/init_strings/init_descriptions/wolfgang")
+modimport("init/init_strings/init_descriptions/wendy")
+modimport("init/init_strings/init_descriptions/wx78")
+modimport("init/init_strings/init_descriptions/wickerbottom")
+modimport("init/init_strings/init_descriptions/woodie")
+modimport("init/init_strings/init_descriptions/wes")
+modimport("init/init_strings/init_descriptions/waxwell")
+modimport("init/init_strings/init_descriptions/wathgrithr")
+modimport("init/init_strings/init_descriptions/webber")
+modimport("init/init_strings/init_descriptions/winona")
+modimport("init/init_strings/init_descriptions/wortox")
+modimport("init/init_strings/init_descriptions/wormwood")
+modimport("init/init_strings/init_descriptions/warly")
+modimport("init/init_strings/init_descriptions/wurt")
+modimport("init/init_strings/init_descriptions/walter")
+modimport("init/init_strings/init_descriptions/wanda")
+modimport("init/init_strings/init_descriptions/winky")
+modimport("init/init_strings/init_descriptions/wathom")
 
 
 
 --	[ 		Number Tuning and PostInits		]	--
-
-modimport("init/init_tuning")
-modimport("init/init_tooltips")--load before postinit please!
 modimport("init/init_postinit")
-modimport("init/init_strings")
 modimport("init/init_actions")
 modimport("init/init_containers")
 modimport("init/init_batterypower")
