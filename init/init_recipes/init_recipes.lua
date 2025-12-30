257019f45da2a0fd34afb4f919016165b856a283
Anomen77 <anomen77@gmail.com>
2025-12-30 22:30:20 +0100

    Moved recipe strings

--- init/init_recipes/init_recipes.lua
+++ init/init_recipes/init_recipes.lua
@@ -82,7 +82,6 @@ AddPrototyperDef(
 )
 
 modimport("init/init_recipes/recipes")
-modimport("init/init_recipes/recipe_strings")
 modimport("init/init_recipes/recipe_changes")
 if GetModConfigData("wixie_walter") then
     modimport("init/init_recipes/recipes_wixie")
