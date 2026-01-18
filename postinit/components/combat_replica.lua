bbfc4037b422f94ddde83a6ce2e090a675d9b0f8
Maximum101 <maxisawesome103@gmail.com>
2026-01-18 10:47:23 -0500

    Depth Eel forking lightning check.

--- postinit/components/combat_replica.lua
+++ postinit/components/combat_replica.lua
@@ -5,15 +5,16 @@ local function FindBeeQueen(inst)
     return inst.prefab == "beequeen"
 end
 
-local Combat = require("components/combat_replica")--postinits do not seem to work.
-
-local _IsAlly = Combat.IsAlly
-function Combat:IsAlly(guy, ...)
-    if guy.prefab == "um_beeguard_blocker" and FindEntity(guy, 30, FindBeeQueen) then
-        return true
-    --elseif guy.prefab == "ancient_trepidation" and not guy:HasTag("hostile") then
-    --    return true
+env.AddClassPostConstruct("components/combat_replica", function(self)
+    local _IsAlly = self.IsAlly
+    function self:IsAlly(guy, ...)
+        if guy.prefab == "um_beeguard_blocker" and FindEntity(guy, 30, FindBeeQueen) then
+            return true
+        --elseif guy.prefab == "ancient_trepidation" and not guy:HasTag("hostile") then
+        --    return true
+        end
+        -- Prevents lightning from forking from a Depth Eel's target to other Depth Worms.
+        if self:GetTarget() ~= guy and self.inst:HasTag("shockworm") and guy:HasTag("worm") then return true end
+        return _IsAlly(self, guy, ...)
     end
-
-    return _IsAlly(self, guy, ...)
-end
+end)
--- postinit/components/inventoryitem_replica.lua
+++ postinit/components/inventoryitem_replica.lua
@@ -1,7 +1,7 @@
 local env = env
 GLOBAL.setfenv(1, GLOBAL)
 
-env.AddClassPostConstruct("components/inventoryitem_replica", function(self, inst)
+env.AddClassPostConstruct("components/inventoryitem_replica", function(self)
     local _SetPickupPos = self.SetPickupPos
     function self:SetPickupPos(pos, ...)
         if self.classified then
