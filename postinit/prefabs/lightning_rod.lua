910d47e793e7947ad5d82eb24b83ccbc5f1be783
Maximum101 <maxisawesome103@gmail.com>
2026-02-15 13:39:04 -0500

    Update lightning_rod.lua

--- postinit/prefabs/lightning_rod.lua
+++ postinit/prefabs/lightning_rod.lua
@@ -1,5 +1,6 @@
 local env = env
 GLOBAL.setfenv(1, GLOBAL)
+local UpvalueHacker = require("tools/upvaluehacker")
 
 env.AddPrefabPostInit("lightning_rod", function(inst)
 	if not TheWorld.ismastersim then
