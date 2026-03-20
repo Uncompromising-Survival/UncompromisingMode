d406c311a9f935c8ccab251493d5adb62030a759
AtobaAzul <atoba.azul.radical@gmail.com>
2026-03-20 20:06:06 -0300

    update update check, disable WX changes when beta

--- init/init_tuning.lua
+++ init/init_tuning.lua
@@ -348,7 +348,7 @@ TUNING.DSTU = {
     ANNOUNCE_BASESTATUS = GetModConfigData("announce_basestatus"),
     EYEBRELLAREWORK = GetModConfigData("eyebrellarework"),
     -- More Config
-    UPDATE_CHECK = CurrentRelease.GreaterOrEqualTo("R40_PEARLMAS"), -- REMEMBER TO ALWAYS UPDATE THIS WITH NEW BETAS.
+    UPDATE_CHECK = CurrentRelease.GreaterOrEqualTo("R41_ST_WX78"), -- REMEMBER TO ALWAYS UPDATE THIS WITH NEW BETAS.
     POCKET_POWERTRIP = GetModConfigData("pocket_powertrip_"),
     WINTER_BURNING = GetModConfigData("winter_burning"),
     BUTTERFLYWINGS_NERF = GetModConfigData("butterflywings_nerf"),
