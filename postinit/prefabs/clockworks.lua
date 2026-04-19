35141fb57ca065452da0d02c33dae70e0c3837d7
Anomen77 <anomen77@gmail.com>
2026-04-19 12:35:03 +0200

    Swapped trader to action

--- postinit/prefabs/clockworks.lua
+++ postinit/prefabs/clockworks.lua
@@ -73,12 +73,15 @@ end
 TUNING.CLOCKWORK_HEALTH_REGEN_PERIOD = 60
 TUNING.CLOCKWORK_HEALTH_REGEN = 0
 
+--[[
 local function ShouldAcceptGears(inst, item, giver)
 	if not inst.components.follower:GetLeader() then
 		return false
 	end
 
-	return item.prefab == "gears"
+	return 	item.prefab == "gears" --and 
+			--inst.components.follower ~= nil and 
+			--inst.components.follower.leader == giver
 end
 
 local function OnAcceptGears(inst, giver, item)
@@ -102,12 +105,63 @@ local function MakeRepairable(inst)
 
 	inst.components.trader:SetAcceptTest(ShouldAcceptGears)
 	inst.components.trader.onaccept = OnAcceptGears
-	inst.components.trader.acceptnontradable = true
+	--inst.components.trader.acceptnontradable = true
 end
 
 for i, v in ipairs(clockworks) do
 	env.AddPrefabPostInit(v, function(inst)
 		MakeRepairable(inst)
+
 	end)
+end]]
+
+local GIVEGEAR = Action({ priority = 1 })
+GIVEGEAR.id = "GIVEGEAR"
+GIVEGEAR.str = "Repair"
+GIVEGEAR.fn = function(act)
+    local target = act.target
+    local item = act.invobject
+
+    if target ~= nil and item ~= nil and item.prefab == "gears" then
+
+		target.components.health:DoDelta(1000)
+
+		target.SoundEmitter:PlaySound("dontstarve/common/telebase_gemplace")
+
+                -- Handle stack properly
+        if item.components.stackable ~= nil then
+            local single = item.components.stackable:Get(1)
+            if single ~= nil then
+                single:Remove()
+            end
+        else
+            item:Remove()
+        end
+        return true
+    end
+
+    return false
 end
+
+env.AddAction(GIVEGEAR)
+
+env.AddComponentAction("USEITEM", "inventoryitem", function(inst, doer, target, actions, right)
+	
+    if target ~= nil
+        and inst.prefab == "gears"
+		and target:HasTag("chess")
+		--and target._hasleader ~= nil
+        --and target._hasleader:value()  then
+		and target.replica.health ~= nil then
+		--and target.replica.health:IsHurt() then
+
+        table.insert(actions, ACTIONS.GIVEGEAR)
+    end
+end)
+
+env.AddStategraphActionHandler("wilson", ActionHandler(ACTIONS.GIVEGEAR, "doshortaction"))
+env.AddStategraphActionHandler("wilson_client", ActionHandler(ACTIONS.GIVEGEAR, "doshortaction"))
+
+env.AddComponentAction("SCENE", "inspectable", function() end)
+
 
