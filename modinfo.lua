43e07922255980743e5ff3d22fb3672e79672baa
Maximum101 <maxisawesome103@gmail.com>
2026-04-20 13:48:40 -0400

    ver inc

--- modinfo.lua
+++ modinfo.lua
@@ -6,7 +6,7 @@ if not folder_name:find("workshop-") then
 end
 
 --RELEASE.MAJOR.MINOR.FIX
-local _version = "1.5.3.0"
+local _version = "1.5.3.1"
 description = [[
 󰀔 [ Version: ]] .. _version .. [[ - "Wixie, Walter, Woby and Winky!" ]
 
