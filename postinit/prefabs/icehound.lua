eb512dbfd082c25a90e726b08b3e87782b04dff1
Maximum101 <maxisawesome103@gmail.com>
2026-04-25 15:08:18 -0400

    Update icehound.lua

--- postinit/prefabs/icehound.lua
+++ postinit/prefabs/icehound.lua
@@ -4,35 +4,25 @@ GLOBAL.setfenv(1, GLOBAL)
 
 local function OnHitOtherFreeze(inst, data)
     local other = data.target
-    if other ~= nil and not (other.components.health ~= nil and other.components.health:IsDead()) then
-		if other ~= nil then
-			if not (other.components.health ~= nil and other.components.health:IsDead()) then
-				if other.components.freezable ~= nil and not other.components.freezable:IsFrozen() and not other.sg:HasStateTag("frozen") then
-					other.components.freezable:AddColdness(2)
-				end
-				if other.components.temperature ~= nil then
-					local mintemp = math.max(other.components.temperature.mintemp, 0)
-					local curtemp = other.components.temperature:GetCurrent()
-					if mintemp < curtemp then
-						other.components.temperature:DoDelta(math.max(-5, mintemp - curtemp))
-					end					
-				end
-			end
-			if other.components.freezable ~= nil then
-				other.components.freezable:SpawnShatterFX()
-			end
-		end
-	end
+    if other and not (other.components.health and other.components.health:IsDead()) then
+        if other.components.freezable and not other.components.freezable:IsFrozen() and not other.sg:HasStateTag("frozen") then
+            other.components.freezable:AddColdness(2)
+            other.components.freezable:SpawnShatterFX()
+        end
+        if other.components.temperature then
+            local mintemp = math.max(other.components.temperature.mintemp, 0)
+            local curtemp = other.components.temperature:GetCurrent()
+            if mintemp < curtemp then
+                other.components.temperature:DoDelta(math.max(-5, mintemp - curtemp))
+            end                    
+        end
+    end
 end
 
 env.AddPrefabPostInit("icehound", function(inst)
-    if not TheWorld.ismastersim then
-        return
-    end
+    if not TheWorld.ismastersim then return end
 
     if TUNING.DSTU.FROSTBITEHOUNDS then
-        if inst.components.combat then
-            inst:ListenForEvent("onhitother", OnHitOtherFreeze)
-        end
+        inst:ListenForEvent("onhitother", OnHitOtherFreeze)
     end
 end)
