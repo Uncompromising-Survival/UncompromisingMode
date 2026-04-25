a13860db932dafa18cf7c046a392aa5fd84690bf
Maximum101 <maxisawesome103@gmail.com>
2026-04-25 16:14:06 -0400

    Update rat_whip.lua

--- scripts/prefabs/rat_whip.lua
+++ scripts/prefabs/rat_whip.lua
@@ -17,32 +17,22 @@ local function onunequip(inst, owner)
 end
 
 local function HungerScalingCheck(attacker)
-	if attacker ~= nil and attacker.components.hunger ~= nil then
-		local value = math.max(attacker.components.hunger:GetPercent(), 0.25)
-		return 3 * value, value
-	end
-
-	return 0, 0
+    if attacker and attacker.components.hunger then
+        local value = math.max(attacker.components.hunger:GetPercent(), .25)
+        return 3 * value, value
+    end
+    return 0, 0
 end
 
 local function WhipDamageCheck(inst, attacker, target)
-	local HungerScaling = HungerScalingCheck(attacker)
-	local BonusDamage = (34 / 3) * HungerScaling
-	return 34 + BonusDamage
+    local HungerScaling = HungerScalingCheck(attacker)
+    local BonusDamage = (34 / 3) * HungerScaling
+    return 34 + BonusDamage
 end
 
-local function onattack(inst, attacker, target, naughtlock)
-    if target ~= nil and target:IsValid() then
-		local naughtlockmult = 1
-		
-		if naughtlock ~= nil then
-			naughtlockmult = 2
-		end
-		
-        local chance = TUNING.WHIP_SUPERCRACK_CREATURE_CHANCE
-
+local function onattack(inst, attacker, target)
+    if target and target:IsValid() then
         local snap = SpawnPrefab("impact")
-
         local x, y, z = inst.Transform:GetWorldPosition()
         local x1, y1, z1 = target.Transform:GetWorldPosition()
         local angle = -math.atan2(z1 - z, x1 - x)
@@ -51,49 +41,43 @@ local function onattack(inst, attacker, target, naughtlock)
 
         --impact sounds normally play through comabt component on the target
         --whip has additional impact sounds logic, which we'll just add here
-		
-		if attacker ~= nil and attacker.components.hunger ~= nil then
-			local value = attacker.components.hunger:GetPercent()
-			local hunger = 3
-			
-			if value < 0.25 then
-				value = 0.25
-			end
-			
-			local scalingvalue = hunger * value
-			
-			snap.Transform:SetScale(scalingvalue / 1.25, scalingvalue / 1.25, scalingvalue / 1.25)
-			
-            if target.SoundEmitter ~= nil then
-				target.SoundEmitter:PlaySound("dontstarve/common/whip_small")
+
+        if attacker and attacker.components.hunger then
+            local value = attacker.components.hunger:GetPercent()
+            local hunger = 3
+
+            if value < .25 then value = .25 end
+
+            local scalingvalue = hunger * value
+
+            snap.Transform:SetScale(scalingvalue / 1.25, scalingvalue / 1.25, scalingvalue / 1.25)
+
+            if target.SoundEmitter then target.SoundEmitter:PlaySound("dontstarve/common/whip_small") end
+
+            if attacker.components.hunger:GetPercent() > 0 then
+                local burnrate = attacker.components.hunger.burnratemodifiers:Get()
+                attacker.components.hunger:DoDelta(-scalingvalue * burnrate)
             end
-			
-			if attacker.components.hunger ~= nil and attacker.components.hunger:GetPercent() > 0 then
-				local burnrate = attacker.components.hunger.burnratemodifiers:Get()
-				attacker.components.hunger:DoDelta((-scalingvalue * burnrate) / naughtlockmult)
-			end
-			local uses1 = 1
-			local uses2 = 1
-			if attacker ~= nil and attacker:IsValid() and attacker.components.efficientuser ~= nil then
-				uses1 = 1 * (attacker.components.efficientuser:GetMultiplier(ACTIONS.ATTACK) or 1) * value
-			end
-			
-			uses2 = 1 * value
-			
-			local uses = uses1 + uses2
-			
-			if naughtlock == nil then 
-				inst.components.fueled:DoDelta(-uses)
-			end
+
+            local uses1 = 1
+            local uses2 = 1
+            if attacker.components.efficientuser then
+                uses1 = 1 * (attacker.components.efficientuser:GetMultiplier(ACTIONS.ATTACK) or 1) * value
+            end
+
+            uses2 = 1 * value
+
+            local uses = uses1 + uses2
+
+            inst.components.fueled:DoDelta(-uses)
         end
     end
 end
 
 local function on_uses_finished(inst)
-    if inst.components.inventoryitem.owner ~= nil then
+    if inst.components.inventoryitem.owner then
         inst.components.inventoryitem.owner:PushEvent("toolbroke", { tool = inst })
     end
-
     inst:Remove()
 end
 
@@ -115,8 +99,8 @@ local function fn()
     --weapon (from weapon component) added to pristine state for optimization
     inst:AddTag("weapon")
     inst:AddTag("donotautopick")
-    
-    MakeInventoryFloatable(inst, "med", nil, 0.9)
+
+    MakeInventoryFloatable(inst, "med", nil, .9)
 
     inst.entity:SetPristine()
 
@@ -125,20 +109,20 @@ local function fn()
     end
 
     inst:AddComponent("weapon")
-	inst.components.weapon:SetDamage(WhipDamageCheck)
+    inst.components.weapon:SetDamage(WhipDamageCheck)
     inst.components.weapon:SetRange(TUNING.WHIP_RANGE)
     inst.components.weapon:SetOnAttack(onattack)
-	
+
     --[[inst:AddComponent("finiteuses")
     inst.components.finiteuses:SetMaxUses(TUNING.WHIP_USES)
     inst.components.finiteuses:SetUses(TUNING.WHIP_USES)
     inst.components.finiteuses:SetOnFinished(inst.Remove)]]
-	
+
     inst:AddComponent("fueled")
     --inst.components.fueled:SetSectionCallback(onfuelchange)
     inst.components.fueled:InitializeFuelLevel(200)
     inst.components.fueled:SetDepletedFn(on_uses_finished)
-	inst.components.fueled.accepting = false
+    inst.components.fueled.accepting = false
     --inst.components.fueled:SetFirstPeriod(TUNING.TURNON_FUELED_CONSUMPTION, TUNING.TURNON_FULL_FUELED_CONSUMPTION)
 
     inst:AddComponent("inspectable")
