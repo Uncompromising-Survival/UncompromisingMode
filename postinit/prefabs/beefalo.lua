a2a8d029ba55b5c2acd8bf0091f3b2893ff60313
TheAxeSwingingSmasher <56132394+TheAxeSwingingSmasher@users.noreply.github.com>
2026-04-25 10:21:23 -0500

    Megapatch: Beefalo brush buff

--- postinit/prefabs/beefalo.lua
+++ postinit/prefabs/beefalo.lua
@@ -11,6 +11,18 @@ local function OnRiderChanged(inst, data)
 	end
 end
 
+local function OnBrushed(inst)
+    if inst.components.health:IsDead() then
+        return
+    end
+
+    if numprizes > 0 and inst.components.domesticatable ~= nil then -- Ratios suggested to improve by 夢我夢中
+        inst.components.domesticatable:DeltaDomestication(TUNING.BEEFALO_DOMESTICATION_BRUSHED_DOMESTICATION*(2/1.67), doer)
+        inst.components.domesticatable:DeltaObedience(TUNING.BEEFALO_DOMESTICATION_BRUSHED_OBEDIENCE*(10/4))
+    end
+end
+
+-- Widow drops tusk guaranteed, there has to be some reason to use the tusk if you're using a beefalo, the brush is the most intuitive and should be better than it currently is.
 env.AddPrefabPostInit("beefalo", function(inst)
 	if not TheWorld.ismastersim then
 		return
@@ -19,4 +31,7 @@ env.AddPrefabPostInit("beefalo", function(inst)
 	if TUNING.DSTU.WATHGRITHR_REWORK == 1 then
 		inst:ListenForEvent("riderchanged", OnRiderChanged)
 	end
+	
+	
+	inst.components.brushable:SetOnBrushed(OnBrushed)
 end)
