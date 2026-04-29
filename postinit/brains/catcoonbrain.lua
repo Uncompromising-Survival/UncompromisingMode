04fec6c5284e7f1406290f9439b8f3d9bcae673c
Maximum101 <maxisawesome103@gmail.com>
2026-04-29 19:57:56 -0400

    Update catcoonbrain.lua

--- postinit/brains/catcoonbrain.lua
+++ postinit/brains/catcoonbrain.lua
@@ -18,12 +18,24 @@ local function PlayAction_Inventory(inst)
         CanSteal,
         STEAL_MUST_TAGS, --see entityreplica.lua
         STEAL_CANT_TAGS,
-		PLAY_TAGS)
-    return target ~= nil
-        and BufferedAction(inst, target, ACTIONS.PICKUP)
-    or nil
+        PLAY_TAGS)
+    return target and BufferedAction(inst, target, ACTIONS.PICKUP) or nil
+end
+
+local UpvalueHacker = require("tools/upvaluehacker")
+local CatcoonBrain = require("brains/catcoonbrain")
+
+local _PlayAction = UpvalueHacker.GetUpvalue(CatcoonBrain.OnStart, "PlayAction")
+
+local function FindActionNode(self)
+    for id, node in pairs(self.bt.root.children) do
+        if node.getactionfn and node.getactionfn == _PlayAction then
+            table.insert(self.bt.root.children, id, DoAction(self.inst, PlayAction_Inventory, "steal", true))
+            return
+        end
+    end
 end
 
 env.AddBrainPostInit("catcoonbrain", function(self)
-    table.insert(self.bt.root.children, 7, DoAction(self.inst, PlayAction_Inventory, "steal", true))
+    FindActionNode(self)
 end)
