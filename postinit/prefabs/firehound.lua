abad69b86433e81cac6a8aca3cda8500fd922578
ShyNuke <leoserow123@gmail.com>
2026-04-30 23:32:43 -0300

    Fire Bite Fix

--- postinit/prefabs/firehound.lua
+++ postinit/prefabs/firehound.lua
@@ -4,9 +4,17 @@ GLOBAL.setfenv(1, GLOBAL)
 local function OnHitOtherBurn(inst, data)
     local other = data.target
     if other and not (other.components.health and other.components.health:IsDead()) then
-        if other.components.burnable and not (other.components.rider and other.components.rider:IsRiding()) then
-            other.components.burnable:Ignite(true, inst, inst)
-        end
+		if other.components.rideable ~= nil and other.components.rideable:IsBeingRidden() then
+			local rider = other.components.rideable:GetRider()
+			if rider ~= nil and rider.components.health ~= nil and not rider.components.health:IsDead() and rider.components.burnable ~= nil then
+				rider.components.burnable:Ignite(true, inst, inst)
+			end
+			return
+		end
+		
+		if other.components.burnable ~= nil then
+			other.components.burnable:Ignite(true, inst, inst)
+		end	
     end
 end
 
