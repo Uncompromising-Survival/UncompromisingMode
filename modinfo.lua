bb656c458565be32cc55c5a1347022eb97bf5a33
TheAxeSwingingSmasher <56132394+TheAxeSwingingSmasher@users.noreply.github.com>
2026-05-04 22:09:34 -0500

    Megapatch: Ver Inc

--- modinfo.lua
+++ modinfo.lua
@@ -7,7 +7,7 @@ end
 
 --MAJOR.MINOR.FIX
 --INCREASE THIS VERSION
-local _version = "4.2.1"
+local _version = "4.2.2"
 
 description = [[
     󰀔 ["On The Rocks!" v1.6  (Beta ver: v]] .. _version .. [[)]
