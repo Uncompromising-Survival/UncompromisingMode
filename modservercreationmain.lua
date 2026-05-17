4acc9e5993640f7f1f508ac2f7b0d0909b3fe549
AtobaAzul <atoba.azul.radical@gmail.com>
2026-05-17 20:53:22 -0300

    lower enable sound volume

--- modservercreationmain.lua
+++ modservercreationmain.lua
@@ -8,7 +8,8 @@ local ServerCreationScreen = TheFrontEnd:GetActiveScreen()
 
 if tostring(ServerCreationScreen) == "ServerCreationScreen" then
     staticScheduler:ExecuteInTime(0, function()
-        TheFrontEnd:GetSound():PlaySound("dontstarve/common/teleportato/teleportato_maxwelllaugh")
+        TheFrontEnd:GetSound():PlaySound("dontstarve/common/teleportato/teleportato_maxwelllaugh", "laugh")
+        TheFrontEnd:GetSound():SetVolume("laugh", 0.33)
         TheFrontEnd:GetSound():PlaySound("dontstarve/sanity/creature2/taunt")
     end)
 end
