a4d9b244e634cd867953bc95f6ef4b7c59a6e39d
AtobaAzul <atoba.azul.radical@gmail.com>
2026-05-18 15:02:21 -0300

    ver

--- modinfo.lua
+++ modinfo.lua
@@ -7,7 +7,7 @@ end
 
 --MAJOR.MINOR.FIX
 --INCREASE THIS VERSION
-local _version = "4.3.0"
+local _version = "4.3.1"
 
 description = [[
     󰀔 ["On The Rocks!" v1.6  (Beta ver: v]] .. _version .. [[)]
