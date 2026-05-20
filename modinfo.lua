80a6272c650bdc1a60685990fc91db4a39cbea57
AtobaAzul <atoba.azul.radical@gmail.com>
2026-05-20 13:58:09 -0300

    very hot very warm fix

--- modinfo.lua
+++ modinfo.lua
@@ -7,7 +7,7 @@ end
 
 --MAJOR.MINOR.FIX
 --INCREASE THIS VERSION
-local _version = "5.1.1"
+local _version = "5.1.2"
 
 description = [[
     󰀔 ["On The Rocks!" v1.6  (Beta ver: v]] .. _version .. [[)]
