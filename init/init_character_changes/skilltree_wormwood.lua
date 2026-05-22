7ecc11f187332549ef84bd6c7ad00e2020488b19
TheAxeSwingingSmasher <56132394+TheAxeSwingingSmasher@users.noreply.github.com>
2026-05-22 06:39:02 -0500

    Adjustments to wormwood's skilltree strings and Wacky's silken sack and bundle art

--- init/init_character_changes/skilltree_wormwood.lua
+++ init/init_character_changes/skilltree_wormwood.lua
@@ -6,7 +6,7 @@ local UI_LEFT, UI_RIGHT = -214, 228
 local UI_VERTICAL_MIDDLE = (UI_LEFT + UI_RIGHT) * 0.5
 local UI_TOP, UI_BOTTOM = 176, 20
 local TILE_SIZE, TILE_HALFSIZE = 34, 16
-local SKILLTREESTRINGS = STRINGS.SKILLTREE.WORMWOOD
+local WORMSKILLS = STRINGS.SKILLTREE.WORMWOOD
 
 --------------------------------------------------------------------------------------------------
 
@@ -26,7 +26,7 @@ local function OnSeasonChange(inst, season)
 end
 
 local STRINGS = GLOBAL.STRINGS
-local SKILLTREESTRINGS = STRINGS.SKILLTREE.WORMWOOD
+local WORMSKILLS = STRINGS.SKILLTREE.WORMWOOD
 
 local function BSlapSomeone(inst, data)
 	
@@ -53,8 +53,8 @@ end
 local skills =
 {
 	wormwood_identify_plants2 = { 
-		title = SKILLTREESTRINGS.IDENTIFY_PLANTS_TITLE, --"Seed Sleuth"
-		desc = SKILLTREESTRINGS.IDENTIFY_PLANTS_DESC,
+		title = WORMSKILLS.IDENTIFY_PLANTS_TITLE, --"Seed Sleuth"
+		desc = WORMSKILLS.IDENTIFY_PLANTS_DESC,
 		icon = "wormwood_identify_plants",
 		pos = {(UI_LEFT + UI_RIGHT) * 0.5, UI_BOTTOM},
 
@@ -76,8 +76,8 @@ local skills =
 		defaultfocus = true,
 	},
 	wormwood_blooming_farmrange1 = { -- This is "Farmhand"
-		title = SKILLTREESTRINGS.BLOOMING_FARMRANGE1_TITLE,
-		desc = SKILLTREESTRINGS.BLOOMING_FARMRANGE1_DESC,
+		title = WORMSKILLS.BLOOMING_FARMRANGE1_TITLE,
+		desc = WORMSKILLS.BLOOMING_FARMRANGE1_DESC,
 		icon = "wormwood_blooming_farmrange1",
 		pos = {UI_VERTICAL_MIDDLE - 105, UI_BOTTOM + 10},
 
@@ -95,8 +95,8 @@ local skills =
 	},
 
 	wormwood_bugs = {
-		title = SKILLTREESTRINGS.BUGS_TITLE,
-		desc = SKILLTREESTRINGS.BUGS_DESC,
+		title = WORMSKILLS.BUGS_TITLE,
+		desc = WORMSKILLS.BUGS_DESC,
 		icon = "wormwood_bugs",
 		
 		pos = {UI_VERTICAL_MIDDLE - 105 - 50, UI_BOTTOM + 10},
@@ -109,18 +109,19 @@ local skills =
 	},
 
 
-	wormwood_originator = {
-		title = "Originator",  --"Juicy Berry Crafting" -> Originator - Combine all plant crafting skills.
-		desc = "Wormwood learns to craft many bushes and plants from his body.",
+	wormwood_originator = { -- WHOOPS Wormiest meant "Progenitor", it'll be fixed in the string, I'm not fixing the var name.
+		title = WORMSKILLS.ORIGINATOR_TITLE,  --"Juicy Berry Crafting" -> Originator - Combine all plant crafting skills.
+		desc = WORMSKILLS.ORIGINATOR_DESC,
 		icon = "wormwood_originator",
 		pos = {UI_VERTICAL_MIDDLE - 115 - 60, UI_BOTTOM + 58},
 
 		group = "crafting",
 		tags = {"crafting"},
 	},
+
 	wormwood_sympathetic_blooming = {
-		title = "Sympathetic Bloomer",  --"Reed crafting" -> Sympathetic Blooming: Being bloomed near Cacti will cause them to flower, being bloomed near a mushtree will have them bloom as well. (Latter idea by Axe).
-		desc = "Being bloomed near cactus and mushtrees will eventually spur them to bloom.",
+		title = WORMSKILLS.SYMBLOOMER_TITLE,  --"Reed crafting" -> Sympathetic Blooming: Being bloomed near Cacti will cause them to flower, being bloomed near a mushtree will have them bloom as well. (Latter idea by Axe).
+		desc = WORMSKILLS.SYMBLOOMER_DESC,
 		icon = "wormwood_sympathetic_blooming",
 		pos = {UI_VERTICAL_MIDDLE - 105 - 100, UI_BOTTOM + 10},
 
@@ -130,6 +131,7 @@ local skills =
 			"wormwood_flytrap",
 		},
 	},
+
 	wormwood_flytrap = {
 		title = "Flytrap",
 		desc = "Grab insects and spores without a net.",
@@ -146,8 +148,8 @@ local skills =
 	},
 
 	wormwood_quick_selffertilizer = {
-		title = SKILLTREESTRINGS.QUICK_SELFFERTILIZER_TITLE,
-		desc = SKILLTREESTRINGS.QUICK_SELFFERTILIZER_DESC,
+		title = WORMSKILLS.QUICK_SELFFERTILIZER_TITLE,
+		desc = WORMSKILLS.QUICK_SELFFERTILIZER_DESC,
 		icon = "wormwood_quick_selffertilizer",
 		pos = {UI_VERTICAL_MIDDLE - 35, UI_BOTTOM + 65},
 
@@ -159,8 +161,8 @@ local skills =
 	},
 
 	wormwood_resilient_crops1 = {
-		title = "Resilient Crops I", --"Mushroom Mastery II" -> Resilient Crops I - Crops planted by wormwood will not get stressed from weeds or debris.
-		desc = "Crops planted by wormwood will not get stressed from weeds or debris.",
+		title = WORMSKILLS.RCROPS_1_TITLE, --"Mushroom Mastery II" -> Resilient Crops I - Crops planted by wormwood will not get stressed from weeds or debris.
+		desc = WORMSKILLS.RCROPS_1_DESC,
 		icon = "wormwood_resilient_crops1",
 		pos = {UI_VERTICAL_MIDDLE - 90, UI_BOTTOM + 95},
 
@@ -172,8 +174,8 @@ local skills =
 		},
 	},
 	wormwood_resilient_crops2 = {
-		title = "Resilient Crops II", --"Mushroom multiplier" -> Resilient Crops II - Wild crops automatically tend themselves.
-		desc = "Wild crops you plant do not need to be tended.",
+		title = WORMSKILLS.RCROPS_2_TITLE, --"Mushroom multiplier" -> Resilient Crops II - Wild crops automatically tend themselves.
+		desc = WORMSKILLS.RCROPS_2_DESC,
 		icon = "wormwood_resilient_crops2",
 		pos = {UI_VERTICAL_MIDDLE - 90, UI_BOTTOM + 145},
 
@@ -184,8 +186,8 @@ local skills =
 		},
 	},
 	wormwood_resilient_crops3 = {
-		title = "Impeccable Crops", --"Mooncap eating" -> "Impeccable crops" Wild crops automatically tend themselves.
-		desc = "Your wild crops will never rot. However, plants left grown will eventually lose their seeds.",
+		title = WORMSKILLS.RCROPS_3_TITLE, --"Mooncap eating" -> "Impeccable crops" Wild crops automatically tend themselves.
+		desc = WORMSKILLS.RCROPS_3_DESC,
 		icon = "wormwood_resilient_crops3",
 		pos = {UI_VERTICAL_MIDDLE - 70, UI_BOTTOM + 190},
 
@@ -193,8 +195,8 @@ local skills =
 		tags = {"crafting"},
 	},
 	wormwood_syrupcrafting = {
-		title = SKILLTREESTRINGS.SYRUPCRAFTING_TITLE,
-		desc = SKILLTREESTRINGS.SYRUPCRAFTING_DESC,
+		title = WORMSKILLS.SYRUPCRAFTING_TITLE,
+		desc = WORMSKILLS.SYRUPCRAFTING_DESC,
 		icon = "wormwood_syrupcrafting",
 		pos = {UI_VERTICAL_MIDDLE - 30, UI_BOTTOM + 125},
 
@@ -208,7 +210,7 @@ local skills =
 	}),
 
 	wormwood_allegiance_count_lock_2 = {
-		desc = SKILLTREESTRINGS.COUNT_LOCK_2_DESC,
+		desc = WORMSKILLS.COUNT_LOCK_2_DESC,
 		pos = {UI_RIGHT - 3.5, UI_TOP - 12},
 		group = "allegiance2",
 		tags = {"allegiance", "lock"},
@@ -219,8 +221,8 @@ local skills =
 	},
 
 	wormwood_allegiance_lunar_plant_gear_1 = {
-		title = SKILLTREESTRINGS.LUNAR_GEAR_1_TITLE, --"Lunar Guardian I": Armors worn by Wormwood will have reduced durability loss while bloomed, number up to interpretation. (By Thaumoking)
-		desc = "Fuse Bramble Husk and Brightshade Armor into Brambleshade Armor. When attacked, sometimes sieze foes in place. This is always triggered when wearing Brightshade or Brambleshade armors.",
+		title = WORMSKILLS.LUNAR_GEAR_1_TITLE, --"Lunar Guardian I": Armors worn by Wormwood will have reduced durability loss while bloomed, number up to interpretation. (By Thaumoking)
+		desc = WORMSKILLS.LUNAR_GEAR_1_DESC,
 		icon = "wormwood_allegiance_lunar_plant_gear_1",
 		pos = {UI_RIGHT - 3.5, UI_TOP + 25},
 		locks = {"wormwood_allegiance_lock_lunar_2", "wormwood_allegiance_count_lock_2"},
@@ -257,8 +259,8 @@ local skills =
 		},
 	},
 	wormwood_allegiance_lunar_plant_gear_2 = { --"Lunar Guardian II"
-		title = SKILLTREESTRINGS.LUNAR_GEAR_2_TITLE,
-		desc = "Vines appear when using brightshade or glass weaponry.",
+		title = WORMSKILLS.LUNAR_GEAR_2_TITLE,
+		desc = WORMSKILLS.LUNAR_GEAR_2_DESC,
 		icon = "wormwood_allegiance_lunar_plant_gear_2",
 		pos = {UI_RIGHT - 3.5, UI_TOP + 65},
 
@@ -267,8 +269,8 @@ local skills =
 	},
 
 	wormwood_blooming_speed1 = {
-		title = SKILLTREESTRINGS.BLOOMING_SPEED1_TITLE,
-		desc = SKILLTREESTRINGS.BLOOMING_SPEED1_DESC,
+		title = WORMSKILLS.BLOOMING_SPEED1_TITLE,
+		desc = WORMSKILLS.BLOOMING_SPEED1_DESC,
 		icon = "wormwood_blooming_speed1",
 		pos = {UI_VERTICAL_MIDDLE + 105, UI_BOTTOM + 10},
 
@@ -289,8 +291,8 @@ local skills =
 		},
 	},
 	wormwood_blooming_speed2 = {
-		title = SKILLTREESTRINGS.BLOOMING_SPEED2_TITLE,
-		desc = SKILLTREESTRINGS.BLOOMING_SPEED2_DESC,
+		title = WORMSKILLS.BLOOMING_SPEED2_TITLE,
+		desc = WORMSKILLS.BLOOMING_SPEED2_DESC,
 		icon = "wormwood_blooming_speed2",
 		pos = {UI_VERTICAL_MIDDLE + 105 + 50, UI_BOTTOM + 10},
 
@@ -312,8 +314,8 @@ local skills =
 		},
 	},
 	wormwood_blooming_overheatprotection = {
-		title = SKILLTREESTRINGS.BLOOMING_OVERHEATPROTECTION_TITLE,
-		desc = "Gain increased wetness and overheating protection while in full bloom.",
+		title = WORMSKILLS.BLOOMING_OVERHEATPROTECTION_TITLE,
+		desc = WORMSKILLS.BLOOMING_OVERHEATPROTECTION_DESC,
 		icon = "wormwood_blooming_overheatprotection",
 		pos = {UI_VERTICAL_MIDDLE + 165, UI_BOTTOM + 60},
 
@@ -336,8 +338,8 @@ local skills =
 		end,
 	},
 	wormwood_blooming_max_upgrade = {
-		title = SKILLTREESTRINGS.BLOOMING_MAX_UPGRADE_TITLE, --"Flower Power":  "+30% nutrients gained" applies to all nutrients. (Should be done)
-		desc = SKILLTREESTRINGS.BLOOMING_MAX_UPGRADE_DESC,
+		title = WORMSKILLS.BLOOMING_MAX_UPGRADE_TITLE, --"Flower Power":  "+30% nutrients gained" applies to all nutrients. (Should be done)
+		desc = WORMSKILLS.BLOOMING_MAX_UPGRADE_DESC,
 		icon = "wormwood_blooming_speed3",
 		pos = {UI_VERTICAL_MIDDLE + 105 + 100, UI_BOTTOM + 10},
 
@@ -352,8 +354,8 @@ local skills =
 	},
 
 	wormwood_blooming_photosynthesis = {
-		title = "Improved Photosynthesis",
-		desc = SKILLTREESTRINGS.BLOOMING_PHOTOSYNTHESIS_DESC,
+		title = WORMSKILLS.BLOOMING_PHOTOSYNTHESIS_TITLE,
+		desc = WORMSKILLS.BLOOMING_PHOTOSYNTHESIS_DESC,
 		icon = "wormwood_blooming_photosynthesis",
 		pos = {UI_VERTICAL_MIDDLE + 165 + 55, UI_BOTTOM + 60},
 
@@ -369,8 +371,8 @@ local skills =
 	},
 
 	wormwood_prick_adept = {
-		title = "Prick Adept",
-		desc = "Even without a bramble husk, wormwood can safely pick and traverse most prickly plants.",
+		title = WORMSKILLS.PRICK_ADEPT_TITLE,
+		desc = WORMSKILLS.PRICK_ADEPT_DESC,
 		icon = "wormwood_prick_adept",
 		pos = {UI_VERTICAL_MIDDLE + 55, UI_BOTTOM + 45 + TILE_SIZE},
 
@@ -382,8 +384,8 @@ local skills =
 	},
 
 	wormwood_blooming_trapbramble = {
-		title = SKILLTREESTRINGS.BLOOMING_TRAPBRAMBLE_TITLE,
-		desc = "Bramble Traps spread thorns over a larger area. Reset nearby Bramble Traps while fully bloomed.",
+		title = WORMSKILLS.BLOOMING_TRAPBRAMBLE_TITLE,
+		desc = WORMSKILLS.BLOOMING_TRAPBRAMBLE_DESC,
 		icon = "wormwood_blooming_trapbramble",
 		pos = {UI_VERTICAL_MIDDLE + 95, UI_BOTTOM + 115},
 
@@ -396,8 +398,8 @@ local skills =
 	},
 
 	wormwood_armor_bramble = {
-		title = SKILLTREESTRINGS.ARMOR_BRAMBLE_TITLE,
-		desc = SKILLTREESTRINGS.ARMOR_BRAMBLE_DESC,
+		title = WORMSKILLS.ARMOR_BRAMBLE_TITLE,
+		desc = WORMSKILLS.ARMOR_BRAMBLE_DESC,
 		icon = "wormwood_armor_bramble",
 		pos = {UI_VERTICAL_MIDDLE + 137, UI_BOTTOM + 145},
 
@@ -410,8 +412,8 @@ local skills =
 	},
 
 	wormwood_armor_bramble2 = {
-		title = "Bramble Burst",
-		desc = "All bramble husk types trigger a second time when attacked.",
+		title = WORMSKILLS.ARMOR_BRAMBLEBURST_TITLE,
+		desc = WORMSKILLS.ARMOR_BRAMBLEBURST_DESC,
 		icon = "wormwood_armor_bramble2",
 		pos = {UI_VERTICAL_MIDDLE + 120, UI_BOTTOM + 187},
 
@@ -421,8 +423,8 @@ local skills =
 	},
 
 	wormwood_mushroommadness = {
-		title = "Mushroom Madness", --"Berry Bush crafting" -> Mushroom Madness: Combines the Mushroom Planters skills into 1.
-		desc = "Mushrooms you plant grow faster in Mushroom Planters and produce a higher yield. Learn how to plant Mushtrees.",
+		title = WORMSKILLS.MUSHMAD_TITLE, --"Berry Bush crafting" -> Mushroom Madness: Combines the Mushroom Planters skills into 1.
+		desc = WORMSKILLS.MUSHMAD_DESC,
 		icon = "wormwood_mushroomplanter_ratebonus2",
 
 		pos = {UI_VERTICAL_MIDDLE + 43, UI_BOTTOM + 150},
@@ -439,7 +441,7 @@ local skills =
 		group = "allegiance1",
 	}),
 	wormwood_allegiance_count_lock_1 = {
-		desc = SKILLTREESTRINGS.COUNT_LOCK_1_DESC,
+		desc = WORMSKILLS.COUNT_LOCK_1_DESC,
 		pos = {UI_LEFT + 13, UI_BOTTOM + 140},
 		group = "allegiance1",
 		tags = {"allegiance", "lock"},
@@ -449,8 +451,8 @@ local skills =
 		end,
 	},
 	wormwood_allegiance_lunar_eqex = {
-		title = "Equivalent Exchange",
-		desc = "Transmute seeds into other seeds.",
+		title = WORMSKILLS.EQEX_TITLE,
+		desc = WORMSKILLS.EQEX_DESC,
 		icon = "wormwood_eqex",
 		pos = {UI_LEFT + 13, UI_BOTTOM + 175},
 		locks = {"wormwood_allegiance_lock_lunar_1", "wormwood_allegiance_count_lock_1"},
@@ -485,8 +487,8 @@ local skills =
 		},
 	},
 	wormwood_moon_cap_eating = {
-		title = "Mooncap Savant", --"Mooncap eating" -> "Impeccable crops" Wild crops automatically tend themselves.
-		desc = "Learn how to plant Moon Shrooms in Mushroom Planters and Lunar Mushtrees. Eat them for sleep-inducing spores. Dusk caps react strangely when ingested.",
+		title = WORMSKILLS.MOONCAP_SAVANT_TITLE, --"Mooncap eating" -> "Impeccable crops" Wild crops automatically tend themselves.
+		desc = WORMSKILLS.MOONCAP_SAVANT_DESC,
 		icon = "wormwood_moon_cap_eating",
 		pos = {UI_LEFT - 14, UI_TOP + 60},
 
@@ -494,8 +496,8 @@ local skills =
 		tags = {"allegiance", "lunar", "lunar_favor"},
 	},
 	wormwood_lunar_mutations = {
-		title = "Mutator Novice",
-		desc = "Tap into your lunar roots to transform Carrots, Dragon Fruit, and light Bulbs into sentient allies.",
+		title = WORMSKILLS.MUTATOR_NOVICE_TITLE,
+		desc = WORMSKILLS.MUTATOR_NOVICE_DESC,
 		icon = "wormwood_mutations",
 		pos = {UI_LEFT + 40, UI_TOP + 60},
 		group = "allegiance1",
