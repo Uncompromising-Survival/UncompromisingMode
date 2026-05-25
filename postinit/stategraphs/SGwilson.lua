8c6ce3c2def1b96e99efad74360c0f0a60d4c6fc
AtobaAzul <atoba.azul.radical@gmail.com>
2026-05-25 20:09:21 -0300

    more shield of terror tweaks

--- postinit/stategraphs/SGwilson.lua
+++ postinit/stategraphs/SGwilson.lua
@@ -118,8 +118,8 @@ env.AddStategraphPostInit("wilson", function(inst)
     local SLEEPREPEL_MUST_TAGS = { "_combat" }
     local SLEEPREPEL_CANT_TAGS = { "player", "companion", "abigail", "shadow", "playerghost", "INLIMBO", "wixieshoved", "invisible",
         "hiding", "NOTARGET", "flight", "toadstool" }
-    local SHIELD_CANT_TAGS = { "player", "companion", "abigail", "shadow", "playerghost", "INLIMBO", "wixieshoved", "invisible",
-        "hiding", "NOTARGET", "flight", "toadstool", "bird"}
+    local SHIELD_CANT_TAGS = { "player", "companion", "abigail", "playerghost", "INLIMBO", "wixieshoved", "invisible",
+        "hiding", "NOTARGET", "flight", "toadstool", "bird", "wall" }
 
     local function CheckShield(inst)
         if inst ~= nil then
@@ -128,55 +128,55 @@ env.AddStategraphPostInit("wilson", function(inst)
             local ents = TheSim:FindEntities(x, y, z, 3.5, SLEEPREPEL_MUST_TAGS, SHIELD_CANT_TAGS)
 
             for i, v in ipairs(ents) do
-                v:AddTag("wixieshoved")
-                SpawnPrefab("round_puff_fx_sm").Transform:SetPosition(v.Transform:GetWorldPosition())
+                if v.components.combat ~= nil and v.components.combat:CanBeAttacked(inst) then
+                    v:AddTag("wixieshoved")
+                    SpawnPrefab("round_puff_fx_sm").Transform:SetPosition(v.Transform:GetWorldPosition())
 
-                if v.components.combat ~= nil then
                     inst.components.combat:DoAttack(v, nil, nil, nil, 76.5 / 59.5, 4)
-                end
 
-                if v.components.locomotor ~= nil and not v:HasTag("stageusher") then
-                    for i = 1, 50 do
-                        v:DoTaskInTime((i - 1) / 50, function(v)
-                            if v ~= nil and inst ~= nil then
-                                local x, y, z = inst.Transform:GetWorldPosition()
-                                local tx, ty, tz = v.Transform:GetWorldPosition()
+                    if v.components.locomotor ~= nil and not v:HasTag("stageusher") then
+                        for i = 1, 50 do
+                            v:DoTaskInTime((i - 1) / 50, function(v)
+                                if v ~= nil and inst ~= nil then
+                                    local x, y, z = inst.Transform:GetWorldPosition()
+                                    local tx, ty, tz = v.Transform:GetWorldPosition()
 
-                                local rad = math.rad(inst:GetAngleToPoint(tx, ty, tz))
-                                local velx = math.cos(rad)  --* 4.5
-                                local velz = -math.sin(rad) --* 4.5
+                                    local rad = math.rad(inst:GetAngleToPoint(tx, ty, tz))
+                                    local velx = math.cos(rad)  --* 4.5
+                                    local velz = -math.sin(rad) --* 4.5
 
-                                local giantreduction = v:HasTag("epic") and 1.5 or v:HasTag("smallcreature") and 0.8 or 1
-                                local shovevalue = inst:HasTag("troublemaker") and 2 or 1
+                                    local giantreduction = v:HasTag("epic") and 1.5 or v:HasTag("smallcreature") and 0.8 or 1
+                                    local shovevalue = inst:HasTag("troublemaker") and 2 or 1
 
-                                local dx, dy, dz =
-                                    tx + (((shovevalue / (i + 3)) * velx) / giantreduction), ty,
-                                    tz + (((shovevalue / (i + 3)) * velz) / giantreduction)
-                                local ground = TheWorld.Map:IsPassableAtPoint(dx, dy, dz)
-                                local boat = TheWorld.Map:GetPlatformAtPoint(dx, dz)
-                                local ocean_collision = TheWorld.Map:IsOceanAtPoint(dx, dy, dz)
+                                    local dx, dy, dz =
+                                        tx + (((shovevalue / (i + 3)) * velx) / giantreduction), ty,
+                                        tz + (((shovevalue / (i + 3)) * velz) / giantreduction)
+                                    local ground = TheWorld.Map:IsPassableAtPoint(dx, dy, dz)
+                                    local boat = TheWorld.Map:GetPlatformAtPoint(dx, dz)
+                                    local ocean_collision = TheWorld.Map:IsOceanAtPoint(dx, dy, dz)
 
-                                if not (v.sg ~= nil and (v.sg:HasStateTag("swimming") or v.sg:HasStateTag("invisible"))) then
-                                    if v ~= nil and dx ~= nil and (ground or boat or ocean_collision and v.components.locomotor:CanPathfindOnWater() or v.components.tiletracker ~= nil and not v:HasTag("whale")) then
-                                        --[[if ocean_collision and v.components.amphibiouscreature and not v.components.amphibiouscreature.in_water then
+                                    if not (v.sg ~= nil and (v.sg:HasStateTag("swimming") or v.sg:HasStateTag("invisible"))) then
+                                        if v ~= nil and dx ~= nil and (ground or boat or ocean_collision and v.components.locomotor:CanPathfindOnWater() or v.components.tiletracker ~= nil and not v:HasTag("whale")) then
+                                            --[[if ocean_collision and v.components.amphibiouscreature and not v.components.amphibiouscreature.in_water then
                                                 v.components.amphibiouscreature:OnEnterOcean()
                                             end]]
-                                        v.Transform:SetPosition(dx, dy, dz)
+                                            v.Transform:SetPosition(dx, dy, dz)
+                                        end
                                     end
                                 end
-                            end
-                        end)
+                            end)
+                        end
                     end
+
+                    v:DoTaskInTime(1, function(v)
+                        v:RemoveTag("wixieshoved")
+                    end)
+
+
+                    inst.sg.statemem.recoilstate = "attack_recoil"
+                    inst:PushEventImmediate("recoil_off", { target = v })
+                    break --only hit once
                 end
-
-                v:DoTaskInTime(1, function(v)
-                    v:RemoveTag("wixieshoved")
-                end)
-
-
-                inst.sg.statemem.recoilstate = "attack_recoil"
-                inst:PushEventImmediate("recoil_off", { target = v })
-                break --only hit once
             end
         end
     end
