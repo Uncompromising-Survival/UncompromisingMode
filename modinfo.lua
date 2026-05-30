119705ca0568cfaf523e46ea74dbf3b86a45f10e
ShyNuke <leoserow123@gmail.com>
2026-05-30 14:03:56 -0300

    Ver.

--- modinfo.lua
+++ modinfo.lua
@@ -7,7 +7,7 @@ end
 
 --MAJOR.MINOR.FIX
 --INCREASE THIS VERSION
-local _version = "5.5.0"
+local _version = "5.6.0"
 
 description = [[
     󰀔 ["On The Rocks!" v1.6  (Beta ver: v]] .. _version .. [[)]
