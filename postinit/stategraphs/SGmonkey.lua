994d5025774bc0577eace3e3a60b725eb0d897b6
Maximum101 <maxisawesome103@gmail.com>
2026-05-29 23:21:54 -0400

    monkey

---
+++ postinit/stategraphs/SGmonkey.lua
@@ -0,0 +1,23 @@
+local env = env
+GLOBAL.setfenv(1, GLOBAL)
+
+local function IsThrowerEquipped(inst)
+    local thrower = inst.weaponitems and inst.weaponitems.thrower
+    local inventory = inst.components.inventory
+    return thrower and thrower:IsValid() and inventory and inventory:IsItemEquipped(thrower)
+end
+
+env.AddStategraphPostInit("monkey", function(inst)
+    local doattack_eventhandler = inst.events["doattack"]
+    if doattack_eventhandler then
+        local doattack_eventhandler_fn = doattack_eventhandler.fn
+        doattack_eventhandler.fn = function(inst, data, ...)
+            if inst.components.health and not (inst.components.health:IsDead() or inst.sg:HasStateTag("busy")) then
+                -- Fixes a bug where Monkies want to throw instead of attack when they shouldn't.
+                inst.sg:GoToState(IsThrowerEquipped(inst) and "throw" or "attack")
+                return
+            end
+            return doattack_eventhandler_fn(inst, data, ...)
+        end
+    end
+end)
