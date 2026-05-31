bc4f9d0e321458c4cd3dad44c37e8b562578a8f1
Maximum101 <maxisawesome103@gmail.com>
2026-05-30 23:21:11 -0400

    update monkey fix.

--- postinit/stategraphs/SGmonkey.lua
+++ postinit/stategraphs/SGmonkey.lua
@@ -10,14 +10,14 @@ end
 env.AddStategraphPostInit("monkey", function(inst)
     local doattack_eventhandler = inst.events["doattack"]
     if doattack_eventhandler then
-        local doattack_eventhandler_fn = doattack_eventhandler.fn
+        --local doattack_eventhandler_fn = doattack_eventhandler.fn
         doattack_eventhandler.fn = function(inst, data, ...)
             if inst.components.health and not (inst.components.health:IsDead() or inst.sg:HasStateTag("busy")) then
                 -- Fixes a bug where Monkeys want to throw instead of attack when they shouldn't.
                 inst.sg:GoToState(IsThrowerEquipped(inst) and "throw" or "attack")
-                return
+                --return
             end
-            return doattack_eventhandler_fn(inst, data, ...)
+            --return doattack_eventhandler_fn(inst, data, ...)
         end
     end
 end)
