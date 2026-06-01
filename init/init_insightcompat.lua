0bdccf78d90ed17cf1ead2de250785dfc8dd67fa
Penguin <penguin@localhost>
2026-06-01 18:46:59 -0500

    Update descriptor loading

--- init/init_insightcompat.lua
+++ init/init_insightcompat.lua
@@ -3,9 +3,9 @@ _G = GLOBAL
 local function AddDescriptors()
     if not _G.rawget(_G, "Insight") then return end
 
-    _G.Insight.descriptors.uncompromising_deerclopsspawner = _G.require("descriptors/uncompromising_deerclopsspawner")
-    _G.Insight.descriptors.mock_dragonflyspawner = _G.require("descriptors/mock_dragonflyspawner")
-    _G.Insight.descriptors.gmoosespawner = _G.require("descriptors/gmoosespawner")
+    _G.Insight.API.V1.AddComponentDescriptor("uncompromising_deerclopsspawner", _G.require("descriptors/uncompromising_deerclopsspawner"), { modname = modname })
+    _G.Insight.API.V1.AddComponentDescriptor("mock_dragonflyspawner", _G.require("descriptors/mock_dragonflyspawner"), { modname = modname })
+    _G.Insight.API.V1.AddComponentDescriptor("gmoosespawner", _G.require("descriptors/gmoosespawner"), { modname = modname })
 end
 
 AddSimPostInit(AddDescriptors) -- _G.Insight.descriptors may not exist yet, but it will exist at AddSimPostInit.
