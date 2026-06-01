6a82a5e6c593440f685837d773126a9ed6c84215
Maximum101 <maxisawesome103@gmail.com>
2026-06-01 05:09:49 -0400

    Update um_pawn.lua

--- scripts/prefabs/um_pawn.lua
+++ scripts/prefabs/um_pawn.lua
@@ -324,14 +324,14 @@ end
 
 local function TestForCrash(inst, data)
     if data then
-        print(tostring(inst).." got target when it isnt supposed to. (New: "..(data.target or "N/A")..", Old: "..(data.oldtarget or "N/A")..")")
+        print(tostring(inst).." got target when it isnt supposed to. (New: "..tostring(data.target or "N/A")..", Old: "..tostring(data.oldtarget or "N/A")..")")
         inst.SoundEmitter:PlaySound("dontstarve/common/teleportato/teleportato_maxwelllaugh")
     end
 end
 
 local function TestForCrash2(inst, data)
     if data then
-        print(tostring(inst).." got target when it isnt supposed to. (Old: "..(data.target or "N/A")..")")
+        print(tostring(inst).." got target when it isnt supposed to. (Old: "..tostring(data.target or "N/A")..")")
         inst.SoundEmitter:PlaySound("dontstarve/common/teleportato/teleportato_maxwelllaugh")
     end
 end
