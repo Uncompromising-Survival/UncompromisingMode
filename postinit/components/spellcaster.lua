6df0549fbc3a2c2cf909099ba717c7b1acb83b1c
Maximum101 <maxisawesome103@gmail.com>
2026-06-02 01:51:23 -0400

    Update spellcaster.lua

--- postinit/components/spellcaster.lua
+++ postinit/components/spellcaster.lua
@@ -20,15 +20,31 @@ env.AddComponentPostInit("spellcaster", function(self)
     end
 end)
 
+--[[local ignoredactions = {ACTIONS.LOOKAT, ACTIONS.WALKTO}
+local function CheckActions(doer, target)
+    local actions = doer.components.playeractionpicker and doer.components.playeractionpicker:GetSceneActions(target, true)
+    local count = 0
+    if actions then
+        for k, v in pairs(actions) do
+            if not table.contains(ignoredactions, v.action) then
+                count = count + 1
+            end
+        end
+    end
+    return count >= 1
+end]]
+
 local UpvalueHacker = require("tools/upvaluehacker")
 env.AddSimPostInit(function()
     local COMPONENT_ACTIONS = UpvalueHacker.GetUpvalue(EntityScript.CollectActions, "COMPONENT_ACTIONS")
     if COMPONENT_ACTIONS then
-        local EQUIPPED = COMPONENT_ACTIONS.EQUIPPED
+        local --[[POINT,]] EQUIPPED = --[[COMPONENT_ACTIONS.POINT,]] COMPONENT_ACTIONS.EQUIPPED
         if EQUIPPED then
             local _EQUIPPED_spellcaster_fn = EQUIPPED["spellcaster"]
             if _EQUIPPED_spellcaster_fn then
                 EQUIPPED["spellcaster"] = function(inst, doer, target, actions, right, ...)
+                    --[[if inst:HasTag("um_checksceneactions") and doer.components.playercontroller
+                        and not doer.components.playercontroller:IsControlPressed(CONTROL_FORCE_INSPECT) and CheckActions(doer, target) then return end]]
                     if CantCastOnTarget(inst, target, true) then return end
                     return _EQUIPPED_spellcaster_fn(inst, doer, target, actions, right, ...)
                 end
