b16048f6d1cfb8bc6875d3aacb85ec5372329b3f
ShyNuke <leoserow123@gmail.com>
2026-06-03 17:17:40 -0300

    Vet Curse Stuff

--- postinit/prefabs/shieldofcthulu.lua
+++ postinit/prefabs/shieldofcthulu.lua
@@ -94,6 +94,10 @@ local function CommonFunctions(inst, sound, anim)
     end
 end
 
+local function OnCharged(inst)
+	inst.SoundEmitter:PlaySound("terraria1/eyeofterror/charge", nil, .3)
+end
+
 local function OnAttack(inst, attacker, target)
     local efficientuser = attacker.components.efficientuser and attacker.components.efficientuser:GetMultiplier(ACTIONS.ATTACK) or 1
     local useMult = efficientuser * inst.components.weapon.attackwearmultipliers:Get()
@@ -169,6 +173,7 @@ env.AddPrefabPostInit("shieldofterror", function(inst)
     CommonFunctions(inst, "eye_shield", "idle")
 
     local rechargeable = inst.components.rechargeable or inst:AddComponent("rechargeable")
+	inst.components.rechargeable:SetOnChargedFn(OnCharged)
 
     inst.UMToggleItemVetcurse = ToggleItemVetcurse
 
