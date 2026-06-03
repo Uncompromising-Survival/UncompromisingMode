1f6cf8328f8a416bc302cdf4e6feccd65d53e4f5
CarlosBraw <91644972+CarlosBraw@users.noreply.github.com>
2026-06-03 15:38:11 -0400

    PKTrades tweaks + Ocupus drops food value tweaks

--- postinit/prefabs/pktrades.lua
+++ postinit/prefabs/pktrades.lua
@@ -1,21 +1,22 @@
 local env = env
 GLOBAL.setfenv(1, GLOBAL)
 
-env.AddPrefabPostInit("coontail", function(inst)
-	if not TheWorld.ismastersim then
-		return
-	end
+local trade_prefabs = {"coontail", "hambat", "tentaclespots"}
+if TUNING.DSTU.LONGPIG then
+	table.insert(trade_prefabs, "reviver")
+end
+for i,v in ipairs(trade_prefabs) do
+	env.AddPrefabPostInit(v, function(inst)
+		if not TheWorld.ismastersim then return end
 
-	inst:AddComponent("tradable")
-	inst.components.tradable.goldvalue = 1
-end)
+		if not inst.components.tradable then
+			inst:AddComponent("tradable")
+		end
 
-env.AddPrefabPostInit("hambat", function(inst)
-	if not TheWorld.ismastersim then
-		return
-	end
-	if inst.components.tradable == nil then
-		inst:AddComponent("tradable")
-	end
-	inst.components.tradable.goldvalue = 1
-end)
+		if v == "coontail" then
+			inst.components.tradable.goldvalue = TUNING.GOLD_VALUES.MEAT
+		else
+			inst.components.tradable.goldvalue = TUNING.GOLD_VALUES.MEAT*2
+		end
+	end)
+end
