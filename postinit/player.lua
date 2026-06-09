f102dfcb91f5d9318e871724b67fe2571a808cc9
Maximum101 <maxisawesome103@gmail.com>
2026-06-08 23:13:40 -0400

    Update player.lua

--- postinit/player.lua
+++ postinit/player.lua
@@ -396,10 +396,11 @@ env.AddPlayerPostInit(function(inst)
     local current_time = os.time()
     local current_month = os.date("*t", current_time).month
 
-    if CLIENT_MOD_RPC["ModdedSkins"] then
+    local UNLOCKMODDEDSKIN_RPC = CLIENT_MOD_RPC["ModdedSkins"] and GetClientModRPC("ModdedSkins", "UnlockModdedSkin")
+    if UNLOCKMODDEDSKIN_RPC then
         inst:DoTaskInTime(0, function()
             if current_month == 6 then
-                SendModRPCToClient(GetClientModRPC("ModdedSkins", "UnlockModdedSkin"), inst.userid, "ms_um_trans_bomb_moon")
+                SendModRPCToClient(UNLOCKMODDEDSKIN_RPC, inst.userid, "ms_um_trans_bomb_moon")
             end
         end)
     end
