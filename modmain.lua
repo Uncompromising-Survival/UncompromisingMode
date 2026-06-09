3b82bce79d1acab20c63a9897107baf95eb481d2
Maximum101 <maxisawesome103@gmail.com>
2026-06-08 22:26:04 -0400

    Update modmain.lua

--- modmain.lua
+++ modmain.lua
@@ -504,6 +504,7 @@ GLOBAL.ancient_amulet_red_init_fn = function(inst, build_name) GLOBAL.basic_init
 GLOBAL.ancient_amulet_red_clear_fn = function(inst) GLOBAL.basic_clear_fn(inst, "amulet_red_ground") end
 
 GLOBAL.um_bomb_moon_init_fn = function(inst, build_name) GLOBAL.basic_init_fn(inst, build_name, "um_bomb_moon") end
+
 GLOBAL.um_bomb_moon_clear_fn = function(inst) GLOBAL.basic_clear_fn(inst, "um_bomb_moon") end
 
 GLOBAL.TUNING.DSTU.MODROOT = MODROOT
