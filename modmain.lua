a97931be3b4c4903a54ee9c953a91d17defd3f8e
Maximum101 <maxisawesome103@gmail.com>
2026-06-08 22:25:55 -0400

    Update modmain.lua

--- modmain.lua
+++ modmain.lua
@@ -506,9 +506,6 @@ GLOBAL.ancient_amulet_red_clear_fn = function(inst) GLOBAL.basic_clear_fn(inst, 
 GLOBAL.um_bomb_moon_init_fn = function(inst, build_name) GLOBAL.basic_init_fn(inst, build_name, "um_bomb_moon") end
 GLOBAL.um_bomb_moon_clear_fn = function(inst) GLOBAL.basic_clear_fn(inst, "um_bomb_moon") end
 
-
-
-
 GLOBAL.TUNING.DSTU.MODROOT = MODROOT
 
 modimport("init/init_statusannouncements")
@@ -518,4 +515,3 @@ AddSimPostInit(function()
         GLOBAL.ShadeRenderer:SetShadeTexture(GLOBAL.ShadeTypes.HoodedForestCanopy, GLOBAL.resolvefilepath("images/giant_tree.tex"))
     end
 end)
-
