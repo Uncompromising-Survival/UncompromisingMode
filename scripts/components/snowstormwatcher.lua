96ce3f6a72c245d5f37175406b868c41a482f38d
Deimos001 <158612751+Deimos001@users.noreply.github.com>
2026-06-09 15:05:24 -0600

    fixed snowstorm watcher crash in caves

--- scripts/components/snowstormwatcher.lua
+++ scripts/components/snowstormwatcher.lua
@@ -46,6 +46,7 @@ function SnowStormWatcher:SnowstormLevel()
 end
 
 local function MiniBlizzNear(inst)
+    if not TheWorld.components.um_snowstormmanager then return false end
     local x, y, z = inst.Transform:GetWorldPosition()
     local miniblizzards = TheWorld.components.um_snowstormmanager:CheckForOtherRimeweeds({ x = x, y = y, z = z }, 32 * 32)
 
