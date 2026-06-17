da891feffdc26ed526f055134d8b7eb0dc458d8d
Maximum101 <maxisawesome103@gmail.com>
2026-06-16 23:06:30 -0400

    Pick/Axe's buff will now work when equipped by minions.

--- postinit/prefabs/multitool_axe_pickaxe.lua
+++ postinit/prefabs/multitool_axe_pickaxe.lua
@@ -3,25 +3,27 @@ GLOBAL.setfenv(1, GLOBAL)
 -----------------------------------------------------------------\
 
 local function Working(owner, data)
-	if owner ~= nil and owner:HasTag("player") then
-		local x, y, z = owner.Transform:GetWorldPosition()
-		owner:ShakeCamera(CAMERASHAKE.SIDE, 0.25, 0.02, 0.25)
-		
-		local ents = TheSim:FindEntities(x, y, z, 4, nil, { "INLIMBO", "DIG_workable" }, { "CHOP_workable", "MINE_workable" })
-		if data.target ~= nil then
-			local ring = SpawnPrefab("groundpoundring_fx")
-			local x1, y1, z1 = data.target.Transform:GetWorldPosition()
+    if owner and not owner:HasTag("equipmentmodel") then
+        local x, y, z = owner.Transform:GetWorldPosition()
+        if owner:HasTag("player") then
+            owner:ShakeCamera(CAMERASHAKE.SIDE, 0.25, 0.02, 0.25)
+        end
+        
+        local ents = TheSim:FindEntities(x, y, z, 4, nil, { "INLIMBO", "DIG_workable" }, { "CHOP_workable", "MINE_workable" })
+        if data.target ~= nil then
+            local ring = SpawnPrefab("groundpoundring_fx")
+            local x1, y1, z1 = data.target.Transform:GetWorldPosition()
 
-			ring.Transform:SetPosition(x1, y1, z1)
-			ring.Transform:SetScale(0.55, 0.55, 0.55)
-		end
-		
-		for k, v in ipairs(ents) do
-			if v ~= data.target and v.components.workable ~= nil and (v.components.workable:GetWorkAction() == ACTIONS.MINE or v.components.workable:GetWorkAction() == ACTIONS.CHOP) and v.prefab ~= "moonstorm_glass_nub" then
-				v.components.workable:WorkedBy(v, 1)
-			end
-		end
-	end
+            ring.Transform:SetPosition(x1, y1, z1)
+            ring.Transform:SetScale(0.55, 0.55, 0.55)
+        end
+        
+        for k, v in ipairs(ents) do
+            if v ~= data.target and v.components.workable ~= nil and (v.components.workable:GetWorkAction() == ACTIONS.MINE or v.components.workable:GetWorkAction() == ACTIONS.CHOP) and v.prefab ~= "moonstorm_glass_nub" then
+                v.components.workable:WorkedBy(v, 1)
+            end
+        end
+    end
 end
 
 env.AddPrefabPostInit("multitool_axe_pickaxe", function(inst)
