9d4b9856d4aee36e29a46ae2d5909bd41620bff8
AtobaAzul <atoba.azul.radical@gmail.com>
2026-06-18 13:33:13 -0300

    ver

--- modinfo.lua
+++ modinfo.lua
@@ -6,7 +6,7 @@ if not folder_name:find("workshop-") then
 end
 
 --RELEASE.MAJOR.MINOR.FIX
-local _version = "1.5.4.2"
+local _version = "1.5.4.3"
 description = [[
 󰀔 [ Version: ]] .. _version .. [[ - "Wixie, Walter, Woby and Winky!" ]
 
