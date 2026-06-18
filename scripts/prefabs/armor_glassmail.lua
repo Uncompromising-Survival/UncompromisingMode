c2667dd4beb19fc05d2a540cee95c9380ab5d250
Deimos001 <158612751+Deimos001@users.noreply.github.com>
2026-06-18 10:12:33 -0600

    (beta fix) Shard-Scale Armor shards fix.

--- scripts/prefabs/armor_glassmail.lua
+++ scripts/prefabs/armor_glassmail.lua
@@ -1,73 +1,73 @@
 local function OnBlocked(owner, data)
     owner.SoundEmitter:PlaySound("dontstarve/wilson/hit_scalemail")
-	--local lavaetaken = false
+    --local lavaetaken = false
 
-	for i = 1,8 do
-		if owner.lavae[i].hidden == false then --and lavaetaken == false  then --I changed my mind, shatter ALL the lavae, like a *glass* cannon XD
-			owner.lavae[i].PlayBreakSound(owner.lavae[i])
-			owner.lavae[i].BreakShard(owner.lavae[i])
-			owner.lavae[i]:Hide()
-			owner.lavae[i].hidden = true
-			owner.lavae[i].Light:Enable(false)
-			--lavaetaken = true
-		end
-	end
+    for i = 1,8 do
+        if owner.lavae[i].hidden == false then --and lavaetaken == false  then --I changed my mind, shatter ALL the lavae, like a *glass* cannon XD
+            owner.lavae[i].PlayBreakSound(owner.lavae[i])
+            owner.lavae[i].BreakShard(owner.lavae[i])
+            owner.lavae[i]:Hide()
+            owner.lavae[i].hidden = true
+            owner.lavae[i].Light:Enable(false)
+            --lavaetaken = true
+        end
+    end
 end
 
 local function SpawnGlassDummies(inst)
-	local x,y,z = inst.Transform:GetWorldPosition()
-	local LIMIT = 2
-	inst.lavae = {}
-	for i = 1,8 do
-		inst.lavae[i] = SpawnPrefab("armor_glassmail_shards")
-		inst.lavae[i].WINDSTAFF_CASTER = inst
-		inst.lavae[i].components.linearcircler:SetCircleTarget(inst)
-		inst.lavae[i].components.linearcircler:Start()
-		inst.lavae[i].components.linearcircler.randAng = i*0.125
-		inst.lavae[i].components.linearcircler.clockwise = false
-		inst.lavae[i].components.linearcircler.distance_limit = LIMIT
-		inst.lavae[i].components.linearcircler.setspeed = 0.2
-		inst.lavae[i]:Hide()
-		inst.lavae[i].hidden = true
-	end
+    local x,y,z = inst.Transform:GetWorldPosition()
+    local LIMIT = 2
+    inst.lavae = {}
+    for i = 1,8 do
+        inst.lavae[i] = SpawnPrefab("armor_glassmail_shards")
+        inst.lavae[i].WINDSTAFF_CASTER = inst
+        inst.lavae[i].components.linearcircler:SetCircleTarget(inst)
+        inst.lavae[i].components.linearcircler:Start()
+        inst.lavae[i].components.linearcircler.randAng = i*0.125
+        inst.lavae[i].components.linearcircler.clockwise = false
+        inst.lavae[i].components.linearcircler.distance_limit = LIMIT
+        inst.lavae[i].components.linearcircler.setspeed = 0.2
+        inst.lavae[i]:Hide()
+        inst.lavae[i].hidden = true
+    end
 end
 
 local function RemoveGlassDummies(inst)
-	for i = 1,8 do
-		if inst.lavae[i] ~= nil then
-			if inst.lavae[i].hidden == false then --and lavaetaken == false  then --I changed my mind, shatter ALL the lavae, like a *glass* cannon XD
-				inst.lavae[i].PlayBreakSound(inst.lavae[i])
-				inst.lavae[i].BreakShard(inst.lavae[i])
-			end
-			inst.lavae[i]:Remove()
-		end
-	end
+    for i = 1,8 do
+        if inst.lavae[i] ~= nil then
+            if inst.lavae[i].hidden == false then --and lavaetaken == false  then --I changed my mind, shatter ALL the lavae, like a *glass* cannon XD
+                inst.lavae[i].PlayBreakSound(inst.lavae[i])
+                inst.lavae[i].BreakShard(inst.lavae[i])
+            end
+            inst.lavae[i]:Remove()
+        end
+    end
 end
 
 local function TempDamage(inst)
-	for i = 1,8 do
-		inst.lavae[i].Speen(inst.lavae[i])
-	end
+    for i = 1,8 do
+        inst.lavae[i].Speen(inst.lavae[i])
+    end
 end
 
 local function TryAddCrystal(inst)
 local rand = math.random(1,8)
-	if inst.lavae[rand].hidden then
-		inst.lavae[rand].hidden = false
-		inst.lavae[rand]:Show()
-		inst.lavae[rand].Light:Enable(true)
-		inst.lavae[rand].SummonShard(inst.lavae[rand])
-	else
-		TryAddCrystal(inst)
-	end
+    if inst.lavae[rand].hidden then
+        inst.lavae[rand].hidden = false
+        inst.lavae[rand]:Show()
+        inst.lavae[rand].Light:Enable(true)
+        inst.lavae[rand].SummonShard(inst.lavae[rand])
+    else
+        TryAddCrystal(inst)
+    end
 end
 
 local function NoCrystalsLeftToSpawn(inst)
 local val = true
 for i = 1,8 do
-	if inst.lavae[i].hidden then
-		val = false 
-	end
+    if inst.lavae[i].hidden then
+        val = false 
+    end
 end
 return val
 end
@@ -90,22 +90,22 @@ local function onequip(inst, owner)
 
     owner.AnimState:OverrideSymbol("swap_body", "armor_glassmail", "swap_body")
 
-	SpawnGlassDummies(owner)
-	
+    SpawnGlassDummies(owner)
+    
     inst:ListenForEvent("blocked", OnBlocked, owner)
     inst:ListenForEvent("attacked", OnBlocked, owner)
-	owner:ListenForEvent("onattackother", UpdateGlass)
-	
+    owner:ListenForEvent("onattackother", UpdateGlass)
+    
     if owner.components.health ~= nil then
         owner.components.health.externalfiredamagemultipliers:SetModifier(inst, 1 - TUNING.ARMORDRAGONFLY_FIRE_RESIST)
     end
-	
+    
     if inst._light == nil or not inst._light:IsValid() then
         inst._light = SpawnPrefab("armor_glassmail_light")
     end
     if owner ~= nil then
         inst._light.entity:SetParent(owner.entity)
-    end	
+    end    
 end
 
 local function onunequip(inst, owner)
@@ -113,18 +113,18 @@ local function onunequip(inst, owner)
 
     inst:RemoveEventCallback("blocked", OnBlocked, owner)
     inst:RemoveEventCallback("attacked", OnBlocked, owner)
-	owner:RemoveEventCallback("onattackother", UpdateGlass)
-	
+    owner:RemoveEventCallback("onattackother", UpdateGlass)
+    
     if owner.components.health ~= nil then
         owner.components.health.externalfiredamagemultipliers:RemoveModifier(inst)
     end
     if inst._light ~= nil then
-		if inst._light:IsValid() then
+        if inst._light:IsValid() then
            inst._light:Remove()
         end
     inst._light = nil
     end
-	RemoveGlassDummies(owner)
+    RemoveGlassDummies(owner)
 end
 
 local function fn()
@@ -133,7 +133,7 @@ local function fn()
     inst.entity:AddTransform()
     inst.entity:AddAnimState()
     inst.entity:AddNetwork()
-	inst.entity:AddLight()
+    inst.entity:AddLight()
     inst.AnimState:SetBloomEffectHandle("shaders/anim.ksh")
 
     inst.Light:SetColour(111/255, 111/255, 227/255)
@@ -141,7 +141,7 @@ local function fn()
     inst.Light:SetFalloff(0.5)
     inst.Light:SetRadius(0.5)
     inst.Light:Enable(true)  
-	
+    
     MakeInventoryPhysics(inst)
 
     inst.AnimState:SetBank("armor_glassmail")
@@ -154,15 +154,11 @@ local function fn()
 
     inst.entity:SetPristine()
 
-    if not TheWorld.ismastersim then
-        return inst
-    end
+    if not TheWorld.ismastersim then return inst end
 
     inst:AddComponent("inspectable")
     inst:AddComponent("inventoryitem")
-
-	inst.components.inventoryitem.atlasname = "images/inventoryimages/armor_glassmail.xml"
-	
+    
     inst:AddComponent("armor")
     inst.components.armor:InitCondition(TUNING.ARMORDRAGONFLY, TUNING.ARMORDRAGONFLY_ABSORPTION)
 
@@ -172,9 +168,10 @@ local function fn()
     inst.components.equippable:SetOnEquip(onequip)
     inst.components.equippable:SetOnUnequip(onunequip)
 
-	inst:AddComponent("timer")
-	
+    inst:AddComponent("timer")
+    
     MakeHauntableLaunch(inst)
+
     return inst
 end
 
@@ -194,9 +191,7 @@ local function armor_glassmail_light_fn()
 
     inst.entity:SetPristine()
 
-    if not TheWorld.ismastersim then
-        return inst
-    end
+    if not TheWorld.ismastersim then return inst end
 
     inst.persists = false
 
@@ -204,5 +199,4 @@ local function armor_glassmail_light_fn()
 end
 
 return Prefab("armor_glassmail", fn),
-Prefab("armor_glassmail_light", armor_glassmail_light_fn)
-
+    Prefab("armor_glassmail_light", armor_glassmail_light_fn)
