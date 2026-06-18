af827b40b621bcd28a85d474586aa8e4b4faac5b
Maximum101 <maxisawesome103@gmail.com>
2026-06-17 21:42:14 -0400

    Update init_ripples.lua

--- init/init_weather/init_ripples.lua
+++ init/init_weather/init_ripples.lua
@@ -38,7 +38,7 @@ env.AddPrefabPostInitAny(function(inst)
         inst.um_ripple_blacklist = true
     end
 
-    if (inst:HasAnyTag("structure", "boulder", "plant") --[[or inst.components.inventoryitem]]) and not inst.um_ripple_blacklist then
+    if (inst:HasAnyTag("structure", "boulder", "plant") --[[or inst.components.inventoryitem]]) and not inst.components.floater and not inst.um_ripple_blacklist then
         if not inst.components.umripples then
             inst:AddComponent("umripples")
         end
