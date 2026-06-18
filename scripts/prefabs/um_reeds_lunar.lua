67fff105ed6169dab7d19a49b58c5f42377954cd
Maximum101 <maxisawesome103@gmail.com>
2026-06-18 16:58:24 -0400

    Update um_reeds_lunar.lua

--- scripts/prefabs/um_reeds_lunar.lua
+++ scripts/prefabs/um_reeds_lunar.lua
@@ -20,13 +20,13 @@ local function on_chop(inst, chopper, remaining_chops)
 
     if remaining_chops > 0 then
         inst.AnimState:PlayAnimation("chop")
-		inst.AnimState:PushAnimation("idle")
+        inst.AnimState:PushAnimation("idle")
     end
 end
 
 local function RemoveWorkable(inst)
-	inst:RemoveComponent("workable")
-	inst.AnimState:PushAnimation("idle_chopped",true)
+    inst:RemoveComponent("workable")
+    inst.AnimState:PushAnimation("idle_chopped",true)
 end
 
 
@@ -34,13 +34,13 @@ local function on_chopped_down(inst, chopper)
     inst.SoundEmitter:PlaySound("dontstarve/forest/appear_wood")
     inst.SoundEmitter:PlaySound("dontstarve/forest/treeCrumble",nil,.4)
 
-	inst.AnimState:PlayAnimation("fall")
-	
-	
-	inst.components.lootdropper:DropLoot()
-	inst.components.timer:StartTimer("regrow",5*60*8)
-	
-	RemoveWorkable(inst)
+    inst.AnimState:PlayAnimation("fall")
+    
+    
+    inst.components.lootdropper:DropLoot()
+    inst.components.timer:StartTimer("regrow",5*60*8)
+    
+    RemoveWorkable(inst)
 end
 
 local function AddWorkable(inst,regrow)
@@ -53,20 +53,18 @@ local function AddWorkable(inst,regrow)
 
     inst.components.workable:SetOnWorkCallback(on_chop)
     inst.components.workable:SetOnFinishCallback(on_chopped_down)
-	if regrow then
-		inst.AnimState:PlayAnimation("regrow")
-	end
+    if regrow then
+        inst.AnimState:PlayAnimation("regrow")
+    end
     inst.AnimState:PushAnimation("idle",true)
 end
 
-
-
 local function Regrow(inst)
-	AddWorkable(inst,true)
+    AddWorkable(inst,true)
 end
 
 local function on_burnt(inst)
-	inst.components.lootdropper:SpawnLootPrefab("ash")
+    inst.components.lootdropper:SpawnLootPrefab("ash")
     inst:Remove()
 end
 
@@ -86,18 +84,19 @@ local function fn()
 
     inst.AnimState:SetBank("um_reeds_lunar")
     inst.AnimState:SetBuild("um_reeds_lunar")
-	inst:AddComponent("umripples")
-    inst.components.umripples.xscale = 1.2
-    inst.components.umripples.yscale = 1.2
-    inst.components.umripples.zscale = 1.2
-	--inst.components.um_ripples.vert_offset = 0.1
-	--inst.components.um_ripples.should_parent_effect = true
-	
+    
     inst.entity:SetPristine()
+
     if not TheWorld.ismastersim then
         return inst
     end
 
+    inst:AddComponent("umripples")
+    inst.components.umripples.xscale = 1.2
+    inst.components.umripples.yscale = 1.2
+    inst.components.umripples.zscale = 1.2
+    --inst.components.um_ripples.vert_offset = 0.1
+    --inst.components.um_ripples.should_parent_effect = true
 
     MakeMediumBurnable(inst)
     inst.components.burnable:SetOnBurntFn(on_burnt)
@@ -106,29 +105,27 @@ local function fn()
     inst:AddComponent("lootdropper")
     inst.components.lootdropper:SetChanceLootTable("um_reeds_lunar")
 
-
-
     MakeHauntableWorkAndIgnite(inst)
 
     local color = 0.7 + math.random() * 0.3
     inst.AnimState:SetMultColour(color, color, color, 1)
 
     inst:AddComponent("inspectable")
-	
-	inst:DoTaskInTime(0,function(inst)
-		if inst.components.timer:TimerExists("regrow") then
-			RemoveWorkable(inst)
-		else
-			AddWorkable(inst)
-		end
-	end)
+    
+    inst:DoTaskInTime(0,function(inst)
+        if inst.components.timer:TimerExists("regrow") then
+            RemoveWorkable(inst)
+        else
+            AddWorkable(inst)
+        end
+    end)
 
 
-	inst:AddComponent("timer")
-	inst:ListenForEvent("timerdone",Regrow)
+    inst:AddComponent("timer")
+    inst:ListenForEvent("timerdone",Regrow)
     MakeSnowCovered(inst)
 
-	return inst
+    return inst
 end
 
 return Prefab("um_reeds_lunar", fn, assets)
