106570609952fb41c9c79307ee7c658e256e8af2
Maximum101 <90657656+Maximum101@users.noreply.github.com>
2026-08-06 20:23:34 -0400

    Update init_horriblefood.lua

--- init/init_horriblefood.lua
+++ init/init_horriblefood.lua
@@ -1,19 +1,17 @@
 local foods = { --health/hunger/sanity
-    coontail = { 9, 9, 7 },
-    glommerwings = { 50, 10, -20 },
-    hambat = { 10, 50, 0 },
-    manrabbit_tail = { 0, 9.4, 15 },
-    pigskin = { 12.5, 5, 0 },
-    shroom_skin = { 25, 37.5, -15 },
-    slurper_pelt = { 0, 15, 0 },
-    tentaclespots = { 15, 15, -10 },
+    coontail = {9, 9, 7},
+    glommerwings = {50, 10, -20},
+    hambat = {10, 50, 0},
+    manrabbit_tail = {0, 9.4, 15},
+    pigskin = {12.5, 5, 0},
+    shroom_skin = {25, 37.5, -15},
+    slurper_pelt = {0, 15, 0},
+    tentaclespots = {15, 15, -10},
 }
 
 for prefab, stats in pairs(foods) do
     AddPrefabPostInit(prefab, function(inst)
-
-        if not GLOBAL.TheWorld.ismastersim then return
-        end
+        if not GLOBAL.TheWorld.ismastersim then return end
 
         local edible = inst.components.edible or inst:AddComponent("edible")
         edible.healthvalue = stats[1]
