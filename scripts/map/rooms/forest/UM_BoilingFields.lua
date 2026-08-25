f2f1a3b374b54cfda13cfa0bf4c16cd4a64826ab
Maximum101 <90657656+Maximum101@users.noreply.github.com>
2026-08-25 07:42:23 -0400

    Fixed a crash with IA.

--- scripts/map/rooms/forest/UM_BoilingFields.lua
+++ scripts/map/rooms/forest/UM_BoilingFields.lua
@@ -222,7 +222,7 @@ AddRoom("BoilingFields_Sinkhole", {
 })
 
 if KnownModIndex:IsModEnabled("workshop-1467214795") then
-    Layouts["cave_entrance_magmabiome_IA"] = StaticLayout.Get("map/static_layouts/cave_entrance_magmabiome_IA")
+    Layouts["cave_entrance_magmabiome_IA"] = StaticLayout.Get("map/static_layouts/cave_entrance_magmabiome_ia")
     AddRoom("BoilingFields_Sinkhole_IA", {
         colour = { r = 0, g = .9, b = 0, a = .50 },
         value = WORLD_TILES.UM_HOTSPRING_IA,
