239bc1ad64eb44e45b66b2c801f34273669717f5
Maximum101 <maxisawesome103@gmail.com>
2026-09-15 04:52:45 -0400

    Update butterfly.lua

--- postinit/prefabs/butterfly.lua
+++ postinit/prefabs/butterfly.lua
@@ -185,7 +185,7 @@ if TUNING.DSTU.BUTTERFLYWINGS_NERF == "slippery" then
     local __ctor = ButterflySpawner._ctor
     function ButterflySpawner:_ctor(inst, ...)
         local ret = __ctor(self, inst, ...)
-        local _GetSpawnPoint = UMUpvalueHacker.TryGetUpvalue(self.OnPostInit, "ToggleUpdate", "ScheduleSpawn", "SpawnButterflyForPlayer", "GetSpawnPoint")
-        if _GetSpawnPoint then UMUpvalueHacker.SetUpvalue(ButterflySpawner.OnPostInit, GetSpawnPoint, "ToggleUpdate", "ScheduleSpawn", "SpawnButterflyForPlayer", "GetSpawnPoint") end
+        --local _GetSpawnPoint = UMUpvalueHacker.TryGetUpvalue(self.OnPostInit, "ToggleUpdate", "ScheduleSpawn", "SpawnButterflyForPlayer", "GetSpawnPoint")
+        --if _GetSpawnPoint then UMUpvalueHacker.SetUpvalue(ButterflySpawner.OnPostInit, GetSpawnPoint, "ToggleUpdate", "ScheduleSpawn", "SpawnButterflyForPlayer", "GetSpawnPoint") end
     end
 end
