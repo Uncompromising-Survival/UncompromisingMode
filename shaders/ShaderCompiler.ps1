8a0d073889c18bbf4a526f213cf2a6121341ada7
AtobaAzul <atoba.azul.radical@gmail.com>
2026-09-16 22:00:48 -0300

    Adjusted shader compiler script

--- shaders/ShaderCompiler.ps1
+++ shaders/ShaderCompiler.ps1
@@ -1,10 +1,12 @@
 # credit: IA team
-$ROOT = "C:\Program Files (x86)\Steam\steamapps\common\Don't Starve Mod Tools\mod_tools\tools\bin"
-$COMPILER_PATH = $ROOT + "\ShaderCompiler.exe"
+$ROOT = "C:\Program Files (x86)\Steam\steamapps\common\Don't Starve Mod Tools\mod_tools"
+$COMPILER_PATH = $ROOT + "\tools\bin\ShaderCompiler.exe"
+$INPUT_PATH = $ROOT + "\shader\shader_input"
+$OUTPUT_PATH = "D:\SteamLibrary\steamapps\common\Don't Starve Together\mods\UncompromisingMode\shaders"
 
 $shader_name = Read-Host Prompt "Shader Name:"
-$vs_shader_path = $ROOT + "\shader_input\" + $shader_name + ".vs"
+$vs_shader_path = $INPUT_PATH + "\" + $shader_name + ".vs"
 
-$ps_shader_path = $ROOT + "\shader_input\" + $shader_name + ".ps"
-$ksh_shader_path = $ROOT + "\shader_output\" + $shader_name + ".ksh"
+$ps_shader_path = $INPUT_PATH + "\" + $shader_name + ".ps"
+$ksh_shader_path = $OUTPUT_PATH + "\" + $shader_name + ".ksh"
 & $COMPILER_PATH -little $shader_name $vs_shader_path $ps_shader_path $ksh_shader_path -oglsl
