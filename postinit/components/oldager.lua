5c4458cb3e858b74797be280f15cd909921b0d3f
Maximum101 <90657656+Maximum101@users.noreply.github.com>
2026-09-18 19:17:44 -0400

    Update oldager.lua

--- postinit/components/oldager.lua
+++ postinit/components/oldager.lua
@@ -3,12 +3,10 @@ GLOBAL.setfenv(1, GLOBAL)
 
 env.AddComponentPostInit("oldager", function(self)
     local _OnTakeDamage = self.OnTakeDamage
-
     function self:OnTakeDamage(amount, overtime, cause, ignore_invincible, afflicter, ignore_absorb, ...)
-        if self.inst.vetcurse_shadowdeath and afflicter ~= nil and afflicter:HasAnyTag("shadowcreature", "nightmarecreature") then
+        if self.inst.vetcurse_shadowdeath and afflicter and afflicter:HasAnyTag("shadowcreature", "nightmarecreature") then
             amount = TUNING.OLDAGE_HEALTH_SCALE * -400
         end
-
         return _OnTakeDamage(self, amount, overtime, cause, ignore_invincible, afflicter, ignore_absorb, ...)
     end
 end)
