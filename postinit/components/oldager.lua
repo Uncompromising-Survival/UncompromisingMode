a3f24990424c8ab615a9278967aa9533a58613b6
Maximum101 <90657656+Maximum101@users.noreply.github.com>
2026-09-18 19:57:34 -0400

    Wanda vetcurse should now get max health changes if there is any.

--- postinit/components/oldager.lua
+++ postinit/components/oldager.lua
@@ -6,7 +6,8 @@ env.AddComponentPostInit("oldager", function(self)
     local _OnTakeDamage = self.OnTakeDamage
     function self:OnTakeDamage(amount, overtime, cause, ignore_invincible, afflicter, ignore_absorb, ...)
         if self.inst.vetcurse_shadowdeath and afflicter and afflicter:HasAnyTag("shadowcreature", "nightmarecreature") then
-            amount = TUNING.OLDAGE_HEALTH_SCALE * -400
+            local healthscale = TUNING.OLDAGE_HEALTH_SCALE
+            amount = healthscale * -self.inst.components.health.maxhealth / (healthscale * healthscale)
         end
         return _OnTakeDamage(self, amount, overtime, cause, ignore_invincible, afflicter, ignore_absorb, ...)
     end
