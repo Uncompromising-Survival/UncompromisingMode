3354474940d5aff41e8fad1942120602f4810675
AtobaAzul <atoba.azul.radical@gmail.com>
2026-09-18 13:05:56 -0300

    FUCK

--- postinit/components/oldager.lua
+++ postinit/components/oldager.lua
@@ -5,15 +5,6 @@ env.AddComponentPostInit("oldager", function(self)
     local _OnTakeDamage = self.OnTakeDamage
 
     function self:OnTakeDamage(amount, overtime, cause, ignore_invincible, afflicter, ignore_absorb, ...)
-        print("amount", amount)
-        print("overtime", overtime)
-        print("cause", cause)
-        print("ignore_invincible", ignore_invincible)
-        print("afflicter", afflicter)
-        print("ignore_absorb", ignore_absorb)
-        print("...", ...)
-        print("self.inst.vetcurse_shadowdeath", self.inst.vetcurse_shadowdeath)
-
         if self.inst.vetcurse_shadowdeath and afflicter ~= nil and afflicter:HasAnyTag("shadowcreature", "nightmarecreature") then
             amount = TUNING.OLDAGE_HEALTH_SCALE * -400
         end
