8f6a31344db636cf758264c7d16ba8722d7799f7
Maximum101 <maxisawesome103@gmail.com>
2026-09-21 11:08:08 -0400

    Update slingshot_claire.lua

--- scripts/prefabs/slingshot_claire.lua
+++ scripts/prefabs/slingshot_claire.lua
@@ -184,7 +184,6 @@ local function OnStopChanneling(inst, user)
 end
 
 local function createlight(inst, user)
-    --print("wixie should be shooting right now")
     if inst.components.equippable.isequipped then
         user.SoundEmitter:KillSound("twirl")
         inst._wheel:SetSpinning(false)
